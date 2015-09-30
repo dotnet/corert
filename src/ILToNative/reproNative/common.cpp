ab3e67a0b525f776b6fab7645b6051a9bc6d6714
Jan Kotas <jkotas@microsoft.com>
2015-09-30 10:29:25 -0700

    Add VS solution files for ad hoc testing

---
+++ src/ILToNative/reproNative/common.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// common.cpp : source file that includes just the standard includes
+// testNative.pch will be the pre-compiled header
+// common.obj will contain the pre-compiled type information
+
+#include "common.h"
+
+// TODO: reference any additional headers you need in common.H
+// and not in this file
