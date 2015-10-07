9a9eb69a468ed7268a2c19bd99efcffce96bfb87
David Wrighton <davidwr@microsoft.com>
2015-10-06 19:41:36 -0700

    Dependency Analysis framework
    Dependency analysis allows tracing what output components depend on other components from a root set instead of simply working with a list of already loaded things.
    
    This patch adds a framework for performing this computation, but no rules that are specific to compiling ILToNative. That will be follow on work.
    
    Included in this change
    
    - DependencyAnalyzerBase
     - Api surface for use of analyzer.
    - DependencyAnalyzer
     - Object that should actually be constructed to perform analysis. Not intended for most use, so as to allow dynamic switching between various particular specializations.
     - Specialized over a mark strategy. (The mark strategy specialization allows log customization)
    - DependencyNodeCore/DependencyNode
     - Base type of dependency node in depedency graph. Provides a set of facilities to describe dependents.
    - IDependencyAnalysisMarkStrategy
     - Interface underlying mark logging customization.
     - Provided mark strategies
      - NoLogStrategy, no support for logging.
       - Marks, but doesn't log anything. Most memory/performance efficient approach. Expected to be used in retail builds by default for best performance.
      - FirstMarkLogStrategy, the first edge to mark a node is logged
       - More memory efficient logging at mark points. In particular, only logs mark's that newly mark a node.
      - FullGraphLogStrategy
       - Used when the ability to generate a complete log of the graph is important. Fairly memory intensive due to the presence of a great many allocated hash tables
    - DgmlWriter
     - Interfaces with DependencyAnalyzer and various mark strategies to generate a DGML graph of the dependencies. DGML graphs can be viewed in Visual Studio
    - Tests
     - Basic unit test framework in place
     - All types of dependency are tested to minimal standards
     - DGML logging is tested.

---
+++ src/ILToNative.DependencyAnalysisFramework/src/ComputedStaticDependencyNode.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ILToNative.DependencyAnalysisFramework
+{
+    public abstract class ComputedStaticDependencyNode<DependencyContextType> : DependencyNodeCore<DependencyContextType>
+    {
+        private IEnumerable<DependencyListEntry> _dependencies;
+        private IEnumerable<CombinedDependencyListEntry> _conditionalDependencies;
+        private static CombinedDependencyListEntry[] s_emptyDynamicList = new CombinedDependencyListEntry[0];
+
+        public void SetStaticDependencies(IEnumerable<DependencyListEntry> dependencies,
+                                          IEnumerable<CombinedDependencyListEntry> conditionalDependencies)
+        {
+            Debug.Assert(_dependencies == null);
+            Debug.Assert(_conditionalDependencies == null);
+            Debug.Assert(dependencies != null);
+
+            _dependencies = dependencies;
+            _conditionalDependencies = conditionalDependencies;
+        }
+
+        public override bool HasConditionalStaticDependencies
+        {
+            get
+            {
+                return _conditionalDependencies != null;
+            }
+        }
+
+        public override bool HasDynamicDependencies
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override bool InterestingForDynamicDependencyAnalysis
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override bool StaticDependenciesAreComputed
+        {
+            get
+            {
+                return _dependencies != null;
+            }
+        }
+
+        public override IEnumerable<CombinedDependencyListEntry> GetConditionalStaticDependencies(DependencyContextType context)
+        {
+            return _conditionalDependencies;
+        }
+
+        public override IEnumerable<DependencyListEntry> GetStaticDependencies(DependencyContextType context)
+        {
+            return _dependencies;
+        }
+
+        public override IEnumerable<CombinedDependencyListEntry> SearchDynamicDependencies(List<DependencyNodeCore<DependencyContextType>> markedNodes, int firstNode, DependencyContextType context)
+        {
+            return s_emptyDynamicList;
+        }
+    }
+}
