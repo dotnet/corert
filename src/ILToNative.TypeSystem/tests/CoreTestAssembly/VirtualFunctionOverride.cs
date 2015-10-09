4f70e59cc6e337bb714ae0385f0c47e724002080
David Wrighton <davidwr@microsoft.com>
2015-10-08 17:15:19 -0700

    Fix incorrect handling of MethodSpecificiations for MethodImpl overrides
    In earlier code review comments, a minor fix was missed.
    - Fix incorrect handling of MethodSpecifications for MethodImpl overrides
    - Update comment to be more correct
    - Add sln file for type system to ease test development
    - Add test for generic method impl case

---
+++ src/ILToNative.TypeSystem/tests/CoreTestAssembly/VirtualFunctionOverride.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VirtualFunctionOverride
+{
+    interface IIFaceWithGenericMethod
+    {
+        void GenMethod<T>();
+    }
+
+    class HasMethodInterfaceOverrideOfGenericMethod : IIFaceWithGenericMethod
+    {
+        void IIFaceWithGenericMethod.GenMethod<T>() { }
+    }
+}
---
+++ src/ILToNative.TypeSystem/tests/VirtualFunctionOverrideTests.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+using Internal.TypeSystem.Ecma;
+using Internal.TypeSystem;
+
+using Xunit;
+
+
+namespace TypeSystemTests
+{
+    public class VirtualFunctionOverrideTests
+    {
+        TestTypeSystemContext _context;
+        EcmaModule _testModule;
+
+        public VirtualFunctionOverrideTests()
+        {
+            _context = new TestTypeSystemContext(TargetArchitecture.X64);
+            var systemModule = _context.CreateModuleForSimpleName("CoreTestAssembly");
+            _context.SetSystemModule(systemModule);
+
+            _testModule = systemModule;
+        }
+
+        [Fact]
+        public void TestGenericMethodInterfaceMethodImplOverride()
+        {
+            //
+            // Ensure MethodImpl based overriding works for MethodSpecs
+            //
+
+            MetadataType interfaceType = _testModule.GetType("VirtualFunctionOverride", "IIFaceWithGenericMethod");
+            MethodDesc interfaceMethod = null;
+
+            foreach(MethodDesc m in interfaceType.GetMethods())
+            {
+                if (m.Name == "GenMethod")
+                {
+                    interfaceMethod = m;
+                    break;
+                }
+            }
+            Assert.NotNull(interfaceMethod);
+
+            MetadataType objectType = _testModule.GetType("VirtualFunctionOverride", "HasMethodInterfaceOverrideOfGenericMethod");
+            MethodDesc expectedVirtualMethod = null;
+            foreach (MethodDesc m in objectType.GetMethods())
+            {
+                if (m.Name.Contains("GenMethod"))
+                {
+                    expectedVirtualMethod = m;
+                    break;
+                }
+            }
+            Assert.NotNull(expectedVirtualMethod);
+
+            Assert.Equal(expectedVirtualMethod, VirtualFunctionResolution.ResolveInterfaceMethodToVirtualMethodOnType(interfaceMethod, objectType));
+        }
+    }
+}
