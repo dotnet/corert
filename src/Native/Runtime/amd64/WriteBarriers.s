eb85fe3e71862b2cc9b0d032322257006bc4d371
Michal Strehovský <michals@microsoft.com>
2015-11-04 13:33:17 -0800

    Some more Linux Hello World fixes
    * Incomplete implementation of write barriers
    * Sort-of implementation of platform.linux.cpp. I'm actually not sure if
    it works, but might for English.
    
    We still need a fix for #162 to be able to actually use the same
    implementation of JitInterface between desktop CLR and CoreCLR.
    
    After that, Hello World on Linux can be compiled, linked, and partially
    executed (it will crash on ROData access that we already have a pull
    request for).

--- src/Native/Runtime/amd64/WriteBarriers.s
+++ src/Native/Runtime/amd64/WriteBarriers.s
@@ -4,3 +4,14 @@
 //
 
 // TODO: Implement Unix write barriers
+
+.global RhpBulkWriteBarrier
+RhpBulkWriteBarrier:
+ret
+
+.global RhpAssignRef
+.global RhpCheckedAssignRef
+RhpAssignRef:
+RhpCheckedAssignRef:
+mov %rsi, (%rdi)
+ret
