bdf5a48f521604f21feafbbf0ef9df63f4dc9a14
Chris Ahna <chrisahn@microsoft.com>
2016-02-16 14:20:39 -0800

    Write the correct INVALIDGCVALUE marker on amd64
    The current code accidentally sign-extends the marker value, leading
    testGCShadow to interpret it as a bad reference.  This change fixes
    this by ensuring that the assembler generates something like:
    
        48 BA CD CC CC CC 00 00 00 00   mov rdx,0CCCCCCCDh
        48 89 11                        mov qword ptr [rcx],rdx
    
    Instead of something like:
    
        48 C7 01 CD CC CC CC            mov qword ptr [rcx],0FFFFFFFFCCCCCCCDh
    
    [tfs-changeset: 1575615]

--- src/Native/Runtime/amd64/WriteBarriers.asm
+++ src/Native/Runtime/amd64/WriteBarriers.asm
@@ -68,9 +68,14 @@ UPDATE_GC_SHADOW macro BASENAME, REFREG, DESTREG
 
     ;; Retrieve shadow location from the stack and restore original DESTREG to the stack. This is an
     ;; additional memory barrier we don't require but it's on the rare path and x86 doesn't have an xchg
-    ;; variant that doesn't implicitly specify the lock prefix.
+    ;; variant that doesn't implicitly specify the lock prefix. Note that INVALIDGCVALUE is a 64-bit
+    ;; immediate and therefore must be moved into a register before it can be written to the shadow
+    ;; location.
     xchg    [rsp], DESTREG
-    mov     qword ptr [DESTREG], INVALIDGCVALUE
+    push    REFREG
+    mov     REFREG, INVALIDGCVALUE
+    mov     qword ptr [DESTREG], REFREG
+    pop     REFREG
 
 &BASENAME&_UpdateShadowHeap_PopThenDone_&REFREG&:
     ;; Restore original DESTREG value from the stack.
@@ -335,7 +340,10 @@ BulkWriteBarrier_UpdateShadowHeap_NextIteration:
     jmp     BulkWriteBarrier_UpdateShadowHeap_CopyLoop
 
 BulkWriteBarrier_UpdateShadowHeap_LostRace:
-    mov     qword ptr [r9], INVALIDGCVALUE
+    ;; Note that INVALIDGCVALUE is a 64-bit immediate and therefore must be moved into a register before
+    ;; it can be written to the shadow location.
+    mov     rax, INVALIDGCVALUE
+    mov     qword ptr [r9], rax
     jmp     BulkWriteBarrier_UpdateShadowHeap_NextIteration
 
 BulkWriteBarrier_UpdateShadowHeap_Done:
