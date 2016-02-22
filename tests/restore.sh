eb50f8d26f31ef2b15563bedec3bb7055e7e9e58
Jan Kotas <jkotas@microsoft.com>
2016-02-22 00:26:28 -0800

    Update NETStandard.Library and AppDeps package versions

--- tests/restore.sh
+++ tests/restore.sh
@@ -32,7 +32,7 @@ fi
 export CoreRT_ToolchainPkg=toolchain.${__BuildRid}.Microsoft.DotNet.ILCompiler.Development
 export CoreRT_ToolchainVer=1.0.2-prerelease-00001
 export CoreRT_AppDepSdkPkg=toolchain.${__BuildRid}.Microsoft.DotNet.AppDep
-export CoreRT_AppDepSdkVer=1.0.6-prerelease-00001
+export CoreRT_AppDepSdkVer=1.0.6-prerelease-00003
 
 __ScriptDir=$(cd "$(dirname "$0")"; pwd -P)
 __BuildStr=${CoreRT_BuildOS}.${CoreRT_BuildArch}.${CoreRT_BuildType}
