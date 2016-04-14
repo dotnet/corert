2bfd041be41503e794bb5ad8e19a266b83a3a441
Manu <manu-silicon@users.noreply.github.com>
2016-04-14 16:17:28 +0900

    Testing of Type and method lookups for generic types
    Testing various aspects of type instantiation and their relation to
    their corresponding definition.

---
+++ src/ILCompiler.TypeSystem/tests/CoreTestAssembly/GenericTypes.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace GenericTypes
+{
+    /// <summary>
+    /// Generic class to be used for testing.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GenericClass<T>
+    {
+        /// <summary>
+        /// Purpose is to manipulate a method involving a generic parameter in its return type.
+        /// </summary>
+        public virtual T Foo()
+        {
+            return default(T);
+        }
+        /// <summary>
+        /// Purpose is to manipulate a method involving a generic parameter in its parameter list.
+        /// </summary>
+        public void Bar(T a)
+        {
+        }
+    }
+
+}
