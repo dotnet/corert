5f0a8c7a88da43e849b771d523b78c51f1e3f494
Michal Strehovský <michals@microsoft.com>
2016-04-17 20:31:21 -0700

    Add IEETypeNode interface
    This is similar to `IMethodNode` in the sense that it lets us preserve
    information about the type associated with the symbol. Same as
    `IMethodNode`, there are two classes implementing this.
    
    I pulled the decision about which one to generate into the slow path (we
    no longer need to consult the compilation group in order to pick a cache
    to check).

---
+++ src/ILCompiler.Compiler/src/Compiler/DependencyAnalysis/IEETypeNode.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Internal.TypeSystem;
+
+namespace ILCompiler.DependencyAnalysis
+{
+    /// <summary>
+    /// A dependency analysis node that represents a runtime type data structure.
+    /// </summary>
+    public interface IEETypeNode : ISymbolNode
+    {
+        TypeDesc Type { get; }
+    }
+}
