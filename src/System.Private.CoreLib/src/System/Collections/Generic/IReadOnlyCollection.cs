5958ac00994b6ea250362dbea6a9c4b00964d8a2
James Ko <jamesqko@gmail.com>
2016-06-07 17:03:32 -0400

    Port dotnet/coreclr#5454 to corert (#1380)

--- src/System.Private.CoreLib/src/System/Collections/Generic/IReadOnlyCollection.cs
+++ src/System.Private.CoreLib/src/System/Collections/Generic/IReadOnlyCollection.cs
@@ -9,36 +9,8 @@ using System.Runtime.CompilerServices;
 namespace System.Collections.Generic
 {
     // Provides a read-only, covariant view of a generic list.
-
-#if CONTRACTS_FULL
-    [ContractClass(typeof(IReadOnlyCollectionContract<>))]
-#endif
     public interface IReadOnlyCollection<out T> : IEnumerable<T>
     {
         int Count { get; }
     }
-#if CONTRACTS_FULL
-    [ContractClassFor(typeof(IReadOnlyCollection<>))]
-    internal abstract class IReadOnlyCollectionContract<T> : IReadOnlyCollection<T>
-    {
-        int IReadOnlyCollection<T>.Count
-        {
-            get
-            {
-                Contract.Ensures(Contract.Result<int>() >= 0);
-                return default(int);
-            }
-        }
-
-        IEnumerator<T> IEnumerable<T>.GetEnumerator()
-        {
-            return default(IEnumerator<T>);
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return default(IEnumerator);
-        }
-    }
-#endif
 }
