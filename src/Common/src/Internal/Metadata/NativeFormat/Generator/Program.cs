1a8ea5fc8fbc453f72b09995628e5a73660b506a
Jan Kotas <jkotas@microsoft.com>
2016-06-20 00:47:45 -0700

    Rewrite native metadata generator in C#
    - SchemaDef.cs is almost direct rewrite of the Python script.
    - The actual emitters are writing the stuff directly using WriteLines. C# string interpolation makes it look pretty nice. The Python scripts used a codedom like machinery that seemed like overkill.

---
+++ src/Common/src/Internal/Metadata/NativeFormat/Generator/Program.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var writer = new PublicGen(@"NativeFormatReaderCommonGen.cs"))
+        {
+            writer.EmitSource();
+        }
+
+        using (var writer = new ReaderGen(@"NativeFormatReaderGen.cs"))
+        {
+            writer.EmitSource();
+        }
+
+        using (var writer = new WriterGen(@"..\..\..\..\..\ILCompiler.MetadataWriter\src\Internal\Metadata\NativeFormat\Writer\NativeFormatWriterGen.cs"))
+        {
+            writer.EmitSource();
+        }
+
+        using (var writer = new MdBinaryReaderGen(@"MdBinaryReaderGen.cs"))
+        {
+            writer.EmitSource();
+        }
+
+        using (var writer = new MdBinaryWriterGen(@"..\..\..\..\..\ILCompiler.MetadataWriter\src\Internal\Metadata\NativeFormat\Writer\MdBinaryWriterGen.cs"))
+        {
+            writer.EmitSource();
+        }
+    }
+}
