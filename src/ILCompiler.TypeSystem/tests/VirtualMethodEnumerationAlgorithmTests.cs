6725fc448f58c4867db8f65df0bd7e0b382062de
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2016-06-28 16:00:29 -0700

    Implement a caching version of virtual method enumeration algorithm (#1439)
    When compiling hello world, computing virtual method overrides shows up as pretty hot. Turns out the slowdown is mostly caused by the enumeration being slow. I'm introducing a caching version of the algorithm.
    
    This drops hello world compilation time from ~3.4 seconds to ~3.2 seconds (!).
    
    Some notes:
    * Caching only the methods on definitions, not on instantiated types.
    * I'm not adding cache invalidation. Since I'm not caching methods on generic instantiations, this shouldn't explode too much.
    * We might get further benefit if we change `IEnumerable<MethodDesc>` to `MethodDesc[]` in the method signature, but I'm not sure we would want to do that.

---
+++ src/ILCompiler.TypeSystem/tests/VirtualMethodEnumerationAlgorithmTests.cs
@@ -0,0 +1,68 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Internal.TypeSystem;
+
+using Xunit;
+
+
+namespace TypeSystemTests
+{
+    public class VirtualMethodEnumerationAlgorithmTests
+    {
+        TestTypeSystemContext _context;
+        ModuleDesc _testModule;
+        MetadataType _testType;
+
+        public VirtualMethodEnumerationAlgorithmTests()
+        {
+            _context = new TestTypeSystemContext(TargetArchitecture.Unknown);
+            var systemModule = _context.CreateModuleForSimpleName("CoreTestAssembly");
+            _context.SetSystemModule(systemModule);
+
+            _testModule = systemModule;
+
+            _testType = _testModule.GetType("VirtualFunctionOverride", "SimpleGeneric`1")
+                .MakeInstantiatedType(_context.GetWellKnownType(WellKnownType.Object));
+        }
+
+        [Fact]
+        public void TestMetadataEnumerationAlgorithm()
+        {
+            var algo = new MetadataVirtualMethodEnumerationAlgorithm();
+
+            DefType objectType = _context.GetWellKnownType(WellKnownType.Object);
+            var objectMethods = algo.ComputeAllVirtualMethods(objectType).ToArray();
+            Assert.Equal(4, objectMethods.Length);
+            Assert.Superset(new HashSet<string> { "Equals", "GetHashCode", "ToString", "Finalize" },
+                new HashSet<string>(objectMethods.Select(m => m.Name)));
+
+            var testTypeMethods = algo.ComputeAllVirtualMethods(_testType).ToArray();
+            Assert.Equal(1, testTypeMethods.Length);
+            Assert.Equal("ToString", testTypeMethods[0].Name);
+            Assert.Equal(_testType, testTypeMethods[0].OwningType);
+        }
+
+        [Fact]
+        public void TestCachingEnumerationAlgorithm()
+        {
+            var algo = new CachingVirtualMethodEnumerationAlgorithm();
+
+            DefType objectType = _context.GetWellKnownType(WellKnownType.Object);
+            var objectMethods = algo.ComputeAllVirtualMethods(objectType).ToArray();
+            Assert.Equal(4, objectMethods.Length);
+            Assert.Superset(new HashSet<string> { "Equals", "GetHashCode", "ToString", "Finalize" },
+                new HashSet<string>(objectMethods.Select(m => m.Name)));
+
+            var testTypeMethods = algo.ComputeAllVirtualMethods(_testType).ToArray();
+            Assert.Equal(1, testTypeMethods.Length);
+            Assert.Equal("ToString", testTypeMethods[0].Name);
+            Assert.Equal(_testType, testTypeMethods[0].OwningType);
+        }
+    }
+}
