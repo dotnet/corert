a14679921336bf53ad3d5d9a0801a1b0c76f8cac
William Kent <wjk@sunsol.me>
2016-07-12 16:33:02 -0400

    CompilationModuleGroup now scans all assemblies for NativeCallable methods
    Previously, it would ignore referenced assemblies (-r flag) except
    for System.Private.CoreLib. Note that the tests currently do not pass
    with this change applied.

--- src/ILCompiler.Compiler/src/Compiler/CompilationModuleGroup.cs
+++ src/ILCompiler.Compiler/src/Compiler/CompilationModuleGroup.cs
@@ -71,6 +71,14 @@ namespace ILCompiler
 
             AddWellKnownTypes(rootProvider);
             AddReflectionInitializationCode(rootProvider);
+            foreach (var inputFile in _typeSystemContext.ReferenceFilePaths)
+            {
+                if (_typeSystemContext.InputFilePaths.ContainsKey(inputFile.Value)) continue;
+
+                var module = _typeSystemContext.GetModuleFromPath(inputFile.Value);
+                if (module.Equals(_typeSystemContext.SystemModule)) continue;
+                AddCompilationRootsForExports(module, rootProvider);
+            }
         }
 
         private void AddWellKnownTypes(IRootingServiceProvider rootProvider)
