f5f7611cd385751342b8027553aa20cf5b203581
Michal Strehovský <michals@microsoft.com>
2016-07-13 16:59:11 -0700

    Add support for single method compilation mode
    Add a set of command line arguments that let us specify a single method
    to compile. This is useful when investigating code generation issue to
    help narrow down the focus and make it easier to set up breakpoints.
    
    Fixes #1142.

---
+++ src/ILCompiler.Compiler/src/Compiler/SingleMethodCompilationModuleGroup.cs
@@ -0,0 +1,57 @@
+﻿using Internal.TypeSystem;
+
+namespace ILCompiler
+{
+    /// <summary>
+    /// A compilation group that only contains a single method. Useful for development purposes when investigating
+    /// code generation issues.
+    /// </summary>
+    public class SingleMethodCompilationModuleGroup : CompilationModuleGroup
+    {
+        private MethodDesc _method;
+
+        public SingleMethodCompilationModuleGroup(CompilerTypeSystemContext typeSystemContext, MethodDesc method)
+            : base(typeSystemContext)
+        {
+            _method = method;
+        }
+
+        public override bool IsSingleFileCompilation
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override bool ContainsMethod(MethodDesc method)
+        {
+            return method == _method;
+        }
+
+        public override bool ContainsType(TypeDesc type)
+        {
+            return false;
+        }
+
+        public override bool ShouldProduceFullType(TypeDesc type)
+        {
+            return false;
+        }
+
+        public override bool ShouldShareAcrossModules(MethodDesc method)
+        {
+            return true;
+        }
+
+        public override bool ShouldShareAcrossModules(TypeDesc type)
+        {
+            return true;
+        }
+
+        public override void AddCompilationRoots(IRootingServiceProvider rootProvider)
+        {
+            rootProvider.AddCompilationRoot(_method, "Single method mode");
+        }
+    }
+}
