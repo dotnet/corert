30eed26b464ff99398f36dcbdaa35ddc9d490735
James Ko <jamesqko@gmail.com>
2016-08-25 21:31:59 -0400

    Port Char.ToString changes from CoreCLR

--- src/System.Private.CoreLib/src/System/String.cs
+++ src/System.Private.CoreLib/src/System/String.cs
@@ -1629,6 +1629,16 @@ namespace System
             return s;
         }
 
+        // This is only intended to be used by char.ToString.
+        // It is necessary to put the code in this class instead of Char, since _firstChar is a private member.
+        // Making _firstChar internal would be dangerous since it would make it much easier to break String's immutability.
+        internal static string CreateFromChar(char c)
+        {
+            string result = FastAllocateString(1);
+            result._firstChar = c;
+            return result;
+        }
+
         internal static String FastAllocateString(int length)
         {
             try
