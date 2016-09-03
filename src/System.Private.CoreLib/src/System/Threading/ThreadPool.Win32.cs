016b67cb721a3be230104fbf64582bae6fd7d8bc
Jan Kotas <jkotas@microsoft.com>
2016-09-03 07:13:42 -0700

    Implement ThreadPool for Win32 (#1797)

--- src/System.Private.CoreLib/src/System/Threading/ThreadPool.Win32.cs
+++ src/System.Private.CoreLib/src/System/Threading/ThreadPool.Win32.cs
@@ -2,7 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Internal.Runtime.Augments;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace System.Threading
 {
@@ -11,14 +12,57 @@ namespace System.Threading
     //
     internal static partial class ThreadPool
     {
-        internal static void QueueDispatch()
+        static IntPtr s_work;
+
+        [NativeCallable(CallingConvention = CallingConvention.StdCall)]
+        private static void DispatchCallback(IntPtr instance, IntPtr context, IntPtr work)
         {
-            throw new NotImplementedException();
+            Debug.Assert(s_work == work);
+            ThreadPoolWorkQueue.Dispatch();
         }
 
-        internal static void QueueLongRunningWork(Action callback)
+        internal static void QueueDispatch()
         {
-            throw new NotImplementedException();
+            if (s_work == IntPtr.Zero)
+            {
+                IntPtr nativeCallback = AddrofIntrinsics.AddrOf<Action<IntPtr, IntPtr, IntPtr>>(DispatchCallback);
+
+                IntPtr work = Interop.mincore.CreateThreadpoolWork(nativeCallback, IntPtr.Zero, IntPtr.Zero);
+                if (work == IntPtr.Zero)
+                    throw new OutOfMemoryException();
+
+                if (Interlocked.CompareExchange(ref s_work, work, IntPtr.Zero) != IntPtr.Zero)
+                    Interop.mincore.CloseThreadpoolWork(work);
+            }
+
+            Interop.mincore.SubmitThreadpoolWork(s_work);
+        }
+
+        [NativeCallable(CallingConvention = CallingConvention.StdCall)]
+        private static void LongRunningWorkCallback(IntPtr instance, IntPtr context)
+        {
+            GCHandle gcHandle = GCHandle.FromIntPtr(context);
+            Action callback = (Action)gcHandle.Target;
+            gcHandle.Free();
+
+            callback();
+        }
+
+        internal unsafe static void QueueLongRunningWork(Action callback)
+        {
+            var environ = default(Interop.mincore.TP_CALLBACK_ENVIRON);
+
+            environ.Initialize();
+            environ.SetLongFunction();
+
+            IntPtr nativeCallback = AddrofIntrinsics.AddrOf<Action<IntPtr, IntPtr>>(LongRunningWorkCallback);
+
+            GCHandle gcHandle = GCHandle.Alloc(callback);
+            if (!Interop.mincore.TrySubmitThreadpoolCallback(nativeCallback, GCHandle.ToIntPtr(gcHandle), &environ))
+            {
+                gcHandle.Free();
+                throw new OutOfMemoryException();
+            }
         }
     }
 }
