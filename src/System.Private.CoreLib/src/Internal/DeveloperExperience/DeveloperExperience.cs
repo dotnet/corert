f33b7cc2c9d090cece779a0a237bbb97b7a6d9d7
Tomas Rylek <trylek@microsoft.com>
2016-09-19 17:02:22 -0700

    Rollback of CS# 1628088, stack trace generator changes originally due to Cody Ohlsen with help from Andrew Au. Multiple team members expressed concerns towards current form of this change. I'll send out a new code review for this change in a day or two, after I manage to sufficiently familiarize with the change.
    Thanks
    
    Tomas
    
    [tfs-changeset: 1628158]

--- src/System.Private.CoreLib/src/Internal/DeveloperExperience/DeveloperExperience.cs
+++ src/System.Private.CoreLib/src/Internal/DeveloperExperience/DeveloperExperience.cs
@@ -2,51 +2,18 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-// #define DUMP_STACKTRACE_BLOB // uncomment this to print the entire stack trace blob's raw encoded bytes to the debug console.
-
 using System;
-using System.Collections.Generic;
+using System.Text;
 using System.Runtime;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
-using System.Text;
 
-using Internal.Runtime;
 using Internal.Runtime.Augments;
-using Internal.DeveloperExperience.StackTrace;
 
 namespace Internal.DeveloperExperience
 {
     public class DeveloperExperience
     {
-        // For every module, keep a map of StackTraceBlobIndexes for each module.
-        private static LowLevelDictionary<IntPtr, StackTraceBlobIndex> s_stackTraceBlobIndexes;
-
-        // Given an rva for a method, Walks through the binary, reading descriptors, to build up and return the method name.
-        private unsafe static string CreateStackTraceString(StackTraceBlobIndex stackTraceBlobIndex, uint rva, byte* pBlob)
-        {
-            uint methodStartRva = rva;
-            uint methodCount = *((uint*)(pBlob) + 1); // Skipping the magic byte
-            uint* pRvaTable = ((uint*)(pBlob) + 5);   // Skipping the magic byte and the table sizes
-
-            // Iterate through each rva.
-            for (uint i = 0; i < methodCount; i++)
-            {
-                if (*pRvaTable != methodStartRva)
-                {
-                    pRvaTable++;
-                }
-                else
-                {
-                    // Create the method descriptor from the offset found.
-                    StackTraceMethodDescriptor md = StackTraceMethodDescriptor.CreateFromBuffer(stackTraceBlobIndex, pBlob, stackTraceBlobIndex.methodOffsets[(int)i]);
-                    return md.ToString();
-                }
-            }
-
-            return null;
-        }
-
         public virtual void WriteLine(String s)
         {
             Debug.WriteLine(s);
@@ -55,82 +22,43 @@ namespace Internal.DeveloperExperience
 
         public virtual String CreateStackTraceString(IntPtr ip, bool includeFileInfo)
         {
-            string methodName = String.Empty;
-            IntPtr moduleBase = RuntimeImports.RhGetModuleFromPointer(ip);
-            IntPtr methodStart = RuntimeImports.RhFindMethodStartAddress(ip);
-            try
+            ReflectionExecutionDomainCallbacks reflectionCallbacks = RuntimeAugments.CallbacksIfAvailable;
+            String moduleFullFileName = null;
+
+            if (reflectionCallbacks != null)
             {
+                IntPtr methodStart = RuntimeImports.RhFindMethodStartAddress(ip);
                 if (methodStart != IntPtr.Zero)
                 {
-                    int rva = RuntimeAugments.ConvertIpToRva(methodStart);
-                    unsafe
+                    string methodName = string.Empty;
+                    try
                     {
-                        byte* pBlob = null;
-                        uint cbBlob = 0;
-                        if (RuntimeImports.RhFindBlob(moduleBase, (uint)ReflectionMapBlob.StackTraceData, &pBlob, &cbBlob) && cbBlob > 0)
-                        {
-                            StackTraceBlobIndex stackTraceBlobIndex = null;
-                            if (s_stackTraceBlobIndexes == null)
-                            {
-                                s_stackTraceBlobIndexes = new LowLevelDictionary<IntPtr, StackTraceBlobIndex>();
-                            }
-                            if (!s_stackTraceBlobIndexes.TryGetValue(moduleBase, out stackTraceBlobIndex))
-                            {
-#if DUMP_STACKTRACE_BLOB
-                                Debug.WriteLine("Entire blob:");
-                                for (int i = 0; i < cbBlob; i++)
-                                {
-                                    Debug.WriteLine(pBlob[i]);
-                                }
-#endif
-                                stackTraceBlobIndex = StackTraceBlobIndex.BuildStackTraceBlobIndex(pBlob, cbBlob);
-                                s_stackTraceBlobIndexes.Add(moduleBase, stackTraceBlobIndex);
-                            }
-
-                            methodName = CreateStackTraceString(stackTraceBlobIndex, (uint)rva, pBlob);
-                            Debug.Assert(methodName != null, "If we have the blob, we better have all the method names for every methods in the module.");
-                        }
+                        methodName = reflectionCallbacks.GetMethodNameFromStartAddressIfAvailable(methodStart);
                     }
+                    catch { }
 
-                    if (methodName == null)
-                    {
-                        // If we can't find anything, default to reflection.
-                        // This can happen if the app is not compiled with the /EnableTextualStackTrace flag
-                        ReflectionExecutionDomainCallbacks reflectionCallbacks = RuntimeAugments.CallbacksIfAvailable;
-
-                        if (reflectionCallbacks != null)
-                        {
-                            methodName = reflectionCallbacks.GetMethodNameFromStartAddressIfAvailable(methodStart);
-                        }
-                    }
-                }
-            }
-            catch 
-            {
-                // Ignoring any error occurred while trying to figure out the methodName
-            }
-
-            if (methodName != null)
-            {
-                return methodName;
-            }
-            else
-            {
-                string moduleFullFileName = RuntimeAugments.TryGetFullPathToApplicationModule(moduleBase);
-
-                // Without any callbacks or the ability to map ip correctly we better admit that we don't know
-                if (string.IsNullOrEmpty(moduleFullFileName))
-                {
-                    return "<unknown>";
+                    if (!string.IsNullOrEmpty(methodName))
+                        return methodName;
                 }
 
-                StringBuilder sb = new StringBuilder();
-                string fileNameWithoutExtension = GetFileNameWithoutExtension(moduleFullFileName);
-                sb.Append(fileNameWithoutExtension);
-                sb.Append("!<BaseAddress>+0x");
-                sb.Append(RuntimeAugments.ConvertIpToRva(ip).ToString("x"));
-                return sb.ToString();
+                // If we don't have precise information, try to map it at least back to the right module.
+                IntPtr moduleBase = RuntimeImports.RhGetModuleFromPointer(ip);
+                moduleFullFileName = RuntimeAugments.TryGetFullPathToApplicationModule(moduleBase);
             }
+
+            // Without any callbacks or the ability to map ip correctly we better admit that we don't know
+            if (string.IsNullOrEmpty(moduleFullFileName))
+            {
+                return "<unknown>";
+            }
+
+            StringBuilder sb = new StringBuilder();
+            String fileNameWithoutExtension = GetFileNameWithoutExtension(moduleFullFileName);
+            int rva = RuntimeAugments.ConvertIpToRva(ip);
+            sb.Append(fileNameWithoutExtension);
+            sb.Append("!<BaseAddress>+0x");
+            sb.Append(rva.ToString("x"));
+            return sb.ToString();
         }
 
         public virtual void TryGetSourceLineInfo(IntPtr ip, out string fileName, out int lineNumber, out int columnNumber)
@@ -177,7 +105,7 @@ namespace Internal.DeveloperExperience
         private static String GetFileName(String path)
         {
             int length = path.Length;
-            for (int i = length; --i >= 0; )
+            for (int i = length; --i >= 0;)
             {
                 char ch = path[i];
                 if (ch == '/' || ch == '\\' || ch == ':')
@@ -189,3 +117,4 @@ namespace Internal.DeveloperExperience
         private static DeveloperExperience s_developerExperience;
     }
 }
+
