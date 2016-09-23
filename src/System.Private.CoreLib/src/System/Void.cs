f8aebba10953f0835827225658fdf1766d141310
Michal Strehovský <michals@microsoft.com>
2016-09-23 16:23:11 -0700

    Fix comment on System.Void
    `typeof(void)` is a legit construct and we do have EETypes for this.

--- src/System.Private.CoreLib/src/System/Void.cs
+++ src/System.Private.CoreLib/src/System/Void.cs
@@ -2,10 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-// 
-
-// 
-
 ////////////////////////////////////////////////////////////////////////////////
 // Void
 //    This class represents the void return type
@@ -14,8 +10,6 @@
 namespace System
 {
     // This class represents the void return type
-    // For RH, this type wont be available at runtime
-    // typeof(void) would fail.
     [System.Runtime.InteropServices.ComVisible(true)]
     public struct Void
     {
