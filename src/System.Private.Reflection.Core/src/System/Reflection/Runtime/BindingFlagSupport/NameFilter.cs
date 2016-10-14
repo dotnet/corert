61b1f6449efcb2b38056065cfd8db5d60502a2de
davidwrighton <davidwr@microsoft.com>
2016-10-13 18:28:10 -0700

    Add support for Ecma string comparison to NameFilter

--- src/System.Private.Reflection.Core/src/System/Reflection/Runtime/BindingFlagSupport/NameFilter.cs
+++ src/System.Private.Reflection.Core/src/System/Reflection/Runtime/BindingFlagSupport/NameFilter.cs
@@ -2,8 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+extern alias ECMA;
+
 using System.Globalization;
-using Internal.Metadata.NativeFormat;
+using NativeFormat=Internal.Metadata.NativeFormat;
+using Ecma = ECMA::System.Reflection.Metadata;
+
 
 namespace System.Reflection.Runtime.BindingFlagSupport
 {
@@ -15,7 +19,8 @@ namespace System.Reflection.Runtime.BindingFlagSupport
         }
 
         public abstract bool Matches(string name);
-        public abstract bool Matches(ConstantStringValueHandle stringHandle, MetadataReader reader);
+        public abstract bool Matches(NativeFormat.ConstantStringValueHandle stringHandle, NativeFormat.MetadataReader reader);
+        public abstract bool Matches(Ecma.StringHandle stringHandle, Ecma.MetadataReader reader);
 
         protected string ExpectedName { get; }
     }
@@ -28,7 +33,8 @@ namespace System.Reflection.Runtime.BindingFlagSupport
         }
 
         public sealed override bool Matches(string name) => name.Equals(ExpectedName, StringComparison.Ordinal);
-        public sealed override bool Matches(ConstantStringValueHandle stringHandle, MetadataReader reader) => stringHandle.StringEquals(ExpectedName, reader);
+        public sealed override bool Matches(NativeFormat.ConstantStringValueHandle stringHandle, NativeFormat.MetadataReader reader) => stringHandle.StringEquals(ExpectedName, reader);
+        public sealed override bool Matches(Ecma.StringHandle stringHandle, Ecma.MetadataReader reader) => reader.StringComparer.Equals(stringHandle, ExpectedName, false);
     }
 
     internal sealed class NameFilterCaseInsensitive : NameFilter
@@ -39,7 +45,8 @@ namespace System.Reflection.Runtime.BindingFlagSupport
         }
 
         public sealed override bool Matches(string name) => name.Equals(ExpectedName, StringComparison.OrdinalIgnoreCase);
-        public sealed override bool Matches(ConstantStringValueHandle stringHandle, MetadataReader reader) => stringHandle.GetConstantStringValue(reader).Value.Equals(ExpectedName, StringComparison.OrdinalIgnoreCase);
+        public sealed override bool Matches(NativeFormat.ConstantStringValueHandle stringHandle, NativeFormat.MetadataReader reader) => stringHandle.GetConstantStringValue(reader).Value.Equals(ExpectedName, StringComparison.OrdinalIgnoreCase);
+        public sealed override bool Matches(Ecma.StringHandle stringHandle, Ecma.MetadataReader reader) => reader.StringComparer.Equals(stringHandle, ExpectedName, true);
     }
 }
 
