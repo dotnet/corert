4ec549796295b34def1c045df7b4a34fa3a7cc15
davidwrighton <davidwr@microsoft.com>
2016-10-25 15:43:12 -0700

    Move VirtualRuntimeParameterInfoArray to its own file

--- src/System.Private.Reflection.Core/src/System/Reflection/Runtime/MethodInfos/RuntimeMethodCommon.cs
+++ src/System.Private.Reflection.Core/src/System/Reflection/Runtime/MethodInfos/RuntimeMethodCommon.cs
@@ -17,35 +17,6 @@ using Internal.Reflection.Core.Execution;
 namespace System.Reflection.Runtime.MethodInfos
 {
     // Helper for GetRuntimeParameters() - array mimic that supports an efficient "array.Skip(1).ToArray()" operation.
-    internal struct VirtualRuntimeParameterInfoArray
-    {
-        public VirtualRuntimeParameterInfoArray(int count)
-            : this()
-        {
-            Debug.Assert(count >= 1);
-            Remainder = (count == 1) ? Array.Empty<RuntimeParameterInfo>() : new RuntimeParameterInfo[count - 1];
-        }
-
-        public RuntimeParameterInfo this[int index]
-        {
-            get
-            {
-                return index == 0 ? First : Remainder[index - 1];
-            }
-
-            set
-            {
-                if (index == 0)
-                    First = value;
-                else
-                    Remainder[index - 1] = value;
-            }
-        }
-
-        public RuntimeParameterInfo First { get; private set; }
-        public RuntimeParameterInfo[] Remainder { get; }
-    }
-
     internal interface IRuntimeMethodCommon<TRuntimeMethodCommon> where TRuntimeMethodCommon : IRuntimeMethodCommon<TRuntimeMethodCommon>, IEquatable<TRuntimeMethodCommon>
     {
         MethodAttributes Attributes { get; }
