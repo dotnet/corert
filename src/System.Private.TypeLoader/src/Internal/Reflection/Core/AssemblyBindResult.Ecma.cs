f6cab690ae38d9988de81bd7e77f2e814aab3c5b
davidwrighton <davidwr@microsoft.com>
2016-10-24 18:13:56 -0700

    Explain why AssemblyBindResult is wrapped in #pragma warning disable

--- src/System.Private.TypeLoader/src/Internal/Reflection/Core/AssemblyBindResult.Ecma.cs
+++ src/System.Private.TypeLoader/src/Internal/Reflection/Core/AssemblyBindResult.Ecma.cs
@@ -6,6 +6,9 @@ using System.Reflection.Metadata;
 
 namespace Internal.Reflection.Core
 {
+    // disable warning that indicates that the order of fields in a partial struct is not specified
+    // This warning is disabled to allow the assembly bind result to have a set of fields that differ
+    // based on which set of compilation directives is currently in use.
 #pragma warning disable CS0282
     public partial struct AssemblyBindResult
     {
