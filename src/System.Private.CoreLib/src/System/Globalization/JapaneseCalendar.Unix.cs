1c6a96b7e9ad777a463734e19766b1bd98660b13
Jan Kotas <jkotas@microsoft.com>
2016-11-03 19:44:19 -0700

    Fix Unix build breaks

---
+++ src/System.Private.CoreLib/src/System/Globalization/JapaneseCalendar.Unix.cs
@@ -0,0 +1,31 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+
+using Internal.Runtime.Augments;
+
+namespace System.Globalization
+{
+    public partial class JapaneseCalendar : Calendar
+    {
+        private static EraInfo[] GetJapaneseEras()
+        {
+            // UNIXTODO: JapaneseCalendar
+            throw new NotImplementedException();
+        }
+
+        private static string GetJapaneseEnglishEraName(int era)
+        {
+            // UNIXTODO: JapaneseCalendar
+            throw new NotImplementedException();
+        }
+
+        private static bool GetJapaneseEraInfo(int era, out DateTimeOffset dateOffset, out string eraName, out string abbreviatedEraName)
+        {
+            // UNIXTODO: JapaneseCalendar
+            throw new NotImplementedException();
+        }
+    }
+}
