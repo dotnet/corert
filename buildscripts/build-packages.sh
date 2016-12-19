9de013096cddc72f252d5387a57303680318ed86
Chris Rummel <crummel@microsoft.com>
2016-12-19 16:04:35 -0600

    Address code review feedback (PR#2325).

--- buildscripts/build-packages.sh
+++ buildscripts/build-packages.sh
@@ -6,6 +6,6 @@ if [ "$BUILDVARS_DONE" != 1 ]; then
     . $scriptRoot/buildvars-setup.sh $*
 fi
 
-$__ProjectRoot/Tools/msbuild.sh "$__sourceroot/pkg/packages.proj" /m /nologo "/flp:v=diag;LogFile=build-packages.log" /p:RepoPath="$__ProjectRoot" /p:NuPkgRid=$__NugetRuntimeId /p:OSGroup=$__BuildOS /p:Configuration=$__BuildType /p:Platform=$__BuildArch /p:COMPUTERNAME=$(hostname) /p:USERNAME=$(id -un) $__UnprocessedBuildArgs $__ExtraMsBuildArgs
+$__ProjectRoot/Tools/msbuild.sh "$__ProjectRoot/pkg/packages.proj" /m /nologo "/flp:v=diag;LogFile=build-packages.log" /p:RepoPath="$__ProjectRoot" /p:NuPkgRid=$__NugetRuntimeId /p:OSGroup=$__BuildOS /p:Configuration=$__BuildType /p:Platform=$__BuildArch /p:COMPUTERNAME=$(hostname) /p:USERNAME=$(id -un) $__UnprocessedBuildArgs $__ExtraMsBuildArgs
 export BUILDERRORLEVEL=$?
 exit $BUILDERRORLEVEL
