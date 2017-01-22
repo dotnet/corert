397e6edc34b35f202a57e746188b9a8a4836365b
Jan Kotas <jkotas@microsoft.com>
2017-01-21 18:26:07 -0800

    Change stackwalking to always use unadjusted IP
    Handling of hardware exceptions had a hack to add +1 to the actual instruction IP. Windows x64 unwinder
    is disassembling instructions at the IP passed in to detect method epilogs. If the bytes at IP + 1
    happened to match the epilog pattern, the unwind is done as if we were in the middle of the epilog that
    lead to spectacular crash.
    
    This change is moving this adjustment to be done later for EH related things only, and not interfere
    with stackwalking.
    
    Fixes #2535

--- src/Native/Runtime/PalRedhawkCommon.h
+++ src/Native/Runtime/PalRedhawkCommon.h
@@ -61,13 +61,14 @@ struct PAL_LIMITED_CONTEXT
     UIntNative GetIp() const { return IP; }
     UIntNative GetSp() const { return SP; }
     UIntNative GetFp() const { return R7; }
+    UIntNative GetLr() const { return LR; }
 #elif defined(_TARGET_ARM64_)
     // @TODO: Add ARM64 registers
     UIntNative IP;
     UIntNative GetIp() const { PORTABILITY_ASSERT("@TODO: FIXME:ARM64"); }
     UIntNative GetSp() const { PORTABILITY_ASSERT("@TODO: FIXME:ARM64"); }
     UIntNative GetFp() const { PORTABILITY_ASSERT("@TODO: FIXME:ARM64"); }
-
+    UIntNative GetLr() const { PORTABILITY_ASSERT("@TODO: FIXME:ARM64"); }
 #elif defined(UNIX_AMD64_ABI)
     // Param regs: rdi, rsi, rdx, rcx, r8, r9, scratch: rax, rdx (both return val), preserved: rbp, rbx, r12-r15
     UIntNative  IP;
