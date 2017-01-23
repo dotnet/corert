1c53c10b42d3254979e6b82421760d16332981bf
Toni Solarin-Sodara <toni.edward@outlook.com>
2017-01-23 10:26:53 +0100

    Revert change to primitive Decimal
    Keeps CoreRT consistent with CoreCLR

--- src/System.Private.CoreLib/src/System/Decimal.cs
+++ src/System.Private.CoreLib/src/System/Decimal.cs
@@ -312,7 +312,7 @@ namespace System
             if (value == null)
                 return 1;
             if (!(value is Decimal))
-                throw new ArgumentException(SR.Arg_MustBeDecimal, nameof(value));
+                throw new ArgumentException(SR.Arg_MustBeDecimal);
 
             Decimal other = (Decimal)value;
             return DecCalc.VarDecCmp(ref this, ref other);
