f75ae6108b5ea3541fa2c9cc5aaa6de138fc9da5
Fadi Hanna <fadim@microsoft.com>
2017-02-21 15:26:29 -0800

    Implementation for the missing NetSTD 2.0 ModuleHandle APIs. Most of them throwing PlatformNotSupported as discussed.
    [tfs-changeset: 1648469]

--- src/System.Private.CoreLib/src/System/ModuleHandle.cs
+++ src/System.Private.CoreLib/src/System/ModuleHandle.cs
@@ -4,12 +4,101 @@
 
 namespace System
 {
+    using System.Reflection;
+
     public struct ModuleHandle
     {
         // Public in full framework but not in .NetCore contract
         internal static readonly ModuleHandle EmptyHandle = default(ModuleHandle);
-#pragma warning disable 169
-        private IntPtr _dummy; //Needed for apireview diff noise elimination
-#pragma warning restore
+
+        private Module _ptr;
+
+        internal ModuleHandle(Module module)
+        {
+            _ptr = module;
+        }
+
+        public override int GetHashCode()
+        {
+            return _ptr != null ? _ptr.GetHashCode() : 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is ModuleHandle))
+                return false;
+
+            ModuleHandle handle = (ModuleHandle)obj;
+
+            return handle._ptr == _ptr;
+        }
+
+        public bool Equals(ModuleHandle handle)
+        {
+            return handle._ptr == _ptr;
+        }
+
+        public RuntimeFieldHandle GetRuntimeFieldHandleFromMetadataToken(int fieldToken)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public RuntimeMethodHandle GetRuntimeMethodHandleFromMetadataToken(int methodToken)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public RuntimeTypeHandle GetRuntimeTypeHandleFromMetadataToken(int typeToken)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public int MDStreamVersion
+        {
+            get
+            {
+                throw new PlatformNotSupportedException();
+            }
+        }
+
+        public static bool operator ==(ModuleHandle left, ModuleHandle right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ModuleHandle left, ModuleHandle right)
+        {
+            return !left.Equals(right);
+        }
+
+        public RuntimeFieldHandle ResolveFieldHandle(int fieldToken)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public RuntimeFieldHandle ResolveFieldHandle(int fieldToken, RuntimeTypeHandle[] typeInstantiationContext, RuntimeTypeHandle[] methodInstantiationContext)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public RuntimeMethodHandle ResolveMethodHandle(int methodToken)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public RuntimeMethodHandle ResolveMethodHandle(int methodToken, RuntimeTypeHandle[] typeInstantiationContext, RuntimeTypeHandle[] methodInstantiationContext)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public RuntimeTypeHandle ResolveTypeHandle(int typeToken)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public RuntimeTypeHandle ResolveTypeHandle(int typeToken, RuntimeTypeHandle[] typeInstantiationContext, RuntimeTypeHandle[] methodInstantiationContext)
+        {
+            throw new PlatformNotSupportedException();
+        }
     }
 }
