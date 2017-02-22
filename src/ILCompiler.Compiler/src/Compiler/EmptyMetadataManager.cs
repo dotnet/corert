a167cfc6144ff693ebbf124f49a4954e8a65502a
David Wrighton <davidwr@microsoft.com>
2017-02-21 17:16:56 -0800

    Disable use of CompilerGeneratedMetadataManager on for Utc non-metadata based builds - Now that the metadata infrastructure is more completely attached, using the CompilerGeneratedMetadataManager on our internal non-metadata suite does not work - Change to using EmptyMetadataManager which simply doesn't generate any metadata info
    [tfs-changeset: 1648485]

---
+++ src/ILCompiler.Compiler/src/Compiler/EmptyMetadataManager.cs
@@ -0,0 +1,53 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+
+using Internal.TypeSystem;
+
+namespace ILCompiler
+{
+    class EmptyMetadataManager : MetadataManager
+    {
+        public EmptyMetadataManager(CompilationModuleGroup group, CompilerTypeSystemContext typeSystemContext) : base(group, typeSystemContext)
+        {
+        }
+
+        public override IEnumerable<ModuleDesc> GetCompilationModulesWithMetadata()
+        {
+            return Array.Empty<ModuleDesc>();
+        }
+
+        public override bool IsReflectionBlocked(MetadataType type)
+        {
+            return true;
+        }
+
+        protected override void ComputeMetadata(out byte[] metadataBlob, out List<MetadataMapping<MetadataType>> typeMappings, out List<MetadataMapping<MethodDesc>> methodMappings, out List<MetadataMapping<FieldDesc>> fieldMappings)
+        {
+            metadataBlob = Array.Empty<byte>();
+
+            typeMappings = new List<MetadataMapping<MetadataType>>();
+            methodMappings = new List<MetadataMapping<MethodDesc>>();
+            fieldMappings = new List<MetadataMapping<FieldDesc>>();
+        }
+
+        /// <summary>
+        /// Is there a reflection invoke stub for a method that is invokable?
+        /// </summary>
+        public override bool HasReflectionInvokeStubForInvokableMethod(MethodDesc method)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Gets a stub that can be used to reflection-invoke a method with a given signature.
+        /// </summary>
+        public override MethodDesc GetReflectionInvokeStub(MethodDesc method)
+        {
+            return null;
+        }
+    }
+}
