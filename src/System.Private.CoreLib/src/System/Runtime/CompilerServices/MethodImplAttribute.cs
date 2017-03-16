34265084bb7fb82eb681155a5cb3a78714ecadd2
Fadi Hanna <fadim@microsoft.com>
2017-03-16 16:10:28 -0700

    CompilerServices APIs
    [tfs-changeset: 1651141]

--- src/System.Private.CoreLib/src/System/Runtime/CompilerServices/MethodImplAttribute.cs
+++ src/System.Private.CoreLib/src/System/Runtime/CompilerServices/MethodImplAttribute.cs
@@ -6,32 +6,26 @@ using System;
 
 namespace System.Runtime.CompilerServices
 {
-    // This Enum matchs the miImpl flags defined in corhdr.h. It is used to specify 
-    // certain method properties.
-    [Flags]
-    public enum MethodImplOptions
-    {
-        Unmanaged = 0x0004,
-        NoInlining = 0x0008,
-        ForwardRef = 0x0010,
-        Synchronized = 0x0020,
-        NoOptimization = 0x0040,
-        PreserveSig = 0x0080,
-        AggressiveInlining = 0x0100,
-        InternalCall = 0x1000
-    }
-
     // Custom attribute to specify additional method properties.
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor, Inherited = false)]
     sealed public class MethodImplAttribute : Attribute
     {
-        internal MethodImplOptions _val;
+        public MethodCodeType MethodCodeType;
 
         public MethodImplAttribute(MethodImplOptions methodImplOptions)
         {
-            _val = methodImplOptions;
+            Value = methodImplOptions;
         }
 
-        public MethodImplOptions Value { get { return _val; } }
+        public MethodImplAttribute(short value)
+        {
+            Value = (MethodImplOptions)value;
+        }
+
+        public MethodImplAttribute()
+        {
+        }
+
+        public MethodImplOptions Value { get; }
     }
 }
