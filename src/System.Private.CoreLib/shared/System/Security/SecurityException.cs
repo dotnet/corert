02e58524fac9d1c2d384bf8f5bd094ae6885fa04
Atsushi Kanamori <atsushik@microsoft.com>
2017-03-20 14:01:10 -0700

    NS2.0 - Complete System.Security namespace.
    - Add GetObjectData() implementation to System.SecurityExcepton
    
    - Clean up a bit and move to shared partition.

--- src/System.Private.CoreLib/shared/System/Security/SecurityException.cs
+++ src/System.Private.CoreLib/shared/System/Security/SecurityException.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Reflection;
 using System.Runtime.Serialization;
 
 namespace System.Security
@@ -15,26 +16,26 @@ namespace System.Security
             HResult = __HResults.COR_E_SECURITY;
         }
 
-        public SecurityException(String message)
+        public SecurityException(string message)
             : base(message)
         {
             HResult = __HResults.COR_E_SECURITY;
         }
 
-        public SecurityException(String message, Exception inner)
+        public SecurityException(string message, Exception inner)
             : base(message, inner)
         {
             HResult = __HResults.COR_E_SECURITY;
         }
 
-        public SecurityException(String message, Type type)
+        public SecurityException(string message, Type type)
             : base(message)
         {
             HResult = __HResults.COR_E_SECURITY;
             PermissionType = type;
         }
 
-        public SecurityException(string message, System.Type type, string state)
+        public SecurityException(string message, Type type, string state)
             : base(message)
         {
             HResult = __HResults.COR_E_SECURITY;
@@ -47,18 +48,17 @@ namespace System.Security
         {
         }
 
-        public override String ToString()
-        {
-            return base.ToString();
-        }
+        public override string ToString() => base.ToString();
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context) => base.GetObjectData(info, context);
 
         public object Demanded { get; set; }
         public object DenySetInstance { get; set; }
-        public System.Reflection.AssemblyName FailedAssemblyInfo { get; set; }
+        public AssemblyName FailedAssemblyInfo { get; set; }
         public string GrantedSet { get; set; }
-        public System.Reflection.MethodInfo Method { get; set; }
+        public MethodInfo Method { get; set; }
         public string PermissionState { get; set; }
-        public System.Type PermissionType { get; set; }
+        public Type PermissionType { get; set; }
         public object PermitOnlySetInstance { get; set; }
         public string RefusedSet { get; set; }
         public string Url { get; set; }
