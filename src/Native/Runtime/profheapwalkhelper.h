0311802c7f44097f3094529d2399af7f0652addf
Sean Gillespie <sean@swgillespie.me>
2017-03-28 09:59:04 -0700

    Use standard C++ types (bool) and consistent types (void*) on the interface (#10463)
    * [Local GC] BOOL -> bool on IGCHeap
    
    * [Local GC] size_t -> void* on IGCHeap
    
    * [Local GC] Silence warnings by being explicit about BOOl -> bool conversions
    
    * Address code review feedback: FinalizeAppDomain BOOL -> bool
    
    * Fix warnings
    
    * Address code review feedback:
      1) Fix a missed default parameter (FALSE) on a parameter of type bool,
      2) Fix invocations of the diagnostic callbacks to use boolean literals
         instead of TRUE and FALSE,
      3) Fix various invocations of GC interface methods in the VM to use
         boolean literals instead of TRUE and FALSE
    
    * Address code review feedback: fix inconsistency

--- src/Native/Runtime/profheapwalkhelper.h
+++ src/Native/Runtime/profheapwalkhelper.h
@@ -10,7 +10,7 @@
 // or a profiler. The implementations of these two functions are in profheapwalkhelper.cpp.
 #if defined(FEATURE_EVENT_TRACE) || defined(GC_PROFILING)
 void ScanRootsHelper(Object* pObj, Object** ppRoot, ScanContext* pSC, DWORD dwFlags);
-BOOL HeapWalkHelper(Object* pBO, void* pvContext);
+bool HeapWalkHelper(Object* pBO, void* pvContext);
 #endif
 
 
