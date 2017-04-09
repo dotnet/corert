47685a021f36938108d438402e6af6d4da86be0a
WinCPP <mandards@gmail.com>
2017-04-09 12:08:24 +0530

    CoreFx #15622 New overload Dictionary.Remove(K, out V)

--- src/System.Private.CoreLib/src/System/Collections/Generic/Dictionary.cs
+++ src/System.Private.CoreLib/src/System/Collections/Generic/Dictionary.cs
@@ -575,6 +575,49 @@ namespace System.Collections.Generic
             return false;
         }
 
+        public bool Remove(TKey key, out TValue value)
+        {
+            if (key == null)
+            {
+                value = default(TValue);
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (buckets != null)
+            {
+                int hashCode = comparer.GetHashCode(key) & 0x7FFFFFFF;
+                int bucket = hashCode % buckets.Length;
+                int last = -1;
+                for (int i = buckets[bucket]; i >= 0; last = i, i = entries[i].next)
+                {
+                    if (entries[i].hashCode == hashCode && comparer.Equals(entries[i].key, key))
+                    {
+                        if (last < 0)
+                        {
+                            buckets[bucket] = entries[i].next;
+                        }
+                        else
+                        {
+                            entries[last].next = entries[i].next;
+                        }
+
+                        value = entries[i].value;
+
+                        entries[i].hashCode = -1;
+                        entries[i].next = freeList;
+                        entries[i].key = default(TKey);
+                        entries[i].value = default(TValue);
+                        freeList = i;
+                        freeCount++;
+                        version++;
+                        return true;
+                    }
+                }
+            }
+            value = default(TValue);
+            return false;
+        }
+
         public bool TryGetValue(TKey key, out TValue value)
         {
             int i = FindEntry(key);
