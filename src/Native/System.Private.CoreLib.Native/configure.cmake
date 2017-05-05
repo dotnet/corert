15d22515d2d778538122472dd9df8237777423ba
Michal Strehovský <michals@microsoft.com>
2017-05-04 22:11:07 -0700

    Implement Guid.NewGuid on Unix

--- src/Native/System.Private.CoreLib.Native/configure.cmake
+++ src/Native/System.Private.CoreLib.Native/configure.cmake
@@ -4,6 +4,8 @@ include(CheckLibraryExists)
 
 check_library_exists(pthread pthread_condattr_setclock "" HAVE_PTHREAD_CONDATTR_SETCLOCK)
 
+check_include_files(uuid/uuid.h HAVE_LIBUUID_H)
+
 check_cxx_source_runs("
 #include <stdlib.h>
 #include <time.h>
@@ -59,6 +61,20 @@ int main(void)
   exit(1);
 }" HAVE_SCHED_GETCPU)
 set(CMAKE_REQUIRED_LIBRARIES)
+
+if(CMAKE_SYSTEM_NAME STREQUAL Darwin)
+  if(NOT HAVE_LIBUUID_H) 
+    unset(HAVE_LIBUUID_H CACHE)
+    message(FATAL_ERROR "Cannot find libuuid. Try installing uuid-dev or the appropriate packages for your platform") 
+  endif()
+elseif(CMAKE_SYSTEM_NAME STREQUAL FreeBSD)
+  # Put FreeBSD dependencies here
+else() # anything else is Linux
+  if(NOT HAVE_LIBUUID_H) 
+    unset(HAVE_LIBUUID_H CACHE)
+    message(FATAL_ERROR "Cannot find libuuid. Try installing uuid-dev or the appropriate packages for your platform") 
+  endif()
+endif()
 
 configure_file(
     ${CMAKE_CURRENT_SOURCE_DIR}/config.h.in
--- src/System.Private.CoreLib/src/System/Guid.cs
+++ src/System.Private.CoreLib/src/System/Guid.cs
@@ -10,7 +10,7 @@ namespace System
 {
     // Represents a Globally Unique Identifier.
     [StructLayout(LayoutKind.Sequential)]
-    public struct Guid : IFormattable, IComparable, IComparable<Guid>, IEquatable<Guid>
+    public partial struct Guid : IFormattable, IComparable, IComparable<Guid>, IEquatable<Guid>
     {
         public static readonly Guid Empty = new Guid();
         ////////////////////////////////////////////////////////////////////////////////
@@ -1271,29 +1271,6 @@ namespace System
         public static bool operator !=(Guid a, Guid b)
         {
             return !(a == b);
-        }
-
-        // This will create a new guid.  Since we've now decided that constructors should 0-init,
-        // we need a method that allows users to create a guid.
-        public static Guid NewGuid()
-        {
-            // CoCreateGuid should never return Guid.Empty, since it attempts to maintain some
-            // uniqueness guarantees.  It should also never return a known GUID, but it's unclear
-            // how extensively it checks for known values.
-            Contract.Ensures(Contract.Result<Guid>() != Guid.Empty);
-
-            Guid g;
-            int hr = Interop.mincore.CoCreateGuid(out g);
-            // We don't expect that this will ever throw an error, none are even documented, and so we don't want to pull 
-            // in the HR to ComException mappings into the core library just for this so we will try a generic exception if 
-            // we ever hit this condition.
-            if (hr != 0)
-            {
-                Exception ex = new Exception();
-                ex.SetErrorCode(hr);
-                throw ex;
-            }
-            return g;
         }
 
         public String ToString(String format)
