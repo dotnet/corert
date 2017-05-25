15c8618abe1df004f96457daab6ce53674220ec1
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2017-05-24 21:04:40 -0700

    Review feedback

--- src/Common/src/System/Runtime/CompilerServices/__BlockReflectionAttribute.cs
+++ src/Common/src/System/Runtime/CompilerServices/__BlockReflectionAttribute.cs
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 /*
   Providing a definition for __BlockReflectionAttribute in an assembly is a signal to the .NET Native toolchain 
   to remove the metadata for all non-public APIs. This both reduces size and disables private reflection on those 
