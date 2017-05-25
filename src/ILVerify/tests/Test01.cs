82aca3a69f7039509f2c44e5df64b952c449caf2
Gergely Kalapos <gergo@kalapos.net>
2017-05-26 00:42:45 +0200

    WIP: Changed .csproj to the new format, added xunit test project. Added one .il file with a simple test.

---
+++ src/ILVerify/tests/Test01.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Internal.IL;
+using Internal.TypeSystem.Ecma;
+using Xunit;
+
+namespace ILVerify.Tests
+{
+    
+    public class Test01
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            var _typeSystemContext = new SimpleTypeSystemContext();
+            _typeSystemContext.InputFilePaths = new Dictionary<String, String> { { "CoreTestAssembly",
+                @"C:\Users\gergo\Source\Repos\gregkalaposcorert\bin\Product\Windows_NT.x64.Debug\CoreTestAssembly\CoreTestAssembly.dll" } };
+            _typeSystemContext.SetSystemModule(_typeSystemContext.GetModuleForSimpleName("CoreTestAssembly"));
+
+
+            var module = _typeSystemContext.GetModuleFromPath(@"C:\Users\gergo\Source\Repos\gregkalaposcorert\src\ILVerify\tests\ILTests\IlVerifyTestAssembly.dll");
+            
+            foreach (var methodHandle in module.MetadataReader.MethodDefinitions)
+            {
+                var method = (EcmaMethod)module.GetMethod(methodHandle);
+
+                var methodIL = EcmaMethodIL.Create(method);
+                if (methodIL == null)
+                    continue;
+
+                var importer = new ILImporter(method, methodIL);
+                importer.Verify();
+            }
+            
+
+            ILVerify.VerifierError vv = new ILVerify.VerifierError();
+            vv = VerifierError.CallAbstract;
+            
+            Assert.Equal(VerifierError.CallAbstract, vv);
+        }
+
+
+
+    }
+}
