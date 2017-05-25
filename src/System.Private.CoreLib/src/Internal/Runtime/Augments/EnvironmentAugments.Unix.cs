e2192561f23d1b98627e00403eb75516342da75d
Atsushi Kanamori <AtsushiKan@users.noreply.github.com>
2017-05-25 14:21:16 -0700

    Fix 28 System.Runtime.Extensions test failures. (#3707)
    SetEnvironmentVariable(string, string)
      GetEnvironmentVariables()
    
      GetEnvironmentVariable(string, target)
      SetEnvironmentVariable(string, string, target)
      GetEnvironmentVariables(target)
    
       Where target = Target.Process.
    
       Target.Machine/User will come up next...
    
    Code is mostly cribbed from CoreCLR with some
    minor call tree reorg with an eye towards
    optimizing out the copies upon copies in
    the GetEnvironmentStrings() path.
    
    Code is written with an eye towards
    moving to shared partition (though I don't
    have time budgeted for that myself.)
    
    Am not doing the Unix versions on CoreRT.
    Focus is on getting tests on Project N up.

--- src/System.Private.CoreLib/src/Internal/Runtime/Augments/EnvironmentAugments.Unix.cs
+++ src/System.Private.CoreLib/src/Internal/Runtime/Augments/EnvironmentAugments.Unix.cs
@@ -3,36 +3,34 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Text;
-using System.Threading;
 using System.Collections;
-using System.Runtime;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
 
 namespace Internal.Runtime.Augments
 {
     /// <summary>For internal use only.  Exposes runtime functionality to the Environments implementation in corefx.</summary>
     public static partial class EnvironmentAugments
     {
-        public static string GetEnvironmentVariable(string variable)
+        private static string GetEnvironmentVariableCore(string variable)
         {
-            if (variable == null)
-                throw new ArgumentNullException(nameof(variable));
-
+            Debug.Assert(variable != null);
             return Marshal.PtrToStringAnsi(Interop.Sys.GetEnv(variable));
         }
 
-        public static IDictionary GetEnvironmentVariables()
+        private static void SetEnvironmentVariableCore(string variable, string value)
         {
-            if ("".Length != 0)
-                throw new NotImplementedException(); // TODO: https://github.com/dotnet/corert/issues/3688 Need to implement GetEnvironmentVariables() properly.
-            return new LowLevelListDictionary();
+            Debug.Assert(variable != null);
+            throw new NotImplementedException();
         }
 
-        public static IDictionary GetEnvironmentVariables(EnvironmentVariableTarget target) { throw new NotImplementedException(); }
-        public static string GetEnvironmentVariable(string variable, EnvironmentVariableTarget target) { throw new NotImplementedException(); }
-        public static void SetEnvironmentVariable(string variable, string value) { throw new NotImplementedException(); }
-        public static void SetEnvironmentVariable(string variable, string value, EnvironmentVariableTarget target) { throw new NotImplementedException(); }
+        public static IEnumerable<KeyValuePair<string,string>> EnumerateEnvironmentVariables()
+        {
+            if ("".Length != 0)
+                throw new NotImplementedException(); // Need to return something better than an empty environment block.
+
+            return Array.Empty<KeyValuePair<string,string>>();
+        }
     }
 }
