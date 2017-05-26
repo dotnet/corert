e46a4a4e452790e13c7f28c1be56cfb827991837
Michal Strehovský <michals@microsoft.com>
2017-05-26 08:51:17 -0700

    WIP

--- src/ILCompiler/repro/Program.cs
+++ src/ILCompiler/repro/Program.cs
@@ -3,11 +3,46 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 internal class Program
 {
+    public struct Struct
+    {
+        public int X;
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void SetX(int x)
+        {
+            X = x;
+        }
+    }
+
+    public delegate void StructSetter(ref Struct s, int value);
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello world");
+        if (String.Empty.Length > 0)
+        {
+            Struct st = default(Struct);
+            st.SetX(123);
+        }
+
+        var mi = typeof(Struct).GetTypeInfo().GetMethod("SetX");
+        Console.WriteLine(mi);
+
+        Struct s = default(Struct);
+        var f = (StructSetter)mi.CreateDelegate(typeof(StructSetter));
+        f(ref s, 1234);
+        Console.WriteLine(s.X);
+
+        if (String.Empty.Length > 0)
+        {
+            // Make sure we generate metadata for the Invoke method
+            StructSetter s2 = f.Invoke;
+            s2(ref s, 555);
+        }
     }
 }
