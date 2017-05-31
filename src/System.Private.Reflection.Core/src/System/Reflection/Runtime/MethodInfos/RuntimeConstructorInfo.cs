e8b2a27df901ef446c893a5bdef1a1e2d844920b
Atsushi Kanamori <atsushik@microsoft.com>
2017-05-31 09:06:48 -0700

    Remove [Serializable] and ISerializable from internal Reflection types
    The presence of [Serializable] was making these internal types
    reflectable. Internal types do not need to implement ISerializable
    on their own - that's for reflection providers that choose
    to make their implementations serializable. We do not.

--- src/System.Private.Reflection.Core/src/System/Reflection/Runtime/MethodInfos/RuntimeConstructorInfo.cs
+++ src/System.Private.Reflection.Core/src/System/Reflection/Runtime/MethodInfos/RuntimeConstructorInfo.cs
@@ -7,7 +7,6 @@ using System.Reflection;
 using System.Diagnostics;
 using System.Globalization;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 using System.Reflection.Runtime.General;
 using System.Reflection.Runtime.TypeInfos;
 using System.Reflection.Runtime.ParameterInfos;
@@ -21,8 +20,7 @@ namespace System.Reflection.Runtime.MethodInfos
     //
     // The runtime's implementation of ConstructorInfo.
     //
-    [Serializable]
-    internal abstract partial class RuntimeConstructorInfo : ConstructorInfo, ISerializable
+    internal abstract partial class RuntimeConstructorInfo : ConstructorInfo
     {
         public abstract override MethodAttributes Attributes { get; }
 
@@ -47,11 +45,6 @@ namespace System.Reflection.Runtime.MethodInfos
         }
 
         public sealed override MethodBody GetMethodBody()
-        {
-            throw new PlatformNotSupportedException();
-        }
-
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             throw new PlatformNotSupportedException();
         }
