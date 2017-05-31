c76104acf6d3e2174acb5a23725ab97e2c84a7fe
Anton Lapounov <antonl@microsoft.com>
2017-05-30 17:30:02 -0700

    Implement ThreadPool.RegisterWaitForSingleObject using Windows thread pool API.
    CR: SergeyK, KouVel
    
    The _ThreadPoolWaitOrTimerCallback class is taken from CoreCLR with minimal changes to simplify potential code unification.  The code in ThreadPool.Windows.cs is written from scratch and expected to properly handle race conditions between wait callbacks, Unregister calls, and the finalizer.  For Unix, keep the old quick-and-dirty RegisterWaitForSingleObject implementation until we properly implement the thread pool.
    
    In CoreCLR the [Unsafe]RegisterWaitForSingleObject overloads taking a TimeSpan timeout have the following argument verification logic:
    
        long tm = (long)timeout.TotalMilliseconds;
        if (tm < -1)
            throw new ArgumentOutOfRangeException(nameof(timeout), SR.ArgumentOutOfRange_NeedNonNegOrNegative1);
        if (tm > (long)Int32.MaxValue)
            throw new ArgumentOutOfRangeException(nameof(timeout), SR.ArgumentOutOfRange_LessEqualToIntegerMaxVal);
    
    where
        ArgumentOutOfRange_NeedNonNegOrNegative1 = Number must be either non-negative and less than or equal to Int32.MaxValue or -1.
        ArgumentOutOfRange_LessEqualToIntegerMaxVal = Argument must be less than or equal to 2^31 - 1 milliseconds.
    
    The later error message is a particular case of the former one and it is used in just these two places in CoreCLR.  In CoreRT I decided to reuse WaitHandle.ToTimeoutMilliseconds(), which reports the former error message for both error cases.  That unifies error reporting across CoreLib methods taking a TimeSpan timeout.
    
    Also note that the [Unsafe]RegisterWaitForSingleObject overloads taking a long timeout make unchecked truncation of that timeout to UInt32.  I kept that behavior for compatibility with CoreCLR.
    
    [tfs-changeset: 1660100]

--- src/Common/src/Interop/Windows/mincore/Interop.ThreadPool.cs
+++ src/Common/src/Interop/Windows/mincore/Interop.ThreadPool.cs
@@ -19,5 +19,19 @@ internal static partial class Interop
 
         [DllImport("api-ms-win-core-threadpool-l1-2-0.dll")]
         internal extern static void CloseThreadpoolWork(IntPtr pwk);
+
+        internal delegate void WaitCallback(IntPtr Instance, IntPtr Context, IntPtr Wait, uint WaitResult);
+
+        [DllImport("api-ms-win-core-threadpool-l1-2-0.dll")]
+        internal extern static IntPtr CreateThreadpoolWait(IntPtr pfnwa, IntPtr pv, IntPtr pcbe);
+
+        [DllImport("api-ms-win-core-threadpool-l1-2-0.dll")]
+        internal extern static void SetThreadpoolWait(IntPtr pwa, IntPtr h, IntPtr pftTimeout);
+
+        [DllImport("api-ms-win-core-threadpool-l1-2-0.dll")]
+        internal extern static void WaitForThreadpoolWaitCallbacks(IntPtr pwa, bool fCancelPendingCallbacks);
+
+        [DllImport("api-ms-win-core-threadpool-l1-2-0.dll")]
+        internal extern static void CloseThreadpoolWait(IntPtr pwa);
     }
 }
