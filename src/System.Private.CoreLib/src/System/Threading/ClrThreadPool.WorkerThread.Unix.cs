f182ce9812da2a754b1391cdb189aa284b6be31e
t-jekor <t-jekor@microsoft.com>
2017-06-05 16:03:14 -0700

    Added thread pool worker thread code (ported over from CoreCLR).

---
+++ src/System.Private.CoreLib/src/System/Threading/ClrThreadPool.WorkerThread.Unix.cs
@@ -0,0 +1,140 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Internal.Runtime.Augments;
+
+namespace System.Threading
+{
+    internal static partial class ClrThreadPool
+    {
+        internal static class WorkerThread
+        {
+            internal static WaitSubsystem.WaitableObject s_semaphore = WaitSubsystem.WaitableObject.NewSemaphore(0, int.MaxValue);
+
+            private const int TimeoutMs = 20 * 1000;
+
+
+            private static IntPtr WorkerThreadStart(IntPtr context)
+            {
+                RuntimeThread.InitializeThreadPoolThread();
+                // Worker Thread Start event
+
+                while (true)
+                {
+                    // Worker thread wait event
+                    while (WaitSubsystem.Wait(s_semaphore, TimeoutMs, false, true))
+                    {
+                        if(!ThreadPoolWorkQueue.Dispatch())
+                        {
+                            return IntPtr.Zero;
+                        }
+
+                        RuntimeThread.CurrentThread.Priority = ThreadPriority.Normal;
+                        CultureInfo.CurrentCulture = CultureInfo.InstalledUICulture;
+                        CultureInfo.CurrentUICulture = CultureInfo.InstalledUICulture;
+                    }
+                    s_threadAdjustmentLock.Acquire();
+                    ThreadCounts counts = ThreadCounts.VolatileReadCounts(ref s_counts);
+                    while (true)
+                    {
+                        if (counts.numActive == counts.numWorking)
+                        {
+                            s_threadAdjustmentLock.Release();
+                            break;
+                        }
+
+                        ThreadCounts newCounts = counts;
+                        newCounts.numActive--;
+                        newCounts.maxWorking = (short)Math.Max(s_minThreads, Math.Min(newCounts.numActive, newCounts.maxWorking));
+                        ThreadCounts oldCounts = ThreadCounts.CompareExchangeCounts(ref s_counts, newCounts, counts);
+                        if (oldCounts.asLong == counts.asLong)
+                        {
+                            s_threadAdjustmentLock.Release();
+                            s_threadPoolHillClimber.ForceChange(newCounts.maxWorking, HillClimbing.State.ThreadTimedOut);
+                            // Worker Thread stop event
+                            return IntPtr.Zero;
+                        }
+                        counts = oldCounts;
+                    } 
+                }
+            }
+
+            internal static void MaybeAddWorkingWorker()
+            {
+                ThreadCounts counts = ThreadCounts.VolatileReadCounts(ref s_counts);
+                ThreadCounts newCounts;
+                while (true)
+                {
+                    newCounts = counts;
+                    newCounts.numWorking = Math.Max(counts.numWorking, Math.Min((short)(counts.numWorking + 1), counts.maxWorking));
+                    newCounts.numActive = Math.Max(counts.numActive, newCounts.numWorking);
+                    
+                    if(newCounts.asLong == counts.asLong)
+                    {
+                        return;
+                    }
+
+                    ThreadCounts oldCounts = ThreadCounts.CompareExchangeCounts(ref s_counts, newCounts, counts);
+
+                    if(oldCounts.asLong == counts.asLong)
+                    {
+                        break;
+                    }
+
+                    counts = oldCounts;
+                }
+
+                short toCreate = (short)(newCounts.numActive - counts.numActive);
+                int toRelease = newCounts.numWorking - counts.numWorking;
+
+                if(toRelease > 0)
+                {
+                    WaitSubsystem.ReleaseSemaphore(s_semaphore, toRelease);
+                }
+
+                while(toCreate > 0)
+                {
+                    if(CreateWorkerThread())
+                    {
+                        toCreate--;
+                    }
+                    else
+                    {
+                        //
+                        // Uh-oh, we promised to create a new thread, but the creation failed.  We have to renege on our
+                        // promise.  This may possibly result in no work getting done for a while, but the gate thread will
+                        // eventually notice that no completions are happening and force the creation of a new thread.
+                        // Of course, there's no guarantee *that* will work - but hopefully enough time will have passed
+                        // to allow whoever's using all the memory right now to release some.
+                        //
+
+                        counts = ThreadCounts.VolatileReadCounts(ref s_counts);
+                        while(true)
+                        {
+                            newCounts = counts;
+                            newCounts.numWorking -= toCreate;
+                            newCounts.numActive -= toCreate;
+
+                            ThreadCounts oldCounts = ThreadCounts.CompareExchangeCounts(ref s_counts, newCounts, counts);
+
+                            if (oldCounts.asLong == counts.asLong)
+                            {
+                                break;
+                            }
+
+                            counts = oldCounts;
+                        }
+
+                        toCreate = 0;
+                    }
+                }
+            }
+
+            private static bool CreateWorkerThread()
+            {
+                return Interop.Sys.RuntimeThread_CreateThread(IntPtr.Zero /*use default stack size*/,
+                    AddrofIntrinsics.AddrOf<Interop.Sys.ThreadProc>(WorkerThreadStart), IntPtr.Zero);
+            }
+        }
+    }
+}
--- src/System.Private.CoreLib/src/System/Threading/ClrThreadPool.cs
+++ src/System.Private.CoreLib/src/System/Threading/ClrThreadPool.cs
@@ -12,6 +12,61 @@ namespace System.Threading
         private static int s_minThreads; // TODO: Initialize
         private static int s_maxThreads; // TODO: Initialize
 
+        [StructLayout(LayoutKind.Explicit)]
+        struct ThreadCounts
+        {
+            [FieldOffset(0)]
+            public short maxWorking;
+            [FieldOffset(2)]
+            public short numActive;
+            [FieldOffset(4)]
+            public short numWorking;
+            [FieldOffset(0)]
+            public long asLong;
+
+            public static ThreadCounts VolatileReadCounts(ref ThreadCounts counts)
+            {
+                return new ThreadCounts
+                {
+                    asLong = Volatile.Read(ref counts.asLong)
+                };
+            }
+
+            public static ThreadCounts CompareExchangeCounts(ref ThreadCounts location, ThreadCounts newCounts, ThreadCounts oldCounts)
+            {
+                ThreadCounts result = new ThreadCounts
+                {
+                    asLong = Interlocked.CompareExchange(ref location.asLong, newCounts.asLong, oldCounts.asLong)
+                };
+
+                if (result.asLong == oldCounts.asLong)
+                {
+                    ValidateCounts(result);
+                    ValidateCounts(newCounts);
+                }
+                return result;
+            }
+
+            private static void ValidateCounts(ThreadCounts counts)
+            {
+                Debug.Assert(counts.maxWorking > 0);
+                Debug.Assert(counts.numActive >= 0);
+                Debug.Assert(counts.numWorking >= 0);
+                Debug.Assert(counts.numWorking <= counts.numActive);
+            }
+        }
+
+
+        private static readonly LowLevelLock s_threadAdjustmentLock = new LowLevelLock();
+        private static ThreadCounts s_counts = new ThreadCounts();
+        private static int s_lastDequeueTime;
+        private static int s_priorCompletedWorkRequestsTime;
+        private static int s_nextCompletedWorkRequestsTime;
+        private static long s_currentSampleStartTime;
+        private static int s_priorCompletionCount = 0;
+        private static int s_completionCount = 0;
+        private static int s_threadAdjustmentInterval;
+
         // TODO: SetMinThreads and SetMaxThreads need to be synchronized with a lock
         // TODO: Compare with CoreCLR implementation and ensure this has the same guarantees.
         public static bool SetMinThreads(int threads)
@@ -43,5 +98,107 @@ namespace System.Threading
         }
 
         public static int GetMaxThreads() => s_maxThreads;
+
+        internal static bool NotifyWorkItemComplete()
+        {
+            Interlocked.Increment(ref s_completionCount);
+            Volatile.Write(ref s_lastDequeueTime, Environment.TickCount);
+
+            bool shouldAdjustWorkers = ShouldAdjustMaxWorkersActive();
+            if(shouldAdjustWorkers && s_threadAdjustmentLock.TryAcquire())
+            {
+                AdjustMaxWorkersActive();
+                s_threadAdjustmentLock.Release();
+            }
+            return ShouldWorkerKeepRunning();
+        }
+
+        private static bool ShouldWorkerKeepRunning()
+        {
+            ThreadCounts counts = ThreadCounts.VolatileReadCounts(ref s_counts);
+            while(true)
+            {
+                if(counts.numActive <= counts.numWorking)
+                {
+                    return true;
+                }
+
+                ThreadCounts newCounts = counts;
+                newCounts.numActive--;
+                newCounts.numWorking--;
+
+                ThreadCounts oldCounts = ThreadCounts.CompareExchangeCounts(ref s_counts, newCounts, counts);
+
+                if(oldCounts.asLong == counts.asLong)
+                {
+                    return false;
+                }
+                counts = oldCounts;
+            }
+        }
+
+        private static void AdjustMaxWorkersActive()
+        {
+            int currentTicks = Environment.TickCount;
+            int totalNumCompletions = Volatile.Read(ref s_completionCount);
+            int numCompletions = totalNumCompletions - Volatile.Read(ref s_priorCompletionCount);
+            long startTime = s_currentSampleStartTime;
+            long endTime = 0; // TODO: PAL High Performance Counter
+            long freq = 0;
+
+            double elapsed = (double)(endTime - startTime) / freq;
+
+            if(elapsed * 1000 >= s_threadAdjustmentInterval / 2)
+            {
+                ThreadCounts currentCounts = ThreadCounts.VolatileReadCounts(ref s_counts);
+                int newMax;
+                (newMax, s_threadAdjustmentInterval) = s_threadPoolHillClimber.Update(currentCounts.maxWorking, elapsed, numCompletions);
+
+                while(newMax != currentCounts.maxWorking)
+                {
+                    ThreadCounts newCounts = currentCounts;
+                    newCounts.maxWorking = (short)newMax;
+
+                    ThreadCounts oldCounts = ThreadCounts.CompareExchangeCounts(ref s_counts, newCounts, currentCounts);
+                    if (oldCounts.asLong == currentCounts.asLong)
+                    {
+                        if(newMax > oldCounts.maxWorking)
+                        {
+                            WorkerThread.MaybeAddWorkingWorker();
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        if(oldCounts.maxWorking > currentCounts.maxWorking && oldCounts.maxWorking >= newMax)
+                        {
+                            // someone (probably the gate thread) increased the thread count more than
+                            // we are about to do.  Don't interfere.
+                            break;
+                        }
+
+                        currentCounts = oldCounts;
+                    }
+                }
+                s_priorCompletionCount = totalNumCompletions;
+                Volatile.Write(ref s_nextCompletedWorkRequestsTime, currentTicks + s_threadAdjustmentInterval);
+                s_priorCompletedWorkRequestsTime = currentTicks;
+                s_currentSampleStartTime = endTime;
+            }
+        }
+
+        private static bool ShouldAdjustMaxWorkersActive()
+        {
+            // We need to subtract by prior time because Environment.TickCount can wrap around, making a comparison of absolute times unreliable.
+            int priorTime = s_priorCompletedWorkRequestsTime;
+            int requiredInterval = Volatile.Read(ref s_nextCompletedWorkRequestsTime) - priorTime;
+            int elapsedInterval = Environment.TickCount - priorTime;
+            if(elapsedInterval >= requiredInterval)
+            {
+                ThreadCounts counts = ThreadCounts.VolatileReadCounts(ref s_counts);
+                return counts.numActive >= counts.maxWorking;
+            }
+            return false;
+        }
     }
 }
