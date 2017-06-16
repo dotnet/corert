ff37b7e7dacff75582f6517157755140bc09c3a8
Jeremy Koritzinsky <t-jekor@microsoft.com>
2017-06-16 12:37:29 -0700

    Remove CLSCompliant attributes

--- src/System.Private.CoreLib/src/System/HighPerformanceCounter.Unix.cs
+++ src/System.Private.CoreLib/src/System/HighPerformanceCounter.Unix.cs
@@ -6,12 +6,10 @@ namespace System
 {
     internal static class HighPerformanceCounter
     {
-        [CLSCompliant(false)]
         public static ulong TickCount => Interop.Sys.GetHighPrecisionCount();
 
         // Cache the frequency on the managed side to avoid the cost of P/Invoke on every access to Frequency
         private static ulong s_frequency;
-        [CLSCompliant(false)]
         public static ulong Frequency => s_frequency == 0 ? s_frequency = Interop.Sys.GetHighPrecisionCounterFrequency() : s_frequency;
     }
 }
