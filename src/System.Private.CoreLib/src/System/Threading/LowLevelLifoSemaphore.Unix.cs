55ee6f275aeeec95f11c13fd07b86f680ad93654
t-jekor <t-jekor@microsoft.com>
2017-06-16 09:59:51 -0700

    Move LowLevelLifoSemaphore to System.Threading instead of System.Threading.ClrThreadPool

---
+++ src/System.Private.CoreLib/src/System/Threading/LowLevelLifoSemaphore.Unix.cs
@@ -0,0 +1,30 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Threading
+{
+    /// <summary>
+    /// A LIFO semaphore.
+    /// Waits on this semaphore are uninterruptible.
+    /// </summary>
+    public class LowLevelLifoSemaphore
+    {
+        private WaitSubsystem.WaitableObject _semaphore;
+
+        public LowLevelLifoSemaphore(int initialSignalCount, int maximumSignalCount)
+        {
+            _semaphore = WaitSubsystem.WaitableObject.NewSemaphore(initialSignalCount, maximumSignalCount);
+        }
+
+        public bool Wait(int timeoutMs)
+        {
+            return WaitSubsystem.Wait(_semaphore, timeoutMs, false, true);
+        }
+
+        public int Release(int count)
+        {
+            return WaitSubsystem.ReleaseSemaphore(_semaphore, count);
+        }
+    }
+}
