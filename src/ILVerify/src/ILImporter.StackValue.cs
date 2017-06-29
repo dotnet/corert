7c5dce9410027f95088b6d25facc0a7c79a0666b
Michael Mayr <mayr_michael@gmx.at>
2017-06-29 16:21:29 +0200

    Removed extra newline

--- src/ILVerify/src/ILImporter.StackValue.cs
+++ src/ILVerify/src/ILImporter.StackValue.cs
@@ -362,8 +362,7 @@ namespace Internal.IL
                         case StackValueKind.ObjRef:
                             return op == ILOpcode.beq || op == ILOpcode.beq_s ||
                                    op == ILOpcode.bne_un || op == ILOpcode.bne_un_s ||
-                                   op == ILOpcode.ceq || 
-                                   op == ILOpcode.cgt_un;
+                                   op == ILOpcode.ceq || op == ILOpcode.cgt_un;
                         default:
                             return false;
                     }
