7573eb072f8556cd4600beadd5251217a11f274c
Michael Mayr <mayr_michael@gmx.at>
2017-07-04 10:06:53 +0200

    Fixed typo

--- src/ILVerify/src/TypeDescExtensions.cs
+++ src/ILVerify/src/TypeDescExtensions.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Internal.TypeSystem
 {
-    public static class TypeDescExtensiosn
+    public static class TypeDescExtensions
     {
         public static TypeDesc ResolveSignatureVariable(this TypeDesc t, MethodDesc method)
         {
