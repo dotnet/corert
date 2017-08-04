6953f03fdd6621db40a63f545aa2d8c2f798dc9d
Petr Bred <bredpetr@gmail.com>
2017-08-04 15:33:58 +0300

    ARM: helpers emitting initial version (#4260)
    Signed-off-by: Petr Bred <bredpetr@gmail.com>

--- src/ILCompiler.Compiler/src/Compiler/DependencyAnalysis/Target_ARM/ARMEmitter.cs
+++ src/ILCompiler.Compiler/src/Compiler/DependencyAnalysis/Target_ARM/ARMEmitter.cs
@@ -18,10 +18,39 @@ namespace ILCompiler.DependencyAnalysis.ARM
         public ObjectDataBuilder Builder;
         public TargetRegisterMap TargetRegister;
 
+        // mov reg, immediate
+        // reg range: 0-7
+        public void EmitMOV(Register reg, byte immediate)
+        {
+            Builder.EmitShort((short)(0x2000 + ((byte)reg << 8) + immediate));
+        }
+
+        // cmp reg, immediate
+        // reg range: 0-7
+        public void EmitCMP(Register reg, byte immediate)
+        {
+            Builder.EmitShort((short)(0x2800 + ((byte)reg << 8) + immediate));
+        }
+
         // add reg, immediate
+        // reg range: 0-7
         public void EmitADD(Register reg, byte immediate)
         {
             Builder.EmitShort((short)(0x3000 + ((byte)reg << 8) + immediate));
+        }
+
+        // sub reg, immediate
+        // reg range: 0-7
+        public void EmitSUB(Register reg, byte immediate)
+        {
+            Builder.EmitShort((short)(0x3800 + ((byte)reg << 8) + immediate));
+        }
+
+        // nop
+        public void EmitNOP()
+        {
+            Builder.EmitByte(0x00);
+            Builder.EmitByte(0xbf);
         }
 
         // push reg
@@ -32,10 +61,41 @@ namespace ILCompiler.DependencyAnalysis.ARM
             Builder.EmitShort((short)(0x0d04 + ((byte)reg << 12)));
         }
 
+        // pop reg
+        public void EmitPOP(Register reg)
+        {
+            Builder.EmitByte(0x5d);
+            Builder.EmitByte(0xf8);
+            Builder.EmitShort((short)(0x0b04 + ((byte)reg << 12)));
+        }
+
         // mov reg, reg
         public void EmitMOV(Register destination, Register source)
         {
             Builder.EmitShort((short)(0x4600 + (((byte)destination & 0x8) << 4) + (((byte)source & 0x8) << 3) + (((byte)source & 0x7) << 3) + ((byte)destination & 0x7)));
+        }
+
+        // ldr reg, [reg]
+        // reg range: 0-7
+        public void EmitLDR(Register destination, Register source)
+        {
+            Builder.EmitShort((short)(0x6800 + (((byte)source & 0x7) << 3) + ((byte)destination & 0x7)));
+        }
+
+        // ldr.w reg, [reg, #offset]
+        // offset range: [-255..4095]
+        public void EmitLDR(Register destination, Register source, short offset)
+        {
+            if (offset >= 0)
+            {
+                Builder.EmitShort((short)(0xf8d0 + ((byte)(source))));
+                Builder.EmitShort((short)(offset + (((byte)destination) << 12)));
+            }
+            else
+            {
+                Builder.EmitShort((short)(0xf850 + ((byte)(source))));
+                Builder.EmitShort((short)(-offset + (((byte)destination) << 12) + (((byte)12) << 8)));
+            }
         }
 
         // mov   reg, [reloc] & 0x0000FFFF
@@ -63,7 +123,55 @@ namespace ILCompiler.DependencyAnalysis.ARM
         // bx reg
         public void EmitJMP(Register destination)
         {
-            Builder.EmitShort((short)(0x47 | ((byte)destination << 3)));
+            Builder.EmitShort((short)(0x4700 + ((byte)destination << 3)));
+        }
+
+        // bx lr
+        public void EmitRET()
+        {
+            EmitJMP(TargetRegister.LR);
+        }
+
+        // bne #offset
+        // offset range: [-256..254]
+        public void EmitBNE(short immediate)
+        {
+            // considering the pipeline with PC
+            immediate -= 4;
+
+            Builder.EmitByte((byte)(immediate >> 1));
+            Builder.EmitByte(0xD1);
+        }
+
+        // beq #offset
+        // offset range: [-256..254]
+        public void EmitBEQ(short immediate)
+        {
+            // considering the pipeline with PC
+            immediate -= 4;
+
+            Builder.EmitByte((byte)(immediate >> 1));
+            Builder.EmitByte(0xD0);
+        }
+
+        // bne label(+4): ret(2) + next(2)
+        // bx lr
+        // label: ...
+        public void EmitRETIfEqual()
+        {
+            EmitBNE(4);
+            EmitRET();
+        }
+
+        // bne label(+8): pop(4) + ret(2) + next(2)
+        // pop {reg}
+        // bx lr
+        // label: ...
+        public void EmitRETIfEqualPOP(Register reg)
+        {
+            EmitBNE(8);
+            EmitPOP(reg);
+            EmitRET();
         }
     }
 }
--- src/ILCompiler.Compiler/src/Compiler/DependencyAnalysis/Target_ARM/ARMReadyToRunGenericHelperNode.cs
+++ src/ILCompiler.Compiler/src/Compiler/DependencyAnalysis/Target_ARM/ARMReadyToRunGenericHelperNode.cs
@@ -14,9 +14,278 @@ namespace ILCompiler.DependencyAnalysis
 {
     partial class ReadyToRunGenericHelperNode
     {
+        protected Register GetContextRegister(ref /* readonly */ ARMEmitter encoder)
+        {
+            if (_id == ReadyToRunHelperId.DelegateCtor)
+                return encoder.TargetRegister.Arg2;
+            else
+                return encoder.TargetRegister.Arg0;
+        }
+
+        protected void EmitDictionaryLookup(NodeFactory factory, ref ARMEmitter encoder, Register context, Register result, GenericLookupResult lookup, bool relocsOnly)
+        {
+            // INVARIANT: must not trash context register
+
+            // Find the generic dictionary slot
+            int dictionarySlot = 0;
+            if (!relocsOnly)
+            {
+                // The concrete slot won't be known until we're emitting data - don't ask for it in relocsOnly.
+                dictionarySlot = factory.GenericDictionaryLayout(_dictionaryOwner).GetSlotForEntry(lookup);
+            }
+
+            // Load the generic dictionary cell
+            encoder.EmitLDR(result, context, ((short)(dictionarySlot * factory.Target.PointerSize)));
+
+            switch (lookup.LookupResultReferenceType(factory))
+            {
+                case GenericLookupResultReferenceType.Indirect:
+                    // Do another indirection
+                    encoder.EmitLDR(result, result);
+                    break;
+
+                case GenericLookupResultReferenceType.ConditionalIndirect:
+                    // Test result, 0x1
+                    // JEQ L1
+                    // mov result, [result-1]
+                    // L1:
+                    throw new NotImplementedException();
+
+                default:
+                    break;
+            }
+        }
+
         protected sealed override void EmitCode(NodeFactory factory, ref ARMEmitter encoder, bool relocsOnly)
         {
-            ARMDebug.EmitNYIAssert(factory, ref encoder, "EmitCode is not implemented");
+            // First load the generic context into the context register.
+            EmitLoadGenericContext(factory, ref encoder, relocsOnly);
+
+            Register contextRegister = GetContextRegister(ref encoder);
+
+            switch (_id)
+            {
+                case ReadyToRunHelperId.GetNonGCStaticBase:
+                    {
+                        ARMDebug.EmitNYIAssert(factory, ref encoder, "GetNonGCStaticBase EmitCode is not implemented");
+
+                        /*
+                       ***
+                       NOT TESTED!!!
+                       ***
+                        Debug.Assert(contextRegister == encoder.TargetRegister.Arg0);
+
+                        MetadataType target = (MetadataType)_target;
+
+                        if (!factory.TypeSystemContext.HasLazyStaticConstructor(target))
+                        {
+                            EmitDictionaryLookup(factory, ref encoder, encoder.TargetRegister.Arg0, encoder.TargetRegister.Result, _lookupSignature, relocsOnly);
+                            encoder.EmitRET();
+                        }
+                        else
+                        {
+                            EmitDictionaryLookup(factory, ref encoder, encoder.TargetRegister.Arg0, encoder.TargetRegister.Arg0, _lookupSignature, relocsOnly);
+                            encoder.EmitMOV(encoder.TargetRegister.Result, encoder.TargetRegister.Arg0);
+
+                            // We need to trigger the cctor before returning the base. It is stored at the beginning of the non-GC statics region.
+                            int cctorContextSize = NonGCStaticsNode.GetClassConstructorContextStorageSize(factory.Target, target);
+                            encoder.EmitSUB(encoder.TargetRegister.Arg0, ((byte)(cctorContextSize)));
+                            // If we get the required state it will be necessary to restore the original value before returning
+                            encoder.EmitPUSH(encoder.TargetRegister.Arg0);
+
+                            // cmp [r0 + ptrSize], 1
+                            encoder.EmitLDR(encoder.TargetRegister.Arg0, encoder.TargetRegister.Arg0, ((short)factory.Target.PointerSize));
+                            encoder.EmitCMP(encoder.TargetRegister.Arg0, ((byte)1));
+                            // return with restoring register
+                            encoder.EmitRETIfEqualPOP(encoder.TargetRegister.Arg0);
+
+                            // just restore after cmp
+                            encoder.EmitPOP(encoder.TargetRegister.Arg0);
+
+                            encoder.EmitLDR(encoder.TargetRegister.Arg0, encoder.TargetRegister.Arg0);
+                            encoder.EmitMOV(encoder.TargetRegister.Arg1, encoder.TargetRegister.Result);
+                            encoder.EmitJMP(factory.HelperEntrypoint(HelperEntrypoint.EnsureClassConstructorRunAndReturnNonGCStaticBase));
+                        }
+                        */
+                    }
+                    break;
+
+                case ReadyToRunHelperId.GetGCStaticBase:
+                    {
+                        ARMDebug.EmitNYIAssert(factory, ref encoder, "GetGCStaticBase EmitCode is not implemented");
+                        /*
+                       ***
+                       NOT TESTED!!!
+                       ***
+                        Debug.Assert(contextRegister == encoder.TargetRegister.Arg0);
+
+                        MetadataType target = (MetadataType)_target;
+
+                        EmitDictionaryLookup(factory, ref encoder, encoder.TargetRegister.Arg0, encoder.TargetRegister.Result, _lookupSignature, relocsOnly);
+
+                        encoder.EmitLDR(encoder.TargetRegister.Result, encoder.TargetRegister.Result);
+                        encoder.EmitLDR(encoder.TargetRegister.Result, encoder.TargetRegister.Result);
+
+                        if (!factory.TypeSystemContext.HasLazyStaticConstructor(target))
+                        {
+                            encoder.EmitRET();
+                        }
+                        else
+                        {
+                            // We need to trigger the cctor before returning the base. It is stored at the beginning of the non-GC statics region.
+                            GenericLookupResult nonGcRegionLookup = factory.GenericLookup.TypeNonGCStaticBase(target);
+                            EmitDictionaryLookup(factory, ref encoder, encoder.TargetRegister.Arg0, encoder.TargetRegister.Arg0, nonGcRegionLookup, relocsOnly);
+
+                            int cctorContextSize = NonGCStaticsNode.GetClassConstructorContextStorageSize(factory.Target, target);
+                            encoder.EmitSUB(encoder.TargetRegister.Arg0, ((byte)(cctorContextSize)));
+                            // If we get the required state it will be necessary to restore the original value before returning
+                            encoder.EmitPUSH(encoder.TargetRegister.Arg0);
+
+                            // cmp [r0 + ptrSize], 1
+                            encoder.EmitLDR(encoder.TargetRegister.Arg0, encoder.TargetRegister.Arg0, ((short)factory.Target.PointerSize));
+                            encoder.EmitCMP(encoder.TargetRegister.Arg0, ((byte)1));
+                            // return with restoring register
+                            encoder.EmitRETIfEqualPOP(encoder.TargetRegister.Arg0);
+
+                            // just restore after cmp
+                            encoder.EmitPOP(encoder.TargetRegister.Arg0);
+
+                            encoder.EmitMOV(encoder.TargetRegister.Arg1, encoder.TargetRegister.Result);
+                            encoder.EmitLDR(encoder.TargetRegister.Arg0, encoder.TargetRegister.Arg0);
+                            encoder.EmitJMP(factory.HelperEntrypoint(HelperEntrypoint.EnsureClassConstructorRunAndReturnGCStaticBase));
+                        }
+                        */
+                    }
+                    break;
+
+                case ReadyToRunHelperId.GetThreadStaticBase:
+                    {
+                        ARMDebug.EmitNYIAssert(factory, ref encoder, "GetThreadStaticBase EmitCode is not implemented");
+                        /*
+                       ***
+                       NOT TESTED!!!
+                       ***
+                        Debug.Assert(contextRegister == encoder.TargetRegister.Arg0);
+
+                        MetadataType target = (MetadataType)_target;
+
+                        // Look up the index cell
+                        EmitDictionaryLookup(factory, ref encoder, encoder.TargetRegister.Arg0, encoder.TargetRegister.Arg1, _lookupSignature, relocsOnly);
+
+                        ISymbolNode helperEntrypoint;
+                        if (factory.TypeSystemContext.HasLazyStaticConstructor(target))
+                        {
+                            // There is a lazy class constructor. We need the non-GC static base because that's where the
+                            // class constructor context lives.
+                            GenericLookupResult nonGcRegionLookup = factory.GenericLookup.TypeNonGCStaticBase(target);
+                            EmitDictionaryLookup(factory, ref encoder, encoder.TargetRegister.Arg0, encoder.TargetRegister.Arg2, nonGcRegionLookup, relocsOnly);
+                            int cctorContextSize = NonGCStaticsNode.GetClassConstructorContextStorageSize(factory.Target, target);
+                            encoder.EmitSUB(encoder.TargetRegister.Arg2, ((byte)(cctorContextSize)));
+                            encoder.EmitLDR(encoder.TargetRegister.Arg2, encoder.TargetRegister.Arg2);
+
+                            helperEntrypoint = factory.HelperEntrypoint(HelperEntrypoint.EnsureClassConstructorRunAndReturnThreadStaticBase);
+                        }
+                        else
+                        {
+                            helperEntrypoint = factory.HelperEntrypoint(HelperEntrypoint.GetThreadStaticBaseForType);
+                        }
+
+                        // First arg: address of the TypeManager slot that provides the helper with
+                        // information about module index and the type manager instance (which is used
+                        // for initialization on first access).
+                        encoder.EmitLDR(encoder.TargetRegister.Arg0, encoder.TargetRegister.Arg1);
+
+                        // Second arg: index of the type in the ThreadStatic section of the modules
+                        encoder.EmitLDR(encoder.TargetRegister.Arg1, encoder.TargetRegister.Arg1, ((short)(factory.Target.PointerSize)));
+
+                        encoder.EmitJMP(helperEntrypoint);
+                        */
+                    }
+                    break;
+
+                case ReadyToRunHelperId.DelegateCtor:
+                    {
+                        ARMDebug.EmitNYIAssert(factory, ref encoder, "DelegateCtor EmitCode is not implemented");
+                        /*
+                       ***
+                       NOT TESTED!!!
+                       ***
+                        // This is a weird helper. Codegen populated Arg0 and Arg1 with the values that the constructor
+                        // method expects. Codegen also passed us the generic context in Arg2.
+                        // We now need to load the delegate target method into Arg2 (using a dictionary lookup)
+                        // and the optional 4th parameter, and call the ctor.
+
+                        Debug.Assert(contextRegister == encoder.TargetRegister.Arg2);
+
+                        var target = (DelegateCreationInfo)_target;
+
+                        EmitDictionaryLookup(factory, ref encoder, encoder.TargetRegister.Arg2, encoder.TargetRegister.Arg2, _lookupSignature, relocsOnly);
+
+                        if (target.Thunk != null)
+                        {
+                            Debug.Assert(target.Constructor.Method.Signature.Length == 3);
+                            encoder.EmitMOV(encoder.TargetRegister.Arg3, target.Thunk);
+                        }
+                        else
+                        {
+                            Debug.Assert(target.Constructor.Method.Signature.Length == 2);
+                        }
+
+                        encoder.EmitJMP(target.Constructor);
+                        */
+                    }
+                    break;
+
+                // These are all simple: just get the thing from the dictionary and we're done
+                case ReadyToRunHelperId.TypeHandle:
+                case ReadyToRunHelperId.MethodHandle:
+                case ReadyToRunHelperId.FieldHandle:
+                case ReadyToRunHelperId.MethodDictionary:
+                case ReadyToRunHelperId.MethodEntry:
+                case ReadyToRunHelperId.VirtualDispatchCell:
+                case ReadyToRunHelperId.DefaultConstructor:
+                    {
+                        ARMDebug.EmitNYIAssert(factory, ref encoder, "general EmitCode is not implemented");
+                        /*
+                       ***
+                       NOT TESTED!!!
+                       ***
+                        EmitDictionaryLookup(factory, ref encoder, contextRegister, encoder.TargetRegister.Result, _lookupSignature, relocsOnly);
+                        encoder.EmitRET();
+                        */
+                    }
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        protected virtual void EmitLoadGenericContext(NodeFactory factory, ref ARMEmitter encoder, bool relocsOnly)
+        {
+            // Assume generic context is already loaded in the context register.
+        }
+    }
+
+    partial class ReadyToRunGenericLookupFromTypeNode
+    {
+        protected override void EmitLoadGenericContext(NodeFactory factory, ref ARMEmitter encoder, bool relocsOnly)
+        {
+            // We start with context register pointing to the EEType
+            Register contextRegister = GetContextRegister(ref encoder);
+
+            // Locate the VTable slot that points to the dictionary
+            int vtableSlot = 0;
+            if (!relocsOnly)
+            {
+                // The concrete slot won't be known until we're emitting data - don't ask for it in relocsOnly.
+                vtableSlot = VirtualMethodSlotHelper.GetGenericDictionarySlot(factory, (TypeDesc)_dictionaryOwner);
+            }
+
+            int pointerSize = factory.Target.PointerSize;
+            int slotOffset = EETypeNode.GetVTableOffset(pointerSize) + (vtableSlot * pointerSize);
+
+            // Load the dictionary pointer from the VTable
+            encoder.EmitLDR(contextRegister, contextRegister, ((short)(slotOffset)));
         }
     }
 }
