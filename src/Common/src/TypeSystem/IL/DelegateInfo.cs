b97a4eee704529924f910f30809b2f692200228e
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2017-10-17 22:10:19 +0200

    Do not generate object array thunk for byref-like signatures (#4741)
    The object array thunk can't be generated for delegates with signatures that have byref-like types in them.

--- src/Common/src/TypeSystem/IL/DelegateInfo.cs
+++ src/Common/src/TypeSystem/IL/DelegateInfo.cs
@@ -109,12 +109,34 @@ namespace Internal.IL
             _closedStaticThunk = new DelegateInvokeClosedStaticThunk(owningDelegate);
             _invokeThunk = new DelegateDynamicInvokeThunk(owningDelegate);
             _closedInstanceOverGeneric = new DelegateInvokeInstanceClosedOverGenericMethodThunk(owningDelegate);
-            _invokeObjectArrayThunk = new DelegateInvokeObjectArrayThunk(owningDelegate);
 
-            if (!owningDelegate.Type.HasInstantiation && IsNativeCallingConventionCompatible(owningDelegate.Signature))
-                _reversePInvokeThunk = new DelegateReversePInvokeThunk(owningDelegate);
-
+            // Methods that have a byref-like type in the signature cannot be invoked with the object array thunk.
+            // We would need to box the parameter and these can't be boxed.
             MethodSignature delegateSignature = owningDelegate.Signature;
+            bool generateObjectArrayThunk = true;
+            for (int i = 0; i < delegateSignature.Length; i++)
+            {
+                TypeDesc paramType = delegateSignature[i];
+                if (paramType.IsByRef)
+                    paramType = ((ByRefType)paramType).ParameterType;
+                if (!paramType.IsSignatureVariable && paramType.IsByRefLike)
+                {
+                    generateObjectArrayThunk = false;
+                    break;
+                }
+            }
+            TypeDesc normalizedReturnType = delegateSignature.ReturnType;
+            if (normalizedReturnType.IsByRef)
+                normalizedReturnType = ((ByRefType)normalizedReturnType).ParameterType;
+            if (!normalizedReturnType.IsSignatureVariable && normalizedReturnType.IsByRefLike)
+                generateObjectArrayThunk = false;
+
+            if (generateObjectArrayThunk)
+                _invokeObjectArrayThunk = new DelegateInvokeObjectArrayThunk(owningDelegate);
+
+            if (!owningDelegate.Type.HasInstantiation && IsNativeCallingConventionCompatible(delegateSignature))
+                _reversePInvokeThunk = new DelegateReversePInvokeThunk(owningDelegate);
+            
             if (delegateSignature.Length > 0)
             {
                 TypeDesc firstParam = delegateSignature[0];
