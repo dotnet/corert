8b3f216cd92e8c628abd565275cee16ba14f51a6
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2017-10-23 12:58:15 +0100

    revert redundant changes to LibraryRootProvider (#1285)

--- src/ILCompiler.Compiler/src/Compiler/LibraryRootProvider.cs
+++ src/ILCompiler.Compiler/src/Compiler/LibraryRootProvider.cs
@@ -2,11 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
-
 using Internal.TypeSystem.Ecma;
 using Internal.TypeSystem;
-using Internal.IL.Stubs.StartupCode;
 
 namespace ILCompiler
 {
