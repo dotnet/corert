c900b642847058f9ca4be81b453d25e6935c5f28
Andy Ayers <andya@microsoft.com>
2017-10-24 14:39:29 -0700

    Update code in AwaitUnsafeOnCompleted to use interface matching
    Given that the jit can now avoid boxing on some interface calls to value types,
    generalize the patterns introduced in AwaitUnsafeOnCompleted in #14718 by
    using interfaces instead of checking for specific types.
    
    Also move the catch-all processing back in line as the jit can now fold the
    interface tests early and not pull in the EH portion of the method unless it is
    needed.

--- src/System.Private.CoreLib/shared/System/Runtime/CompilerServices/ValueTaskAwaiter.cs
+++ src/System.Private.CoreLib/shared/System/Runtime/CompilerServices/ValueTaskAwaiter.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace System.Runtime.CompilerServices
 {
     /// <summary>Provides an awaiter for a <see cref="ValueTask{TResult}"/>.</summary>
-    public struct ValueTaskAwaiter<TResult> : ICriticalNotifyCompletion
+    public struct ValueTaskAwaiter<TResult> : ICriticalNotifyCompletion, IValueTaskAwaiter
     {
         /// <summary>The value being awaited.</summary>
         private ValueTask<TResult> _value; // Methods are called on this; avoid making it readonly so as to avoid unnecessary copies
