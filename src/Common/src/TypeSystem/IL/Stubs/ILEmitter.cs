d90546c2cad3e60766bbae7f0e66ac25e6eca183
Michal Strehovský <michals@microsoft.com>
2017-12-23 10:59:21 +0100

    Add algorithm to compute the MaxStack value
    This serves two purposes:
    
    - Compute MaxStack for generated IL. We were estimating the MaxStack by reporting the number of bytes of IL but this was inhibiting inlining in RyuJIT
    - Add code to validate stack balance for IL we generate. This lets us catch issues before they reach codegen. This should help productivity because it lets us avoid having to either build a checked RyuJIT to troubleshoot this, or debug the IL manually on paper.

--- src/Common/src/TypeSystem/IL/Stubs/ILEmitter.cs
+++ src/Common/src/TypeSystem/IL/Stubs/ILEmitter.cs
@@ -473,12 +473,16 @@ namespace Internal.IL.Stubs
         private readonly MethodDesc _method;
         private readonly MethodDebugInformation _debugInformation;
 
+        private const int MaxStackNotSet = -1;
+        private int _maxStack;
+
         public ILStubMethodIL(MethodDesc owningMethod, byte[] ilBytes, LocalVariableDefinition[] locals, Object[] tokens, MethodDebugInformation debugInfo = null)
         {
             _ilBytes = ilBytes;
             _locals = locals;
             _tokens = tokens;
             _method = owningMethod;
+            _maxStack = MaxStackNotSet;
 
             if (debugInfo == null)
                 debugInfo = MethodDebugInformation.None;
@@ -492,6 +496,7 @@ namespace Internal.IL.Stubs
             _tokens = methodIL._tokens;
             _method = methodIL._method;
             _debugInformation = methodIL._debugInformation;
+            _maxStack = methodIL._maxStack;
         }
 
         public override MethodDesc OwningMethod
@@ -516,8 +521,9 @@ namespace Internal.IL.Stubs
         {
             get
             {
-                // Conservative estimate...
-                return _ilBytes.Length;
+                if (_maxStack == MaxStackNotSet)
+                    _maxStack = this.ComputeMaxStack();
+                return _maxStack;
             }
         }
 
@@ -686,7 +692,9 @@ namespace Internal.IL.Stubs
                 debugInfo = new EmittedMethodDebugInformation(sequencePoints);
             }
 
-            return new ILStubMethodIL(owningMethod, ilInstructions, _locals.ToArray(), _tokens.ToArray(), debugInfo);
+            var result = new ILStubMethodIL(owningMethod, ilInstructions, _locals.ToArray(), _tokens.ToArray(), debugInfo);
+            result.CheckStackBalance();
+            return result;
         }
 
         private class EmittedMethodDebugInformation : MethodDebugInformation
