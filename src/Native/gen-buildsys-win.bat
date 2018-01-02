dc742c9791daba16f5567812476e1f6d71350cb4
Jan Kotas <jkotas@microsoft.com>
2018-01-01 18:43:47 -0800

    Cleanup and simplify makefiles
    - Delete redundant or obsolete parts in makefiles
    - Update macOS prerequisites

--- src/Native/gen-buildsys-win.bat
+++ src/Native/gen-buildsys-win.bat
@@ -9,16 +9,12 @@ if NOT %argC%==4 GOTO :USAGE
 if %1=="/?" GOTO :USAGE
 
 setlocal
-set basePath=%~dp0
-:: remove quotes
-set "basePath=%basePath:"=%"
-:: remove trailing slash
-if %basePath:~-1%==\ set "basePath=%basePath:~0,-1%"
 
-set __VSString=12 2013
-if /i "%2" == "vs2015" (set __VSString=14 2015)
-if /i "%2" == "vs2017" (set __VSString=15 2017)
-if /i "%3" == "x64" (set __VSString=%__VSString% Win64)
+set __CmakeGenerator=Visual Studio 15 2017
+if /i "%2" == "vs2017" (set __CmakeGenerator=Visual Studio 15 2017)
+if /i "%3" == "x64" (set __CmakeGenerator=%__CmakeGenerator% Win64)
+if /i "%3" == "arm64" (set __CmakeGenerator=%__CmakeGenerator% Win64)
+if /i "%3" == "arm" (set __CmakeGenerator=%__CmakeGenerator% ARM)
 
 if defined CMakePath goto DoGen
 
@@ -29,7 +25,7 @@ for /f "delims=" %%a in ('powershell -NoProfile -ExecutionPolicy ByPass "& %~dp0
 if "%3" == "wasm" (
   emcmake "%CMakePath%" "-DEMSCRIPTEN_GENERATE_BITCODE_STATIC_LIBRARIES=1" "-DCMAKE_TOOLCHAIN_FILE=%EMSCRIPTEN%/cmake/Modules/Platform/Emscripten.cmake" "-DCMAKE_BUILD_TYPE=%4" -G "NMake Makefiles" %1
 ) else (
-  "%CMakePath%" "-DCLR_CMAKE_TARGET_ARCH=%3" -G "Visual Studio %__VSString%" %1
+  "%CMakePath%" "-DCLR_CMAKE_TARGET_ARCH=%3" -G "%__CmakeGenerator%" %1
 )
 endlocal
 GOTO :DONE
@@ -38,15 +34,9 @@ GOTO :DONE
   echo "Usage..."
   echo "gen-buildsys-win.bat <path to top level CMakeLists.txt> <VSVersion>"
   echo "Specify the path to the top level CMake file - <corert>/src/Native"
-  echo "Specify the VSVersion to be used - VS2013 or VS2015"
+  echo "Specify the VSVersion to be used - VS2017"
   echo "Specify the build type (Debug, Release)"
   EXIT /B 1
 
 :DONE
   EXIT /B 0
-
-
-
-
-
-
