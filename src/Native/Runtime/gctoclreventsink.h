7b43f95938eec19378e085f5d44121fb87169cff
Sean Gillespie <segilles@microsoft.com>
2018-01-25 18:17:57 -0800

    FEATURE_EVENT_TRACE 3/n: Defining and Firing Dynamic Events (#16000)

--- src/Native/Runtime/gctoclreventsink.h
+++ src/Native/Runtime/gctoclreventsink.h
@@ -10,7 +10,8 @@
 
 class GCToCLREventSink : public IGCToCLREventSink
 {
-    /* [LOCALGC TODO] This will be filled with events as they get ported */
+public:
+    void FireDynamicEvent(const char* eventName, void* payload, uint32_t payloadSize);
 };
 
 extern GCToCLREventSink g_gcToClrEventSink;
