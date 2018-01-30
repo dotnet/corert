6108b0211b48b4759ead9fe3ebbd5361f222b5af
Adeel <adeelbm@outlook.com>
2018-01-30 09:24:57 +0200

    Address CR feedback

--- src/BuildIntegration/findvcvarsall.bat
+++ src/BuildIntegration/findvcvarsall.bat
@@ -5,18 +5,21 @@ SET vswherePath=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.ex
 IF NOT EXIST "%vswherePath%" GOTO :ERROR
 
 FOR /F "tokens=*" %%i IN (	'
-	  "%vswherePath%" -latest -products * ^
+	  "%vswherePath%" -latest -prerelease -products * ^
         -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 ^
         -property installationPath'
       ) DO SET vsBase=%%i
 
 IF "%vsBase%"=="" GOTO :ERROR
 
->"%~dp0\.vcvarsallwithargs" echo("%vsBase%\vc\Auxiliary\Build\vcvarsall.bat" %1%
+CALL "%vsBase%\vc\Auxiliary\Build\vcvarsall.bat" %1% > NUL
+
+FOR /F "delims=" %%W IN ('where link') DO (
+    FOR %%A IN ("%%W") DO ECHO %%~dpA
+    EXIT /B 0
+)
 
 ENDLOCAL
-
-EXIT /B 0
 
 :ERROR
     ECHO "Visual Studio not found, try to downloading it from https://www.visualstudio.com/ and select Desktop Development for C++ workload."
