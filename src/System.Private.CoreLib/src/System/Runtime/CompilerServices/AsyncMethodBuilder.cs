7319b8aa5a3d138058f6f6e8152f7dfbd03f22a2
Ben Adams <thundercat@illyriad.co.uk>
2018-02-01 15:21:33 +0000

    AsyncMethodBuilder => AsyncMethodBuilderCore

--- src/System.Private.CoreLib/src/System/Runtime/CompilerServices/AsyncMethodBuilder.cs
+++ src/System.Private.CoreLib/src/System/Runtime/CompilerServices/AsyncMethodBuilder.cs
@@ -71,7 +71,7 @@ namespace System.Runtime.CompilerServices
         [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine
-            => AsyncMethodBuilder.Start(ref stateMachine);
+            => AsyncMethodBuilderCore.Start(ref stateMachine);
 
         /// <summary>Associates the builder with the state machine it represents.</summary>
         /// <param name="stateMachine">The heap-allocated state machine object.</param>
@@ -236,7 +236,7 @@ namespace System.Runtime.CompilerServices
         [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine
-            => AsyncMethodBuilder.Start(ref stateMachine);
+            => AsyncMethodBuilderCore.Start(ref stateMachine);
 
         /// <summary>Associates the builder with the state machine it represents.</summary>
         /// <param name="stateMachine">The heap-allocated state machine object.</param>
@@ -433,7 +433,7 @@ namespace System.Runtime.CompilerServices
         [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine
-            => AsyncMethodBuilder.Start(ref stateMachine);
+            => AsyncMethodBuilderCore.Start(ref stateMachine);
 
         /// <summary>Associates the builder with the state machine it represents.</summary>
         /// <param name="stateMachine">The heap-allocated state machine object.</param>
@@ -715,8 +715,36 @@ namespace System.Runtime.CompilerServices
         /// <exception cref="System.ArgumentNullException">The <paramref name="stateMachine"/> argument is null (Nothing in Visual Basic).</exception>
         [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine
-            => AsyncMethodBuilder.Start(ref stateMachine);
+        internal static void Start<TStateMachine>(ref TStateMachine stateMachine)
+            where TStateMachine : IAsyncStateMachine
+        {
+            Thread currentThread = Thread.CurrentThread;
+            ExecutionContext previousExecutionCtx = currentThread.ExecutionContext;
+            SynchronizationContext previousSyncCtx = currentThread.SynchronizationContext;
+
+            // Async state machines are required not to throw, so no need for try/finally here.
+            stateMachine.MoveNext();
+
+            // The common case is that these have not changed, so avoid the cost of a write barrier if not needed.
+            if (previousSyncCtx != currentThread.SynchronizationContext)
+            {
+                // Restore changed SynchronizationContext back to previous
+                currentThread.SynchronizationContext = previousSyncCtx;
+            }
+
+            ExecutionContext currentExecutionCtx = currentThread.ExecutionContext;
+            if (previousExecutionCtx != currentExecutionCtx)
+            {
+                // Restore changed ExecutionContext back to previous
+                currentThread.ExecutionContext = previousExecutionCtx;
+                if ((currentExecutionCtx != null && currentExecutionCtx.HasChangeNotifications) ||
+                    (previousExecutionCtx != null && previousExecutionCtx.HasChangeNotifications))
+                {
+                    // There are change notifications; trigger any affected
+                    ExecutionContext.OnValuesChanged(currentExecutionCtx, previousExecutionCtx);
+                }
+            }
+        }
 
         //
         // We are going to do something odd here, which may require some explaining.  GetCompletionAction does quite a bit
@@ -928,48 +956,6 @@ namespace System.Runtime.CompilerServices
             if (runner != null)
                 return runner.m_stateMachine.MoveNext;
             return action;
-        }
-    }
-
-    internal static class AsyncMethodBuilder
-    {
-        /// <summary>Initiates the builder's execution with the associated state machine.</summary>
-        /// <typeparam name="TStateMachine">Specifies the type of the state machine.</typeparam>
-        /// <param name="stateMachine">The state machine instance, passed by reference.</param>
-        [DebuggerStepThrough]
-        public static void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine
-        {
-            if (stateMachine == null) // TStateMachines are generally non-nullable value types, so this check will be elided
-            {
-                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.stateMachine);
-            }
-
-            Thread currentThread = Thread.CurrentThread;
-            ExecutionContext previousExecutionCtx = currentThread.ExecutionContext;
-            SynchronizationContext previousSyncCtx = currentThread.SynchronizationContext;
-
-            // Async state machines are required not to throw, so no need for try/finally here.
-            stateMachine.MoveNext();
-
-            // The common case is that these have not changed, so avoid the cost of a write barrier if not needed.
-            if (previousSyncCtx != currentThread.SynchronizationContext)
-            {
-                // Restore changed SynchronizationContext back to previous
-                currentThread.SynchronizationContext = previousSyncCtx;
-            }
-
-            ExecutionContext currentExecutionCtx = currentThread.ExecutionContext;
-            if (previousExecutionCtx != currentExecutionCtx)
-            {
-                // Restore changed ExecutionContext back to previous
-                currentThread.ExecutionContext = previousExecutionCtx;
-                if ((currentExecutionCtx != null && currentExecutionCtx.HasChangeNotifications) ||
-                    (previousExecutionCtx != null && previousExecutionCtx.HasChangeNotifications))
-                {
-                    // There are change notifications; trigger any affected
-                    ExecutionContext.OnValuesChanged(currentExecutionCtx, previousExecutionCtx);
-                }
-            }
         }
     }
 }
