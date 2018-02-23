929d4e4398a7ec8da532bd53db0e2469bb837b8d
John Doe <github.john.doe@outlook.com>
2018-02-22 17:36:30 -0800

    definfed -> defined

--- src/Native/gc/unix/gcenv.unix.cpp
+++ src/Native/gc/unix/gcenv.unix.cpp
@@ -11,7 +11,7 @@
 // This isn't something we want, because we're totally fine using non-posix functions.
 #if defined(__APPLE__)
  #define _DARWIN_C_SOURCE
-#endif // definfed(__APPLE__)
+#endif // defined(__APPLE__)
 
 #include <pthread.h>
 #include <signal.h>
