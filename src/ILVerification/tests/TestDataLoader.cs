3a7b082ab3a8a6d24b35914c785894b9dc94821b
Julien Couvreur <jcouv@users.noreply.github.com>
2018-03-01 00:01:00 -0800

    Fix ILVerification.Tests project (#5471)
    * Use correct name to fetch embedded resource
    
    * Fix the test project
    
    * Use GetTypeInfo().Assembly

--- src/ILVerification/tests/TestDataLoader.cs
+++ src/ILVerification/tests/TestDataLoader.cs
@@ -10,12 +10,13 @@ using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 using System.Reflection.PortableExecutable;
 using System.Text;
+using ILVerify;
 using Internal.TypeSystem.Ecma;
 using Newtonsoft.Json;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace ILVerify.Tests
+namespace ILVerification.Tests
 {
     /// <summary>
     /// Parses the methods in the test assemblies. 
@@ -169,10 +170,10 @@ namespace ILVerify.Tests
                 simpleNameToPathMap.Add(Path.GetFileNameWithoutExtension(fileName), TESTASSEMBLYPATH + fileName);
             }
 
-            Assembly coreAssembly = typeof(object).Assembly;
+            Assembly coreAssembly = typeof(object).GetTypeInfo().Assembly;
             simpleNameToPathMap.Add(coreAssembly.GetName().Name, coreAssembly.Location);
 
-            Assembly systemRuntime = Assembly.Load("System.Runtime");
+            Assembly systemRuntime = Assembly.Load(new AssemblyName("System.Runtime"));
             simpleNameToPathMap.Add(systemRuntime.GetName().Name, systemRuntime.Location);
 
             var resolver = new TestResolver(simpleNameToPathMap);
