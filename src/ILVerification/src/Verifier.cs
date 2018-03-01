552e76b3aee63ac76020edee8f0f67ebcd1476ef
Julien Couvreur <jcouv@users.noreply.github.com>
2018-02-28 21:37:14 -0800

    Use correct name to fetch embedded resource (#5464)

--- src/ILVerification/src/Verifier.cs
+++ src/ILVerification/src/Verifier.cs
@@ -18,7 +18,7 @@ namespace ILVerify
     public class Verifier
     {
         private Lazy<ResourceManager> _stringResourceManager =
-            new Lazy<ResourceManager>(() => new ResourceManager("ILVerify.Resources.Strings", typeof(Verifier).GetTypeInfo().Assembly));
+            new Lazy<ResourceManager>(() => new ResourceManager("FxResources.ILVerification.SR", typeof(Verifier).GetTypeInfo().Assembly));
 
         private ILVerifyTypeSystemContext _typeSystemContext;
 
