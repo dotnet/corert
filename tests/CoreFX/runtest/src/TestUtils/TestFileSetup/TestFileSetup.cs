bf6b0572c967cfc492a1e0bf5a7d070124143026
Andon Andonov <anandono@microsoft.com>
2018-03-14 14:51:25 -0700

    Enable CoreFX tests on CoreRT (#5329)
    * Generate Test projects - Intermittent checkin
    
    * Intermittent check-in running exe, tests need restoring
    
    * Add Test utitilites project
    
    * End-to-End test build Windows
    
    * Change structure
    
    * Fix RDXML and add reflectable types
    
    * Add json parsing and xml logs
    
    * End-to-end test run and display
    
    * Helper project style changes
    
    * Remove direct dll references add package dependencies
    
    * Stylistic changes + deps
    
    * Style fixup
    
    * XML reader escape
    
    * Dependency and comment cleanup
    
    * Respond to PR feedback
    
    * Intermittent checkin - Build helper projects
    
    Shell scripts
    
    * Building and working shell scripts
    
    * End-to-end build and run
    
    * Add error checking
    
    * Style Fixup
    
    * Address PR comments
    
    * Revert line change
    
    * Remove Net.Security.Tests
    
    * OSX Changes
    
    * Update URL; update dependencies
    
    * Remove Collections.Immutable and update to long-lasting URL

---
+++ tests/CoreFX/runtest/src/TestUtils/TestFileSetup/TestFileSetup.cs
@@ -0,0 +1,251 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CoreFX.TestUtils.TestFileSetup
+{
+    /// <summary>
+    /// Defines the set of flags that represent exit codes
+    /// </summary>
+    [Flags]
+    public enum ExitCode : int
+    {
+        Success = 0,
+        HttpError = 1,
+        IOError = 2,
+        UnknownError = 10
+
+    }
+
+    /// <summary>
+    /// This helper class is used to fetch CoreFX tests from a specified URL, unarchive them and create a flat directory structure
+    /// through which to iterate.
+    /// </summary>
+    public static class TestFileSetup
+    {
+        private static HttpClient httpClient;
+        private static bool cleanTestBuild = false;
+
+        private static string outputDir;
+        private static string testUrl;
+        private static string testListPath;
+
+        public static void Main(string[] args)
+        {
+            ExitCode exitCode = ExitCode.UnknownError;
+            ArgumentSyntax argSyntax = ParseCommandLine(args);
+
+            if (!Directory.Exists(outputDir))
+            {
+                try
+                {
+                    Directory.CreateDirectory(outputDir);
+                }
+                catch (IOException)
+                {
+                    exitCode = ExitCode.IOError;
+                    Environment.Exit((int)exitCode);
+                }
+            }
+
+            // parse args
+            try
+            {
+                SetupTests(testUrl, outputDir, ReadTestNames(testListPath)).Wait();
+                exitCode = ExitCode.Success;
+
+            }
+            catch (HttpRequestException)
+            {
+                exitCode = ExitCode.HttpError;
+            }
+            catch (IOException)
+            {
+                exitCode = ExitCode.IOError;
+            }
+
+            Environment.Exit((int)exitCode);
+        }
+
+        public static ArgumentSyntax ParseCommandLine(string[] args)
+        {
+            ArgumentSyntax argSyntax = ArgumentSyntax.Parse(args, syntax =>
+            {
+                syntax.DefineOption("out|outDir|outputDirectory", ref outputDir, "Directory where tests are downloaded");
+                syntax.DefineOption("testUrl", ref testUrl, "URL, pointing to the list of tests");
+                syntax.DefineOption("testListJsonPath", ref testListPath, "JSON-formatted list of test assembly names to download");
+                syntax.DefineOption("clean", ref cleanTestBuild, "Remove all previously built test assemblies");
+            });
+
+            return argSyntax;
+        }
+
+        public static Dictionary<string, string> ReadTestNames(string testFilePath)
+        {
+            Debug.Assert(File.Exists(testFilePath));
+
+            Dictionary<string, string> testNames = new Dictionary<string, string>();
+
+            // We're being a passed a list of test assembly names, so anything that's not a string is invalid
+            using (var sr = new StreamReader(testFilePath))
+            using (var jsonReader = new JsonTextReader(sr))
+            {
+                while (jsonReader.Read())
+                {
+                    if (jsonReader.TokenType == JsonToken.String)
+                    {
+                        testNames.Add(jsonReader.Value.ToString(), string.Empty);
+                    }
+                }
+            }
+
+            return testNames;
+        }
+
+        public static async Task SetupTests(string jsonUrl, string destinationDirectory, Dictionary<string, string> testNames = null, bool runAllTests = false)
+        {
+            Debug.Assert(Directory.Exists(destinationDirectory));
+            Debug.Assert(runAllTests || testNames != null);
+
+            string tempDirPath = Path.Combine(destinationDirectory, "temp");
+            if (!Directory.Exists(tempDirPath))
+            {
+                Directory.CreateDirectory(tempDirPath);
+            }
+            Dictionary<string, string> testPayloads = await GetTestUrls(jsonUrl, testNames, runAllTests);
+
+            await GetTestArchives(testPayloads, tempDirPath);
+            ExpandArchivesInDirectory(tempDirPath, destinationDirectory);
+
+            Directory.Delete(tempDirPath);
+        }
+        public static async Task<Dictionary<string, string>> GetTestUrls(string jsonUrl, Dictionary<string, string> testNames = null, bool runAllTests = false)
+        {
+            if (httpClient is null)
+            {
+                httpClient = new HttpClient();
+            }
+
+            Debug.Assert(runAllTests || testNames != null);
+
+            // Set up the json stream reader
+            using (var responseStream = await httpClient.GetStreamAsync(jsonUrl))
+            using (var streamReader = new StreamReader(responseStream))
+            using (var jsonReader = new JsonTextReader(streamReader))
+            {
+                // Manual parsing - we only need to key-value pairs from each object and this avoids deserializing all of the work items into objects
+                string markedTestName = string.Empty;
+                string currentPropertyName = string.Empty;
+
+                while (jsonReader.Read())
+                {
+                    if (jsonReader.Value != null)
+                    {
+                        switch (jsonReader.TokenType)
+                        {
+                            case JsonToken.PropertyName:
+                                currentPropertyName = jsonReader.Value.ToString();
+                                break;
+                            case JsonToken.String:
+                                if (currentPropertyName.Equals("WorkItemId"))
+                                {
+                                    string currentTestName = jsonReader.Value.ToString();
+
+                                    if (runAllTests || testNames.ContainsKey(currentTestName))
+                                    {
+                                        markedTestName = currentTestName;
+                                    }
+                                }
+                                else if (currentPropertyName.Equals("PayloadUri") && markedTestName != string.Empty)
+                                {
+                                    testNames[markedTestName] = jsonReader.Value.ToString();
+                                    markedTestName = string.Empty;
+                                }
+                                break;
+                        }
+                    }
+                }
+
+            }
+            return testNames;
+        }
+
+        public static async Task GetTestArchives(Dictionary<string, string> testPayloads, string downloadDir)
+        {
+            if (httpClient is null)
+            {
+                httpClient = new HttpClient();
+            }
+
+            foreach (string testName in testPayloads.Keys)
+            {
+                string payloadUri = testPayloads[testName];
+
+                if (!Uri.IsWellFormedUriString(payloadUri, UriKind.Absolute))
+                    continue;
+
+                using (var response = await httpClient.GetStreamAsync(payloadUri))
+                {
+                    if (response.CanRead)
+                    {
+                        // Create the test setup directory if it doesn't exist
+                        if (!Directory.Exists(downloadDir))
+                        {
+                            Directory.CreateDirectory(downloadDir);
+                        }
+
+                        // CoreFX test archives are output as .zip regardless of platform
+                        string archivePath = Path.Combine(downloadDir, testName + ".zip");
+
+                        // Copy to a temp folder 
+                        using (FileStream file = new FileStream(archivePath, FileMode.Create))
+                        {
+                            await response.CopyToAsync(file);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        public static void ExpandArchivesInDirectory(string archiveDirectory, string destinationDirectory, bool cleanup = true)
+        {
+            Debug.Assert(Directory.Exists(archiveDirectory));
+            Debug.Assert(Directory.Exists(destinationDirectory));
+
+            string[] archives = Directory.GetFiles(archiveDirectory, "*.zip", SearchOption.TopDirectoryOnly);
+
+            foreach (string archivePath in archives)
+            {
+                string destinationDirName = Path.Combine(destinationDirectory, Path.GetFileNameWithoutExtension(archivePath));
+
+                // If doing clean test build - delete existing artefacts
+                if (Directory.Exists(destinationDirName) && cleanTestBuild)
+                {
+                    Directory.Delete(destinationDirName, true);
+                }
+
+                ZipFile.ExtractToDirectory(archivePath, destinationDirName);
+
+
+                // Delete archives
+                if (cleanup)
+                {
+                    File.Delete(archivePath);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ tests/CoreFX/runtest/src/TestUtils/XUnit/ResultFormatter.cs
@@ -0,0 +1,115 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+/// <summary>
+/// This helper class reads xunit xml log files and prints the tallied test results to the console
+/// </summary>
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace CoreFX.TestUtils.XUnit
+{
+    public static class ResultFormatter
+    {
+        private static string logDir;
+        private static string logPattern;
+
+        public static void Main(string[] args)
+        {
+            ArgumentSyntax syntax = ParseCommandLine(args);
+            IEnumerable<string> logFiles = DiscoverLogs(logDir, logPattern);
+            PrintTotals(logFiles);
+        }
+
+        private static void PrintTotals(IEnumerable<string> logFiles)
+        {
+            int total = 0;
+            int passed = 0;
+            int failed = 0;
+            int skipped = 0;
+            ulong timeElapsed = 0;
+
+            foreach(string logFile in logFiles)
+            {
+                // XMLReader escapes the character sequence \\.. as just a single backslash \ - Is this intended behavior? 
+                using (XmlReader reader = XmlReader.Create(logFile.Replace(@"\\..", @"\..")))
+                {
+                    reader.MoveToContent();
+                    reader.ReadToDescendant("collection");
+                    do
+                    {
+                        // Get total tests in current element
+                        string totalAttr = reader.GetAttribute("total");
+                        int currentTotal;
+                        Int32.TryParse(totalAttr, out currentTotal);
+                        total += currentTotal;
+
+                        // Get passed tests 
+                        string passedAttr = reader.GetAttribute("passed");
+                        int currentPassed;
+                        Int32.TryParse(passedAttr, out currentPassed);
+                        passed += currentPassed;
+
+                        // Get failed tests
+                        string failedAttr = reader.GetAttribute("failed");
+                        int currentFailed;
+                        Int32.TryParse(failedAttr, out currentFailed);
+                        failed += currentFailed;
+
+                        // Get skipped tests
+                        string skippedAttr = reader.GetAttribute("skipped");
+                        int currentSkipped;
+                        Int32.TryParse(skippedAttr, out currentSkipped);
+                        skipped += currentSkipped;
+
+                        // Get time elapsed
+                        string timeAttr = reader.GetAttribute("time");
+                        ulong currentTime;
+                        UInt64.TryParse(timeAttr, out currentTime);
+                        timeElapsed += currentTime;
+
+                    } while (reader.ReadToNextSibling("collection"));
+
+                }
+            }
+
+            Console.WriteLine("=== CoreFX TEST EXECUTION SUMMARY ===: ");
+            Console.WriteLine(String.Format("Total: {0}, Passed: {1}, Failed: {2}, Skipped: {3}", total, passed, failed, timeElapsed));
+            Console.WriteLine("Detailed logs written to: " + logDir);
+
+        }
+
+        private static ArgumentSyntax ParseCommandLine(string[] args)
+        {
+
+            ArgumentSyntax argSyntax = ArgumentSyntax.Parse(args, syntax =>
+            {
+                syntax.DefineOption("log|logDirectory|logDir", ref logDir, "Path to directory of xml test results");
+                syntax.DefineOption("pattern|p", ref logPattern, "Pattern of XUnit log filenames for which to search");
+            });
+
+            return argSyntax;
+        }
+
+        public static IEnumerable<string> DiscoverLogs(string logDirectory, string logPattern)
+        {
+            Debug.Assert(Directory.Exists(logDirectory));
+            Console.WriteLine(logDirectory);
+            var logFiles = Directory.EnumerateFiles(logDirectory, logPattern, SearchOption.AllDirectories);
+            
+            return logFiles;
+        }
+
+
+    }
+}
