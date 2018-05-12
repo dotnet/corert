12cf424569ca0e6dbe79aaa457b2cfba4bbbc7c1
scott <scott.waye@hubse.com>
2018-05-12 17:13:16 -0500

    comment span

--- src/ILCompiler.WebAssembly/src/CodeGen/ILToWebAssemblyImporter.cs
+++ src/ILCompiler.WebAssembly/src/CodeGen/ILToWebAssemblyImporter.cs
@@ -625,15 +625,15 @@ namespace Internal.IL
             }
             else if (toStoreKind == LLVMTypeKind.LLVMIntegerTypeKind && valueTypeKind == LLVMTypeKind.LLVMDoubleTypeKind)
             {
-                var width = LLVM.GetIntTypeWidth(LLVM.TypeOf(source));
-                if (width == 32)
-                {
+//                var width = LLVM.GetIntTypeWidth(LLVM.TypeOf(source));
+//                if (width == 32)
+//                {
                     typedToStore = LLVM.BuildSIToFP(builder, source, valueType, "CastIfNecessarySIToFloat");
-                }
-                else
-                {
-                    throw new NotImplementedException($"trying to cast {toStoreKind} to {valueTypeKind}");
-                }
+//                }
+//                else
+//                {
+//                    throw new NotImplementedException($"trying to cast {toStoreKind} to {valueTypeKind}");
+//                }
             }
 
             return typedToStore;
