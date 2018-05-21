332c0ffd36441d55b47cb47a0262d6a972584e4e
Toni Solarin-Sodara <toni.edward@outlook.com>
2018-05-22 00:29:31 +0100

    add preliminary InterpreterExecutionStrategy class (#5011)

---
+++ src/System.Private.Interpreter/src/Internal/Runtime/Interpreter/InterpreterExecutionStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Internal.Runtime.TypeLoader;
+
+namespace Internal.Runtime.Interpreter
+{
+    class InterpreterExecutionStrategy : MethodExecutionStrategy
+    {
+        public override IntPtr OnEntryPoint(MethodEntrypointPtr entrypointInfo, IntPtr callerArgumentsInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
