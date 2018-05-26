3bdd6b38590b4f194d51afaf467df84cd697b992
Maryam Ariyan <maariyan@microsoft.com>
2018-05-25 17:51:47 -0700

    Removing extra ifdef by moving logic to non-shared files

--- src/System.Private.CoreLib/src/System/Diagnostics/StackFrame.CoreRT.cs
+++ src/System.Private.CoreLib/src/System/Diagnostics/StackFrame.CoreRT.cs
@@ -139,5 +139,7 @@ namespace System.Diagnostics
                 builder.AppendLine(DeveloperExperience.Default.CreateStackTraceString(_ipAddress, _needFileInfo));
             }
         }
+
+        private bool IncludeFileInfoIfAvailable() => (_method != null) || AppendStackFrameWithoutMethodBase(sb);
     }
 }
