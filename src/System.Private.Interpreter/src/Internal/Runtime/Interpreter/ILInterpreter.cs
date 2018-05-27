5bb44d7d75639b3c1650ce71b8c5e644dbb1d75d
Toni Solarin-Sodara <toni.edward@outlook.com>
2018-05-27 15:14:12 +0100

    add LowLevelStack class (#5011)

--- src/System.Private.Interpreter/src/Internal/Runtime/Interpreter/ILInterpreter.cs
+++ src/System.Private.Interpreter/src/Internal/Runtime/Interpreter/ILInterpreter.cs
@@ -9,19 +9,25 @@ namespace Internal.Runtime.Interpreter
 {
     class ILInterpreter
     {
-        private MethodDesc _method;
-        private MethodIL _methodIL;
+        private readonly MethodDesc _method;
+        private readonly MethodIL _methodIL;
+        private readonly LowLevelStack<object> _stack;
 
         public ILInterpreter(MethodDesc method, MethodIL methodIL)
         {
             _method = method;
             _methodIL = methodIL;
+            _stack = new LowLevelStack<object>();
         }
 
         public void Interpret(ref CallInterceptorArgs callInterceptorArgs)
         {
             ILDisassembler disassembler = new ILDisassembler(_methodIL);
             ILInstruction instruction = disassembler.GetNextILInstruction();
+            while (instruction != null)
+            {
+                instruction = disassembler.GetNextILInstruction();
+            }
         }
     }
 }
