3bf00da5e9632d6a25d32270d7e3c710517c574f
Maryam Ariyan <maariyan@microsoft.com>
2018-05-31 15:42:06 -0700

    Reduces diff in IResourceGroveler

--- src/System.Private.CoreLib/src/System/Resources/IResourceGroveler.cs
+++ src/System.Private.CoreLib/src/System/Resources/IResourceGroveler.cs
@@ -2,10 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-
 /*============================================================
 **
 ** 
@@ -16,6 +12,10 @@ using System.Globalization;
 **
 ** 
 ===========================================================*/
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace System.Resources
 {
