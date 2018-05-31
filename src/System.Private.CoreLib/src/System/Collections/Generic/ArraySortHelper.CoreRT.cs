1db06d2defcb1482ce9f8867211c45936a38410a
Jan Kotas <jkotas@microsoft.com>
2018-05-31 14:04:51 -0700

    Delete unnecessary MembersMustExist annotation

--- src/System.Private.CoreLib/src/System/Collections/Generic/ArraySortHelper.CoreRT.cs
+++ src/System.Private.CoreLib/src/System/Collections/Generic/ArraySortHelper.CoreRT.cs
@@ -11,10 +11,6 @@ namespace System.Collections.Generic
 {
     internal partial class ArraySortHelper<T>
     {
-        // WARNING: We allow diagnostic tools to directly inspect this member (s_defaultArraySortHelper). 
-        // See https://github.com/dotnet/corert/blob/master/Documentation/design-docs/diagnostics/diagnostics-tools-contract.md for more details. 
-        // Please do not change the type, the name, or the semantic usage of this member without understanding the implication for tools. 
-        // Get in touch with the diagnostics team if you have questions.
         private static readonly ArraySortHelper<T> s_defaultArraySortHelper = new ArraySortHelper<T>();
 
         public static ArraySortHelper<T> Default
@@ -28,10 +24,6 @@ namespace System.Collections.Generic
 
     internal partial class ArraySortHelper<TKey, TValue>
     {
-        // WARNING: We allow diagnostic tools to directly inspect this member (s_defaultArraySortHelper). 
-        // See https://github.com/dotnet/corert/blob/master/Documentation/design-docs/diagnostics/diagnostics-tools-contract.md for more details. 
-        // Please do not change the type, the name, or the semantic usage of this member without understanding the implication for tools. 
-        // Get in touch with the diagnostics team if you have questions.
         private static readonly ArraySortHelper<TKey, TValue> s_defaultArraySortHelper = new ArraySortHelper<TKey, TValue>();
 
         public static ArraySortHelper<TKey, TValue> Default
