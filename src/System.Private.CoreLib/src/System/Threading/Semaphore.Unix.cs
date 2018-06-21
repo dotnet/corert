018bd0fe2f48311b6478da0360e8f2a500da8114
Anipik <anirudhagnihotry098@gmail.com>
2018-06-21 15:41:43 -0700

    Fixed build on Unix

--- src/System.Private.CoreLib/src/System/Threading/Semaphore.Unix.cs
+++ src/System.Private.CoreLib/src/System/Threading/Semaphore.Unix.cs
@@ -2,8 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.Win32.SafeHandles;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace System.Threading
 {
@@ -38,7 +40,7 @@ namespace System.Threading
             waitHandle.DangerousAddRef();
             try
             {
-                return WaitSubsystem.ReleaseSemaphore(handle, releaseCount);
+                return WaitSubsystem.ReleaseSemaphore(waitHandle.DangerousGetHandle(), releaseCount);
             }
             finally
             {
