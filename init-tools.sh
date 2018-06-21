4871869f78c48ee9c54ef35b436be7f9dc905df6
Jan Kotas <jkotas@microsoft.com>
2018-06-20 20:38:07 -0700

    Undo accidental 2.0 -> 2.1 renames (#5981)

--- init-tools.sh
+++ init-tools.sh
@@ -131,7 +131,7 @@ if [ ! -e $__INIT_TOOLS_DONE_MARKER ]; then
         echo "Initializing BuildTools..."
         echo "Running: $__BUILD_TOOLS_PATH/init-tools.sh $__scriptpath $__DOTNET_CMD $__TOOLRUNTIME_DIR $__PACKAGES_DIR" >> $__init_tools_log
 
-        # Executables restored with .NET Core 2.1 do not have executable permission flags. https://github.com/NuGet/Home/issues/4424
+        # Executables restored with .NET Core 2.0 do not have executable permission flags. https://github.com/NuGet/Home/issues/4424
         chmod +x $__BUILD_TOOLS_PATH/init-tools.sh
         $__BUILD_TOOLS_PATH/init-tools.sh $__scriptpath $__DOTNET_CMD $__TOOLRUNTIME_DIR $__PACKAGES_DIR >> $__init_tools_log
         if [ "$?" != "0" ]; then
@@ -142,7 +142,7 @@ if [ ! -e $__INIT_TOOLS_DONE_MARKER ]; then
     fi
 
     echo "Making all .sh files executable under Tools."
-    # Executables restored with .NET Core 2.1 do not have executable permission flags. https://github.com/NuGet/Home/issues/4424
+    # Executables restored with .NET Core 2.0 do not have executable permission flags. https://github.com/NuGet/Home/issues/4424
     ls $__scriptpath/Tools/*.sh | xargs chmod +x
     ls $__scriptpath/Tools/scripts/docker/*.sh | xargs chmod +x
 
