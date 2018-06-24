a2f08668ec92ab8fd7a6c70ddcdc6227789c0c9a
Anipik <anirudhagnihotry098@gmail.com>
2018-06-23 21:29:23 -0700

    non shared changes and disabling tests

--- src/System.Private.CoreLib/src/System/Threading/Timer.Windows.cs
+++ src/System.Private.CoreLib/src/System/Threading/Timer.Windows.cs
@@ -66,7 +66,7 @@ namespace System.Threading
     {
         private void SignalNoCallbacksRunning()
         {
-            Interop.mincore.SetEvent(_notifyWhenNoCallbacksRunning.SafeWaitHandle);
+            Interop.Kernel32.SetEvent(_notifyWhenNoCallbacksRunning.SafeWaitHandle);
         }
     }
 }
