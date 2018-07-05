9bc263d84540139c69d42c4573928d3195ef34db
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-07-05 23:04:45 +0200

    address PR feedback

--- src/System.Private.CoreLib/src/System/Activator.cs
+++ src/System.Private.CoreLib/src/System/Activator.cs
@@ -11,6 +11,7 @@ using System.Diagnostics;
 using System.Globalization;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.Remoting;
 using System.Runtime;
 
 using Internal.Reflection.Augments;
@@ -148,17 +149,18 @@ namespace System
 
         private const BindingFlags ConstructorDefault = BindingFlags.Instance | BindingFlags.Public | BindingFlags.CreateInstance;
 
-        public static System.Runtime.Remoting.ObjectHandle CreateInstance(string assemblyName, string typeName) { throw new PlatformNotSupportedException(); }
+        // https://github.com/dotnet/corefx/issues/30845
+        public static ObjectHandle CreateInstance(string assemblyName, string typeName) { throw new PlatformNotSupportedException(); }
 
-        public static System.Runtime.Remoting.ObjectHandle CreateInstance(string assemblyName, string typeName, bool ignoreCase, System.Reflection.BindingFlags bindingAttr, System.Reflection.Binder binder, object[] args, System.Globalization.CultureInfo culture, object[] activationAttributes) { throw new PlatformNotSupportedException(); }
+        public static ObjectHandle CreateInstance(string assemblyName, string typeName, bool ignoreCase, System.Reflection.BindingFlags bindingAttr, System.Reflection.Binder binder, object[] args, System.Globalization.CultureInfo culture, object[] activationAttributes) { throw new PlatformNotSupportedException(); }
 
-        public static System.Runtime.Remoting.ObjectHandle CreateInstance(string assemblyName, string typeName, object[] activationAttributes) { throw new PlatformNotSupportedException(); }
+        public static ObjectHandle CreateInstance(string assemblyName, string typeName, object[] activationAttributes) { throw new PlatformNotSupportedException(); }
 
-        public static System.Runtime.Remoting.ObjectHandle CreateInstanceFrom(string assemblyFile, string typeName) { throw new PlatformNotSupportedException(); }
+        public static ObjectHandle CreateInstanceFrom(string assemblyFile, string typeName) { throw new PlatformNotSupportedException(); }
 
-        public static System.Runtime.Remoting.ObjectHandle CreateInstanceFrom(string assemblyFile, string typeName, bool ignoreCase, System.Reflection.BindingFlags bindingAttr, System.Reflection.Binder binder, object[] args, System.Globalization.CultureInfo culture, object[] activationAttributes) { throw new PlatformNotSupportedException(); }
+        public static ObjectHandle CreateInstanceFrom(string assemblyFile, string typeName, bool ignoreCase, System.Reflection.BindingFlags bindingAttr, System.Reflection.Binder binder, object[] args, System.Globalization.CultureInfo culture, object[] activationAttributes) { throw new PlatformNotSupportedException(); }
 
-        public static System.Runtime.Remoting.ObjectHandle CreateInstanceFrom(string assemblyFile, string typeName, object[] activationAttributes) { throw new PlatformNotSupportedException(); }
+        public static ObjectHandle CreateInstanceFrom(string assemblyFile, string typeName, object[] activationAttributes) { throw new PlatformNotSupportedException(); }
     }
 }
 
