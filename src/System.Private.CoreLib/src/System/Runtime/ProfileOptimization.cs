c45bf3dba52fac80783511050b88775a59f45b95
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-07-14 22:27:54 +0200

    added no-op ProfileOptimization

---
+++ src/System.Private.CoreLib/src/System/Runtime/ProfileOptimization.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Runtime
+{
+    public static class ProfileOptimization
+    {
+        public static void SetProfileRoot(string directoryPath) { }
+        public static void StartProfile(string profile) { }
+    }
+}
