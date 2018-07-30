552227852c01c5e6e238f4cd3d999c3bf84ab489
Simon Nattress <simonn@microsoft.com>
2018-07-30 13:54:33 -0700

    ReadyToRun ETW Test Harness
    A test harness to validate ready-to-run binaries properly execute the ready-to-run native code instead of silently falling back to Jit.
    
    To validate this, The Microsoft.Diagnostics.Tracing (EventTrace) package is used to enable CLR ETW events during test execution. The ModuleLoad event is intercepted to find the module IDs of assemblies relevant to test (recall, all CLR ETW events are machine-wide across all CoreCLR / CLR runtimes currently running). The MethodLoadVerbose events are intercepted and their module ID is matched with those filtered in the ModuleLoad event. This allows us to focus solely on the test binaries and not the dependent framework.
    
    An optional whitelist file can be provided, whose format is a text file with one method name per line. The output of test failures can be pasted into a whitelist file to create a baseline.

---
+++ tests/src/tools/ReadyToRun.TestHarness/Program.cs
@@ -0,0 +1,206 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers.Clr;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Session;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReadyToRun.TestHarness
+{
+    //
+    // Test scenarios this harness will be useful for:
+    // * Given a fully ready-to-run compiled test, validate no methods in the test module are Jitted
+    // * Given a change to a dependent class, determine the set of r2r methods that are now 
+    //    invalid and fall back to Jit.
+    //
+    class Program
+    {
+        // Default timeout in milliseconds
+        private const int DefaultTestTimeOut = 60000;
+
+        // Error code returned when events get lost. Use this to re-run the test a few times.
+        private const int StatusTestErrorEventsLost = -101;
+        private const int StatusTestErrorMethodsJitted = -102;
+        private const int StatusTestErrorTimeOut = -103;
+        private const int StatusTestErrorBadInput = -104;
+        private const int StatusTestPassed = 100;
+
+        static void ShowUsage()
+        {
+            Console.WriteLine("dotnet ReadyToRun.Harness <PathToCoreRun> <PathToTestBinary> [MethodWhiteListFile]");
+        }
+
+        static int Main(string[] args)
+        {
+            int exitCode = StatusTestErrorBadInput;
+            string whiteListFile = null;
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Error: Missing required arguments.");
+                ShowUsage();
+                return exitCode;
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine($"Error: {args[0]} is an invalid path.");
+                ShowUsage();
+                return exitCode;
+            }
+
+            if (!File.Exists(args[1]))
+            {
+                Console.WriteLine($"Error: {args[1]} is an invalid path.");
+                ShowUsage();
+                return exitCode;
+            }
+
+            if (args.Length == 3 && File.Exists(args[2]))
+            {
+                whiteListFile = args[2];
+            }
+
+            string coreRunExePath = args[0];
+            string testExe = args[1];
+
+            // TODO: CoreCLR test bed has tests with multiple assemblies - we'll need to add them here when we support that
+            var testModules = new HashSet<string>();
+            testModules.Add(testExe);
+
+            using (var session = new TraceEventSession("ReadyToRunTestSession"))
+            {
+                var r2rMethodFilter = new ReadyToRunJittedMethods(session, testModules);
+                session.EnableProvider(ClrTraceEventParser.ProviderGuid, TraceEventLevel.Verbose, (ulong)(ClrTraceEventParser.Keywords.Jit | ClrTraceEventParser.Keywords.Loader));
+                
+                Task.Run(() => exitCode = RunTest(session, coreRunExePath, testExe));
+
+                // Block, processing callbacks for events we subscribed to
+                session.Source.Process();
+
+                Console.WriteLine("Test execution " + (exitCode == StatusTestPassed ? "PASSED" : "FAILED"));
+                int analysisResult = AnalyzeResults(r2rMethodFilter, whiteListFile);
+
+                Console.WriteLine("Test jitted method analysis " + (analysisResult == StatusTestPassed ? "PASSED" : "FAILED"));
+
+                // If the test passed, return the Jitted method analysis result
+                // If the test failed, return its execution exit code
+                exitCode = exitCode == StatusTestPassed ? analysisResult : exitCode;
+            }
+
+            Console.WriteLine($"Final test result: {exitCode}");
+            return exitCode;
+        }
+
+        private static int AnalyzeResults(ReadyToRunJittedMethods jittedMethods, string whiteListFilePath = null)
+        {
+            var whiteListedMethods = new HashSet<string>();
+            if (!string.IsNullOrEmpty(whiteListFilePath))
+            {
+                using (TextReader tr = File.OpenText(whiteListFilePath))
+                {
+                    string line = "";
+                    while ((line = tr.ReadLine()) != null)
+                    {
+                        whiteListedMethods.Add(line);
+                    }
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("*** Jitted method analysis ***");
+
+            Console.WriteLine("");
+            Console.WriteLine("White listed methods that were jitted (these don't count as test failures):");
+            int whiteListedJittedMethodCount = 0;
+            foreach (var jittedMethod in jittedMethods.JittedMethods)
+            {
+                if (whiteListedMethods.Contains(jittedMethod.methodName))
+                {
+                    Console.WriteLine(jittedMethod.methodName);
+                    ++whiteListedJittedMethodCount;
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Methods that were jitted without a whitelist entry (test failure):");
+            int jittedMethodCount = 0;
+            foreach (var jittedMethod in jittedMethods.JittedMethods)
+            {
+                if (!whiteListedMethods.Contains(jittedMethod.methodName))
+                {
+                    Console.WriteLine(jittedMethod.methodName);
+                    ++jittedMethodCount;
+                }
+            }
+
+            if (jittedMethodCount > 0)
+            {
+                Console.WriteLine($"Error: {jittedMethodCount} methods were jitted.");
+                return StatusTestErrorMethodsJitted;
+            }
+
+            return StatusTestPassed;
+        }
+
+        private static int RunTest(TraceEventSession session, string coreRunPath, string testExecutable)
+        {
+            int exitCode = -100;
+
+            try
+            {
+                using (var process = new Process())
+                {
+                    process.StartInfo.FileName = coreRunPath;
+                    process.StartInfo.Arguments = testExecutable;
+                    process.StartInfo.UseShellExecute = false;
+                    
+                    process.Start();
+
+                    process.OutputDataReceived += delegate(object sender, DataReceivedEventArgs args)
+                    {
+                        Console.WriteLine(args.Data);
+                    };
+
+                    process.ErrorDataReceived += delegate(object sender, DataReceivedEventArgs args)
+                    {
+                        Console.WriteLine(args.Data);
+                    };
+
+                    if (process.WaitForExit(DefaultTestTimeOut))
+                    {
+                        exitCode = process.ExitCode;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Test execution timed out.");
+                        exitCode = StatusTestErrorTimeOut;
+                    }
+
+                    Console.WriteLine($"Test exited with code {process.ExitCode}");
+                    
+                    if (session.EventsLost > 0)
+                    {
+                        Console.WriteLine($"Error - {session.EventsLost} got lost in the nether.");
+                        return StatusTestErrorEventsLost;
+                    }
+                }
+            }
+            finally
+            {
+                // Stop ETL collection on the main thread
+                session.Stop();
+            }
+            
+            return exitCode;
+        }
+    }
+}
