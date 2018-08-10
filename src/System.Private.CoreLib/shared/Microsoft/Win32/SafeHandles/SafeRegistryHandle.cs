f0428c9ffb985e3648b234c7e2bd17c2aedfbe9a
Anipik <anirudhagnihotry098@gmail.com>
2018-08-10 15:44:22 -0700

    Moved registry handle to  shared

---
+++ src/System.Private.CoreLib/shared/Microsoft/Win32/SafeHandles/SafeRegistryHandle.cs
@@ -0,0 +1,23 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace Microsoft.Win32.SafeHandles
+{
+    internal sealed class SafeRegistryHandle : SafeHandleZeroOrMinusOneIsInvalid
+    {
+        internal SafeRegistryHandle() : base(true) { }
+
+        public SafeRegistryHandle(IntPtr preexistingHandle, bool ownsHandle) : base(ownsHandle)
+        {
+            SetHandle(preexistingHandle);
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            return (Interop.RegCloseKey(handle) == Interop.Errors.ERROR_SUCCESS);
+        }
+    }
+}
