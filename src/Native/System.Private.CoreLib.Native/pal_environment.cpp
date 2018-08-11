2b2b945c2a5133b433a55be94ac77430650867c1
Robert Miles <rmiles@sjvassoc.com>
2018-08-11 14:42:21 -0400

    checkpoint

--- src/Native/System.Private.CoreLib.Native/pal_environment.cpp
+++ src/Native/System.Private.CoreLib.Native/pal_environment.cpp
@@ -30,3 +30,17 @@ extern "C" void CoreLibNative_Exit(int32_t exitCode)
 {
     exit(exitCode);
 }
+
+extern "C" char** CoreLibNative_EnvironGetSystemEnvironment()
+{
+    char** sysEnviron;
+
+#if HAVE__NSGETENVIRON
+    sysEnviron = *(_NSGetEnviron());
+#else   // HAVE__NSGETENVIRON
+    extern char **environ;
+    sysEnviron = environ;
+#endif  // HAVE__NSGETENVIRON
+
+    return sysEnviron;
+}
--- src/System.Private.CoreLib/src/Internal/Runtime/Augments/EnvironmentAugments.Unix.cs
+++ src/System.Private.CoreLib/src/Internal/Runtime/Augments/EnvironmentAugments.Unix.cs
@@ -30,7 +30,89 @@ namespace Internal.Runtime.Augments
             if ("".Length != 0)
                 throw new NotImplementedException(); // Need to return something better than an empty environment block.
 
-            return Array.Empty<KeyValuePair<string,string>>();
+            unsafe
+            {
+                // Get byte** of environment variables from native interop
+                var unsafeBlock = (byte**)Interop.Sys.EnvironGetSystemEnvironment();
+                if (unsafeBlock == (byte**)0)
+                    throw new OutOfMemoryException();
+                
+                // Find total length of two-dimensional byte**
+                int rowIndex = 0;
+                int totalLength = 0;
+                while (unsafeBlock[rowIndex] != null && unsafeBlock[rowIndex][0] != 0)
+                {
+                    byte* p = unsafeBlock[rowIndex];
+                    while (*p != 0) // Continue until end-of-line char '\0'
+                    {
+                        p++;
+                    }
+                    totalLength += checked((int)(p - unsafeBlock[rowIndex] + 1));
+                    rowIndex++;
+                }
+
+                // Copy two-dimensional byte** to a flat char[] for parsing
+                rowIndex = 0;
+                var blockIndex = 0;
+                char[] block = new char[totalLength];
+                while (unsafeBlock[rowIndex] != null && unsafeBlock[rowIndex][0] != 0)
+                {
+                    byte* p = unsafeBlock[rowIndex];
+                    while (*p != 0) // Continue until end-of-line char '\0'
+                    {
+                        p++;
+                    }
+                    var rowLength = checked((int)(p - unsafeBlock[rowIndex] + 1));
+                    for (int i = 0; i < rowLength; i++)
+                    {
+                        // Copy original byte to a Unicode char in our flat char[]
+                        block[blockIndex++] = Convert.ToChar(unsafeBlock[rowIndex][i]);
+                    }
+                    rowIndex++;
+                }
+
+                // Parse flat char[] and return
+                return EnumerateEnvironmentVariables(block);
+            }
+        }
+
+        private static IEnumerable<KeyValuePair<string, string>> EnumerateEnvironmentVariables(char[] block)
+        {
+            // To maintain complete compatibility with prior versions we need to return a Hashtable.
+            // We did ship a prior version of Core with LowLevelDictionary, which does iterate the
+            // same (e.g. yields DictionaryEntry), but it is not a public type.
+            //
+            // While we could pass Hashtable back from CoreCLR the type is also defined here. We only
+            // want to surface the local Hashtable.
+            for (int i = 0; i < block.Length; i++)
+            {
+                int startKey = i;
+
+                // Skip to key. On some old OS, the environment block can be corrupted.
+                // Some will not have '=', so we need to check for '\0'. 
+                while (block[i] != '=' && block[i] != '\0')
+                    i++;
+                if (block[i] == '\0')
+                    continue;
+
+                // Skip over environment variables starting with '='
+                if (i - startKey == 0)
+                {
+                    while (block[i] != 0)
+                        i++;
+                    continue;
+                }
+
+                string key = new string(block, startKey, i - startKey);
+                i++;  // skip over '='
+
+                int startValue = i;
+                while (block[i] != 0)
+                    i++; // Read to end of this entry 
+                string value = new string(block, startValue, i - startValue); // skip over 0 handled by for loop's i++
+
+                yield return new KeyValuePair<string, string>(key, value);
+            }
         }
 
         private static void ExitRaw()
