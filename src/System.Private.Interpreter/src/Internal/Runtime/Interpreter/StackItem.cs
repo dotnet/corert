0ed3a0089ec7ba59af3757599845034575e48ff6
Toni Solarin-Sodara <toni.edward@outlook.com>
2018-09-04 10:51:58 +0100

    [Interpreter] Load Constants (#6284)
    This PR adds support for loading constants to the interpreter. The following OpCodes are now supported:
    
    * `ldc.*`
    * `ldnull`
    * `ldstr`

--- src/System.Private.Interpreter/src/Internal/Runtime/Interpreter/StackItem.cs
+++ src/System.Private.Interpreter/src/Internal/Runtime/Interpreter/StackItem.cs
@@ -4,56 +4,66 @@
 
 using System;
 using Internal.IL;
+using Internal.TypeSystem;
 
 namespace Internal.Runtime.Interpreter
 {
     internal abstract class StackItem
     {
-        public StackValueKind Kind { get; set; }
+        public StackValueKind Kind { get; protected set; }
+        public WellKnownType Type { get; protected set; }
     }
 
     internal class StackItem<T> : StackItem
     {
         public T Value { get; }
 
-        public StackItem(T value, StackValueKind kind)
+        public StackItem(T value, StackValueKind kind, WellKnownType type)
         {
             Value = value;
             Kind = kind;
+            Type = type;
         }
     }
 
     internal class Int32StackItem : StackItem<int>
     {
-        public Int32StackItem(int value) : base(value, StackValueKind.Int32)
+        public Int32StackItem(int value) : base(value, StackValueKind.Int32, WellKnownType.Int32)
         {
         }
     }
 
     internal class Int64StackItem : StackItem<long>
     {
-        public Int64StackItem(long value) : base(value, StackValueKind.Int64)
+        public Int64StackItem(long value) : base(value, StackValueKind.Int64, WellKnownType.Int64)
         {
         }
     }
 
-    internal class FloatStackItem : StackItem<double>
+    internal class FloatStackItem : StackItem<float>
     {
-        public FloatStackItem(double value) : base(value, StackValueKind.Float)
+        public FloatStackItem(float value) : base(value, StackValueKind.Float, WellKnownType.Single)
+        {
+        }
+    }
+
+    internal class DoubleStackItem : StackItem<double>
+    {
+        public DoubleStackItem(double value) : base(value, StackValueKind.Float, WellKnownType.Double)
         {
         }
     }
 
     internal class ValueTypeStackItem : StackItem<ValueType>
     {
-        public ValueTypeStackItem(ValueType value) : base(value, StackValueKind.ValueType)
+        public ValueTypeStackItem(ValueType value) : base(value, StackValueKind.ValueType, WellKnownType.ValueType)
         {
         }
     }
 
     internal class ObjectRefStackItem : StackItem<Object>
     {
-        public ObjectRefStackItem(Object value) : base(value, StackValueKind.ObjRef)
+        public ObjectRefStackItem(Object value) : base(value, StackValueKind.ObjRef, WellKnownType.Object)
         {
         }
     }
