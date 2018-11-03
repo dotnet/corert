68c8b022abb11591d4f6219f9ee333c290a147b8
scott <scott.waye@hubse.com>
2018-11-03 16:47:47 -0500

    move fpext and refactor

--- src/ILCompiler.WebAssembly/src/CodeGen/EvaluationStack.cs
+++ src/ILCompiler.WebAssembly/src/CodeGen/EvaluationStack.cs
@@ -456,11 +456,6 @@ namespace Internal.IL
 
         protected override LLVMValueRef ValueAsTypeInternal(LLVMTypeRef type, LLVMBuilderRef builder, bool signExtend)
         {
-            if (Type.IsWellKnownType(WellKnownType.Single) && type.TypeKind == LLVMTypeKind.LLVMDoubleTypeKind)
-            {
-                var loadedVal = ILImporter.LoadValue(builder, RawLLVMValue, Type, LLVMTypeRef.FloatType(), signExtend, $"fpext_Load{Name}");
-                return LLVM.BuildFPExt(builder, loadedVal, type, $"fpext{Name}");
-            }
             return ILImporter.LoadValue(builder, RawLLVMValue, Type, type, signExtend, $"Load{Name}");
         }
     }
