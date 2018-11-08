a5e76d490f67da73ff0434a2f6e168d531cb9f04
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2018-11-08 11:44:03 -0800

    Leap Seconds Support

---
+++ src/System.Private.CoreLib/src/System/DateTime.Win32.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System
+{
+    public readonly partial struct DateTime
+    {
+        private static unsafe bool SystemSupportsLeapSeconds()
+        {
+            Interop.NtDll.SYSTEM_LEAP_SECOND_INFORMATION slsi = new Interop.NtDll.SYSTEM_LEAP_SECOND_INFORMATION();
+            return Interop.NtDll.NtQuerySystemInformation(
+                                    Interop.NtDll.SystemLeapSecondInformation,
+                                    (void *) &slsi,
+                                    sizeof(Interop.NtDll.SYSTEM_LEAP_SECOND_INFORMATION),
+                                    null) == 0 && slsi.Enabled;
+        }
+    }
+}
