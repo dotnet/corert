b5ff5fa73b15c6d73006c3daa4f2e40ea36d18cb
Michal Strehovský <michals@microsoft.com>
2018-11-14 11:47:10 +0100

    Make it possible to get from a module to an assembly

--- src/Common/src/TypeSystem/Ecma/EcmaModule.cs
+++ src/Common/src/TypeSystem/Ecma/EcmaModule.cs
@@ -176,22 +176,22 @@ namespace Internal.TypeSystem.Ecma
 
         private LockFreeReaderHashtable<EntityHandle, IEntityHandleObject> _resolvedTokens;
 
-        internal EcmaModule(TypeSystemContext context, PEReader peReader, MetadataReader metadataReader)
-            : base(context)
+        internal EcmaModule(TypeSystemContext context, PEReader peReader, MetadataReader metadataReader, IAssemblyDesc containingAssembly)
+            : base(context, containingAssembly)
         {
             _peReader = peReader;
             _metadataReader = metadataReader;
             _resolvedTokens = new EcmaObjectLookupHashtable(this);
         }
 
-        public static EcmaModule Create(TypeSystemContext context, PEReader peReader)
+        public static EcmaModule Create(TypeSystemContext context, PEReader peReader, IAssemblyDesc containingAssembly)
         {
             MetadataReader metadataReader = CreateMetadataReader(context, peReader);
 
-            if (metadataReader.IsAssembly)
+            if (containingAssembly == null)
                 return new EcmaAssembly(context, peReader, metadataReader);
             else
-                return new EcmaModule(context, peReader, metadataReader);
+                return new EcmaModule(context, peReader, metadataReader, containingAssembly);
         }
 
         private static MetadataReader CreateMetadataReader(TypeSystemContext context, PEReader peReader)
