aa43ff2b549b6b5a69c15d338da6b41282b50e85
Jan Kotas <jkotas@microsoft.com>
2019-01-07 18:18:03 -0800

    CoreRT updates for files moved to shared partition

--- src/System.Private.CoreLib/shared/System/Runtime/InteropServices/InterfaceTypeAttribute.cs
+++ src/System.Private.CoreLib/shared/System/Runtime/InteropServices/InterfaceTypeAttribute.cs
@@ -2,22 +2,20 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-
 namespace System.Runtime.InteropServices
 {
     [AttributeUsage(AttributeTargets.Interface, Inherited = false)]
     public sealed class InterfaceTypeAttribute : Attribute
     {
-        internal ComInterfaceType _val;
         public InterfaceTypeAttribute(ComInterfaceType interfaceType)
         {
-            _val = interfaceType;
+            Value = interfaceType;
         }
         public InterfaceTypeAttribute(short interfaceType)
         {
-            _val = (ComInterfaceType)interfaceType;
+            Value = (ComInterfaceType)interfaceType;
         }
-        public ComInterfaceType Value { get { return _val; } }
+
+        public ComInterfaceType Value { get; }
     }
 }
