21bd5a1c0bba5b1ed2e55dbe49d4017e54ccfd38
Konstantin Baladurin <k.baladurin@samsung.com>
2019-01-11 18:13:10 +0300

    [CppCodeGen] Fix importing ldftn (#6810)
    In the case of ldvirtftn we should pop object off the stack

--- src/ILCompiler.CppCodeGen/src/CppCodeGen/ILToCppImporter.cs
+++ src/ILCompiler.CppCodeGen/src/CppCodeGen/ILToCppImporter.cs
@@ -2281,6 +2281,10 @@ namespace Internal.IL
                 }
                 else
                 {
+                    // pop object reference off the stack
+                    if (opCode == ILOpcode.ldvirtftn)
+                        _stack.Pop();
+
                     bool exactContextNeedsRuntimeLookup;
                     if (method.HasInstantiation)
                     {
