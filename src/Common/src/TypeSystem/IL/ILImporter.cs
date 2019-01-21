16a9c475d86fb1f60314b8b54009dfe6a322c09e
Toni Solarin-Sodara <toni.edward@outlook.com>
2019-01-21 21:49:35 +0100

    move StackValueKind to shared standalone file (#5011)

--- src/Common/src/TypeSystem/IL/ILImporter.cs
+++ src/Common/src/TypeSystem/IL/ILImporter.cs
@@ -1,7 +1,3 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
 using System;
 
 using Internal.TypeSystem;
@@ -9,29 +5,6 @@ using Internal.IL;
 
 namespace Internal.IL
 {
-    //
-    // Corresponds to "I.12.3.2.1 The evaluation stack" in ECMA spec
-    //
-    internal enum StackValueKind
-    {
-        /// <summary>An unknow type.</summary>
-        Unknown,
-        /// <summary>Any signed or unsigned integer values that can be represented as a 32-bit entity.</summary>
-        Int32,
-        /// <summary>Any signed or unsigned integer values that can be represented as a 64-bit entity.</summary>
-        Int64,
-        /// <summary>Underlying platform pointer type represented as an integer of the appropriate size.</summary>
-        NativeInt,
-        /// <summary>Any float value.</summary>
-        Float,
-        /// <summary>A managed pointer.</summary>
-        ByRef,
-        /// <summary>An object reference.</summary>
-        ObjRef,
-        /// <summary>A value type which is not any of the primitive one.</summary>
-        ValueType
-    }
-
     internal partial class ILImporter
     {
         private BasicBlock[] _basicBlocks; // Maps IL offset to basic block
--- src/Common/src/TypeSystem/IL/ILReader.cs
+++ src/Common/src/TypeSystem/IL/ILReader.cs
@@ -7,29 +7,6 @@ using Internal.TypeSystem;
 
 namespace Internal.IL
 {
-    //
-    // Corresponds to "I.12.3.2.1 The evaluation stack" in ECMA spec
-    //
-    internal enum StackValueKind
-    {
-        /// <summary>An unknow type.</summary>
-        Unknown,
-        /// <summary>Any signed or unsigned integer values that can be represented as a 32-bit entity.</summary>
-        Int32,
-        /// <summary>Any signed or unsigned integer values that can be represented as a 64-bit entity.</summary>
-        Int64,
-        /// <summary>Underlying platform pointer type represented as an integer of the appropriate size.</summary>
-        NativeInt,
-        /// <summary>Any float value.</summary>
-        Float,
-        /// <summary>A managed pointer.</summary>
-        ByRef,
-        /// <summary>An object reference.</summary>
-        ObjRef,
-        /// <summary>A value type which is not any of the primitive one.</summary>
-        ValueType
-    }
-
     internal struct ILReader
     {
         private int _currentOffset;
