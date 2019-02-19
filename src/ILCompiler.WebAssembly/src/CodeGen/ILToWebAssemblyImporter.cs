5b5ea98b99e765ef49df2b9b4793f8efbb29577e
scott <scott.waye@hubse.com>
2019-02-19 18:56:52 -0500

    remove using

--- src/ILCompiler.WebAssembly/src/CodeGen/ILToWebAssemblyImporter.cs
+++ src/ILCompiler.WebAssembly/src/CodeGen/ILToWebAssemblyImporter.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using Internal.TypeSystem;
 using ILCompiler;
 using LLVMSharp;
