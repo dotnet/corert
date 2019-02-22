dotnet/corert

commit e652d4d24cdbb1b89a2f63fe3e40a45dd83afc76

Move Timer to shared CoreLib partition.
Update to code from CoreCLR PR, add EventWaitHandle.Set static method.

src/System.Private.CoreLib/src/System/Threading/Timer.Unix.cs
@@ -26,11 +26,12 @@ namespace System.Threading
         /// </summary>
         private static volatile int s_nextTimerDuration;
 
-        private void SetTimer(uint actualDuration)
+        private TimerQueue(int id)
         {
-            // This function is called with the TimerQueue lock acquired
-            Debug.Assert(Lock.IsAcquired);
+        }
 
+        private bool SetTimer(uint actualDuration)
+        {
             // Note: AutoResetEvent.WaitOne takes an Int32 value as a timeout.
             // The TimerQueue code ensures that timer duration is not greater than max Int32 value
             Debug.Assert(actualDuration <= (uint)int.MaxValue);
@@ -50,6 +51,8 @@ namespace System.Threading
             {
                 s_timerEvent.Set();
             }
+
+            return true;
         }
 
 
@@ -57,7 +60,7 @@ namespace System.Threading
         /// This method is executed on a dedicated a timer thread. Its purpose is
         /// to handle timer request and notify the TimerQueue when a timer expires.
         /// </summary>
-        private static void TimerThread()
+        private void TimerThread()
         {
             int currentTimerInterval;
 
@@ -92,7 +95,7 @@ namespace System.Threading
                 // Check whether TimerQueue needs to process expired timers.
                 if (timerHasExpired)
                 {
-                    Instance.FireNextTimers();
+                    FireNextTimers();
 
                     // When FireNextTimers() installs a new timer, it also sets the timer event.
                     // Reset the event so the timer thread is not woken up right away unnecessary.
@@ -113,33 +116,6 @@ namespace System.Threading
             get
             {
                 return Environment.TickCount;
-            }
-        }
-    }
-
-    internal sealed partial class TimerQueueTimer
-    {
-        private void SignalNoCallbacksRunning()
-        {
-            object toSignal = _notifyWhenNoCallbacksRunning;
-            Debug.Assert(toSignal is WaitHandle || toSignal is Task<bool>);
-
-            if (toSignal is WaitHandle wh)
-            {
-                SafeWaitHandle waitHandle = wh.SafeWaitHandle;
-                waitHandle.DangerousAddRef();
-                try
-                {
-                    WaitSubsystem.SetEvent(waitHandle.DangerousGetHandle());
-                }
-                finally
-                {
-                    waitHandle.DangerousRelease();
-                }
-            }
-            else
-            {
-                ((Task<bool>)toSignal).TrySetResult(true);
             }
         }
     }

src/System.Private.CoreLib/src/System.Private.CoreLib.csproj
@@ -292,7 +292,6 @@
     <Compile Include="System\Threading\Tasks\AsyncCausalityTracer.WinRT.cs" Condition="'$(EnableWinRT)' == 'true'" />
     <Compile Include="System\Threading\ThreadPool.CoreRT.cs" />
     <Compile Include="System\Threading\ThreadPoolCallbackWrapper.cs" />
-    <Compile Include="System\Threading\Timer.cs" />
     <Compile Include="System\Threading\Volatile.cs" />
     <Compile Include="System\Threading\WaitHandle.cs" />
     <Compile Include="System\Threading\WaitHandleArray.cs" />

src/System.Private.CoreLib/src/System/Diagnostics/Tracing/FrameworkEventSource.cs
@@ -19,6 +19,8 @@ namespace System.Diagnostics.Tracing
             public const EventKeywords ThreadTransfer = (EventKeywords)0x0010;
         }
 
+        public static bool IsInitialized { get => true; }
+
         public bool IsEnabled(EventLevel level, EventKeywords keywords)
         {
             return false;
@@ -29,6 +31,14 @@ namespace System.Diagnostics.Tracing
         }
 
         public void ThreadPoolDequeueWorkObject(object workID)
+        {
+        }
+
+        public void ThreadTransferSendObj(object id, int kind, string info, bool multiDequeues, int intInfo1, int intInfo2)
+        {
+        }
+
+        public void ThreadTransferReceiveObj(object id, int kind, string info)
         {
         }
     }

src/System.Private.CoreLib/src/System/Threading/EventWaitHandle.Unix.cs
@@ -53,6 +53,20 @@ namespace System.Threading
             }
         }
 
+        internal static bool Set(SafeWaitHandle waitHandle)
+        {
+            waitHandle.DangerousAddRef();
+            try
+            {
+                WaitSubsystem.SetEvent(waitHandle.DangerousGetHandle());
+                return true;
+            }
+            finally
+            {
+                waitHandle.DangerousRelease();
+            }
+        }
+
         private SafeWaitHandle ValidateHandle()
         {
             // The field value is modifiable via the public <see cref="WaitHandle.SafeWaitHandle"/> property, save it locally

src/System.Private.CoreLib/src/System/Threading/Timer.Windows.cs
@@ -14,22 +14,29 @@ namespace System.Threading
     internal partial class TimerQueue
     {
         private IntPtr _nativeTimer;
+        private readonly int _id;
+
+        private TimerQueue(int id)
+        {
+            _id = id;
+        }
 
         [NativeCallable(CallingConvention = CallingConvention.StdCall)]
         private static void TimerCallback(IntPtr instance, IntPtr context, IntPtr timer)
         {
+            int id = (int)context;
             var wrapper = ThreadPoolCallbackWrapper.Enter();
-            Instance.FireNextTimers();
+            Instances[id].FireNextTimers();
             wrapper.Exit();
         }
 
-        private unsafe void SetTimer(uint actualDuration)
+        private unsafe bool SetTimer(uint actualDuration)
         {
             if (_nativeTimer == IntPtr.Zero)
             {
                 IntPtr nativeCallback = AddrofIntrinsics.AddrOf<Interop.mincore.TimerCallback>(TimerCallback);
 
-                _nativeTimer = Interop.mincore.CreateThreadpoolTimer(nativeCallback, IntPtr.Zero, IntPtr.Zero);
+                _nativeTimer = Interop.mincore.CreateThreadpoolTimer(nativeCallback, (IntPtr)_id, IntPtr.Zero);
                 if (_nativeTimer == IntPtr.Zero)
                     throw new OutOfMemoryException();
             }
@@ -37,6 +44,8 @@ namespace System.Threading
             // Negative time indicates the amount of time to wait relative to the current time, in 100 nanosecond units
             long dueTime = -10000 * (long)actualDuration;
             Interop.mincore.SetThreadpoolTimer(_nativeTimer, &dueTime, 0, 0);
+
+            return true;
         }
 
         //
@@ -59,24 +68,6 @@ namespace System.Threading
 
                 // convert to 100ns to milliseconds, and truncate to 32 bits.
                 return (int)(uint)(time100ns / 10000);
-            }
-        }
-    }
-
-    internal sealed partial class TimerQueueTimer
-    {
-        private void SignalNoCallbacksRunning()
-        {
-            object toSignal = _notifyWhenNoCallbacksRunning;
-            Debug.Assert(toSignal is WaitHandle || toSignal is Task<bool>);
-
-            if (toSignal is WaitHandle wh)
-            {
-                Interop.Kernel32.SetEvent(wh.SafeWaitHandle);
-            }
-            else
-            {
-                ((Task<bool>)toSignal).TrySetResult(true);
             }
         }
     }

commit 4b1bd0fa8a99729adc767a7e0375b79c672d7e87

Fix bug introduced in commit e652d4d24cdbb1b89a2f63fe3e40a45dd83afc76 (#6905 / #6898)

src/System.Private.CoreLib/src/System/Threading/Timer.Unix.cs
@@ -19,12 +19,12 @@ namespace System.Threading
         /// This event is used by the timer thread to wait for timer expiration. It is also
         /// used to notify the timer thread that a new timer has been set.
         /// </summary>
-        private static AutoResetEvent s_timerEvent;
+        private AutoResetEvent _timerEvent;
 
         /// <summary>
         /// This field stores the value of next timer that the timer thread should install.
         /// </summary>
-        private static volatile int s_nextTimerDuration;
+        private volatile int _nextTimerDuration;
 
         private TimerQueue(int id)
         {
@@ -35,21 +35,21 @@ namespace System.Threading
             // Note: AutoResetEvent.WaitOne takes an Int32 value as a timeout.
             // The TimerQueue code ensures that timer duration is not greater than max Int32 value
             Debug.Assert(actualDuration <= (uint)int.MaxValue);
-            s_nextTimerDuration = (int)actualDuration;
+            _nextTimerDuration = (int)actualDuration;
 
             // If this is the first time the timer is set then we need to create a thread that
             // will manage and respond to timer requests. Otherwise, simply signal the timer thread
             // to notify it that the timer duration has changed.
-            if (s_timerEvent == null)
+            if (_timerEvent == null)
             {
-                s_timerEvent = new AutoResetEvent(false);
+                _timerEvent = new AutoResetEvent(false);
                 RuntimeThread thread = RuntimeThread.Create(TimerThread, 0);
                 thread.IsBackground = true; // Keep this thread from blocking process shutdown
                 thread.Start();
             }
             else
             {
-                s_timerEvent.Set();
+                _timerEvent.Set();
             }
 
             return true;
@@ -65,7 +65,7 @@ namespace System.Threading
             int currentTimerInterval;
 
             // Get wait time for the next timer
-            currentTimerInterval = Interlocked.Exchange(ref s_nextTimerDuration, Timeout.Infinite);
+            currentTimerInterval = Interlocked.Exchange(ref _nextTimerDuration, Timeout.Infinite);
 
             for (;;)
             {
@@ -73,7 +73,7 @@ namespace System.Threading
                 // We will be woken up because either 1) the wait times out, which will indicate that
                 // the current timer has expired and/or 2) the TimerQueue installs a new (earlier) timer.
                 int startWait = TickCount;
-                bool timerHasExpired = !s_timerEvent.WaitOne(currentTimerInterval);
+                bool timerHasExpired = !_timerEvent.WaitOne(currentTimerInterval);
                 uint elapsedTime = (uint)(TickCount - startWait);
 
                 // The timer event can be set after this thread reads the new timer interval but before it enters
@@ -99,11 +99,11 @@ namespace System.Threading
 
                     // When FireNextTimers() installs a new timer, it also sets the timer event.
                     // Reset the event so the timer thread is not woken up right away unnecessary.
-                    s_timerEvent.Reset();
+                    _timerEvent.Reset();
                     currentTimerInterval = Timeout.Infinite;
                 }
 
-                int nextTimerInterval = Interlocked.Exchange(ref s_nextTimerDuration, Timeout.Infinite);
+                int nextTimerInterval = Interlocked.Exchange(ref _nextTimerDuration, Timeout.Infinite);
                 if (nextTimerInterval != Timeout.Infinite)
                 {
                     currentTimerInterval = nextTimerInterval;