a1d7d27a6dd2940abc87408349c5909110fed45a
Simon Nattress <nattress@gmail.com>
2019-03-01 16:25:32 -0800

    Update CoreCLR test scripting and tests
    The scripts to support running downloaded CoreCLR tests are from 2017 and have received some compelling updates since then:
    
    - No longer depend on desktop Xunit runner (`xunit.runner.msbuild`) and instead uses `xunit.console`. This should unblock https://github.com/dotnet/corert/issues/7032.
    - Cross platform Python script to drive test execution which lets us remove a bunch of the scripting we brought over last time.
    - All OSes now share the same test filter `*.issues.targets` files
    
    The process here was similar to last time - drop runtest.py into this repo and then bring over enough scripts to make it run the test tree. Alternatively, we could have re-written this scripting to clone CoreCLR, build the product and tests, and run them with a custom launcher that first runs CPAOT / ILC. This takes about 25 minutes for the Pri0 tests and would slow our CI down significantly. The Pri0 test binaries are stored in an Azure blob which downloads and extracts in 1 minute.

--- tests/CoreCLR/build-and-run-test.sh
+++ tests/CoreCLR/build-and-run-test.sh
@@ -5,10 +5,14 @@
 # the unix CoreCLR tests don't have a custom runner override environment variable.
 # See issue https://github.com/dotnet/coreclr/issues/9007
 
-export TestExecutable=$1
-export TestFileName=${TestExecutable%.*}
+export TestExecutable=$2
+export TestFileName=${TestExecutable%.exe}
 
+echo "corerun. TestExecutable=${TestExecutable}. Test Filename: ${TestFileName}"
 source "$CoreRT_TestRoot/coredump_handling.sh"
+
+# Cleanup prvious test run artifacts
+rm -r native 2>/dev/null
 
 if [[ $CoreRT_EnableCoreDumps == 1 ]]; then
     set_up_core_dump_generation
@@ -18,14 +22,15 @@ cp $CoreRT_TestRoot/CoreCLR/Test.csproj .
 
 __msbuild_dir=${CoreRT_TestRoot}/../Tools
 echo ${__msbuild_dir}/msbuild.sh /m /p:IlcPath=${CoreRT_ToolchainDir} /p:Configuration=${CoreRT_BuildType} Test.csproj
-${__msbuild_dir}/msbuild.sh /m /p:IlcPath=${CoreRT_ToolchainDir} /p:Configuration=${CoreRT_BuildType} Test.csproj
+${__msbuild_dir}/msbuild.sh /m /p:IlcPath=${CoreRT_ToolchainDir} /p:Configuration=${CoreRT_BuildType} Test.csproj /v:d
 
 if [[ $CoreRT_EnableCoreDumps == 1 ]]; then
     # Handle any core files generated when running the test IL through the toolchain.
     inspect_and_delete_core_files $CoreRT_ToolchainDir/corerun "$CoreRT_ToolchainDir"
 fi
 
-# Remove the test executable from the arg list so it isn't passed to test execution
+# Remove the test directory and executable from the arg list so it isn't passed to test execution
+shift
 shift
 
 testExtRepo=$( dirname ${CoreRT_TestRoot} )/tests_downloaded/CoreCLR/
@@ -66,3 +71,4 @@ fi
 rm -r native 2>/dev/null
 
 exit $testScriptExitCode
+
