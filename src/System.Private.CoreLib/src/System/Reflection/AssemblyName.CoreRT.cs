afd93b7c2564ade79a57b3183ce34811a2136f45
EgorBo <egorbo@gmail.com>
2019-04-10 17:43:12 +0300

    Address feedback

--- src/System.Private.CoreLib/src/System/Reflection/AssemblyName.CoreRT.cs
+++ src/System.Private.CoreLib/src/System/Reflection/AssemblyName.CoreRT.cs
@@ -8,18 +8,25 @@ namespace System.Reflection
 {
     public sealed partial class AssemblyName : ICloneable, IDeserializationCallback, ISerializable
     {
-        public void nInit()
+        public AssemblyName(string assemblyName)
         {
+            if (assemblyName == null)
+                throw new ArgumentNullException(nameof(assemblyName));
+            if ((assemblyName.Length == 0) ||
+                (assemblyName[0] == '\0'))
+                throw new ArgumentException(SR.Format_StringZeroLength);
+
+            _name = assemblyName;
             RuntimeAssemblyName runtimeAssemblyName = AssemblyNameParser.Parse(_name);
             runtimeAssemblyName.CopyToAssemblyName(this);
         }
 
-        private byte[] nGetPublicKeyToken()
+        private byte[] ComputePublicKeyToken()
         {
             return AssemblyNameHelpers.ComputePublicKeyToken(_publicKey);
         }
 
-        public static AssemblyName nGetFileInformation(string assemblyFile)
+        public static AssemblyName GetFileInformationCore(string assemblyFile)
         {
             throw new PlatformNotSupportedException(SR.Arg_PlatformNotSupported_AssemblyName_GetAssemblyName);
         }
