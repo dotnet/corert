580deced96716f8154051f929caedd2a45804daa
Krzysztof Wicher <mordotymoja@gmail.com>
2019-04-14 05:28:50 -0700

    remove duplicate GetCurrentThreadId (#7289)

--- src/System.Private.CoreLib/src/System/Environment.CoreRT.Windows.cs
+++ src/System.Private.CoreLib/src/System/Environment.CoreRT.Windows.cs
@@ -10,7 +10,7 @@ namespace System
 {
     public static partial class Environment
     {
-        internal static int CurrentNativeThreadId => unchecked((int)Interop.mincore.GetCurrentThreadId());
+        internal static int CurrentNativeThreadId => unchecked((int)Interop.Kernel32.GetCurrentThreadId());
 
         internal static long TickCount64 => (long)Interop.mincore.GetTickCount64();
     }
