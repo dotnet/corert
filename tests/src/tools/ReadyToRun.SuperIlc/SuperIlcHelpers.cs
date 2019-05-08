4724c33e39386e022fd851919b9842a03ec488cd
Simon Nattress <nattress@gmail.com>
2019-05-08 15:08:56 -0700

    Add SuperIlc compile-nuget command
    Add support for compiling assemblies from Nuget packages with Crossgen / CPAOT to validate them on real-world code. The compile-nuget command takes a list of Nuget packages and for each package creates an empty console app, adds the package to the app, publishes, and then runs Crossgen / CPAOT. This initial command does not run the app and is meant for gathering data on noisiness of compile logs, ensuring compilation regressions don't happen, tracking ready-to-run binary sizes, etc.
    
    Top100Nuget.txt is provided as an example, and contains the 100 most popular Nuget packages on nuget.org as of this commit.

--- tests/src/tools/ReadyToRun.SuperIlc/SuperIlcHelpers.cs
+++ tests/src/tools/ReadyToRun.SuperIlc/SuperIlcHelpers.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -17,6 +21,7 @@ namespace ReadyToRun.SuperIlc
         public bool NoExe { get; set; }
         public bool NoEtw { get; set; }
         public bool NoCleanup { get; set; }
+        public FileInfo PackageList { get; set; }
         public bool Sequential { get; set; }
         public bool Framework { get; set; }
         public bool UseFramework { get; set; }
