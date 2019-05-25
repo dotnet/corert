60915f867b279e2881913fd6733b556b986dd2bd
Simon Nattress <nattress@gmail.com>
2019-05-24 17:10:06 -0700

    Restore CoreCLRRuntime on Linux (#7448)
    * Restore CoreCLRRuntime on Linux
    
    When running tests, invoke the test build to restore the runtime / corefx / corerun to a well known folder needed to run Ready-to-run tests against

--- buildscripts/build-tests.sh
+++ buildscripts/build-tests.sh
@@ -10,6 +10,25 @@ if [ "$BUILDVARS_DONE" != 1 ]; then
     . $scriptRoot/buildvars-setup.sh $*
 fi
 
+# Restore CoreCLR for ReadyToRun testing
+${__dotnetclipath}/dotnet msbuild ${__ProjectRoot}/tests/dirs.proj /nologo /t:Restore "/flp:v=normal;LogFile=$__TestBuildLog" /p:NuPkgRid=$__NugetRuntimeId /maxcpucount /p:OSGroup=$__BuildOS /p:Configuration=$__BuildType /p:Platform=$__BuildArch $__ExtraMsBuildArgs
+
+echo Commencing build of test components for $__BuildOS.$__BuildArch.$__BuildType
+echo
+
+
+${__dotnetclipath}/dotnet msbuild ${__ProjectRoot}/tests/dirs.proj /nologo "/flp:v=normal;LogFile=$__TestBuildLog" /p:NuPkgRid=$__NugetRuntimeId /maxcpucount /p:OSGroup=$__BuildOS /p:Configuration=$__BuildType /p:Platform=$__BuildArch $__ExtraMsBuildArgs "/p:RepoPath=$__ProjectRoot"  "/p:RepoLocalBuild=true" /nodeReuse:false
+export BUILDERRORLEVEL=$?
+if [ $BUILDERRORLEVEL != 0 ]; then
+    echo Test build failed with exit code $BUILDERRORLEVEL. Refer to $__TestBuildLog for details.
+    exit $BUILDERRORLEVEL
+fi
+
+# No run. Only build >:|
+if [ -n "$__BuildTests" ]; then
+    exit 0
+fi
+
 pushd ${__ProjectRoot}/tests
 source ${__ProjectRoot}/tests/runtest.sh $__BuildOS $__BuildArch $__BuildType -cross $__CrossBuild -dotnetclipath $__dotnetclipath
 TESTERRORLEVEL=$?
