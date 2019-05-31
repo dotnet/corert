dotnet/corert

commit b6d5e3974aa9cdc2ed22a0c48272036f539f0827

Fix building Ubuntu shared libraries (#7249)
* fix build of shared library for linux-x64

* fix SharedLibrary test

* fix StubDispatch for macos

* avoid PLT relocation for RhpCidResolve

* Use NodeAliases to distinguish Global BlobNodes

* Use alternate symbol name if available

* use alternate name only for BlobNodes

src/ILCompiler.Compiler/src/Compiler/DependencyAnalysis/ObjectWriter.cs
@@ -180,14 +180,14 @@ namespace ILCompiler.DependencyAnalysis
         }
 
         [DllImport(NativeObjectWriterFileName)]
-        private static extern void EmitSymbolDef(IntPtr objWriter, byte[] symbolName);
-        public void EmitSymbolDef(byte[] symbolName)
+        private static extern void EmitSymbolDef(IntPtr objWriter, byte[] symbolName, int global);
+        public void EmitSymbolDef(byte[] symbolName, bool global = false)
         {
-            EmitSymbolDef(_nativeObjectWriter, symbolName);
+            EmitSymbolDef(_nativeObjectWriter, symbolName, global ? 1 : 0);
         }
-        public void EmitSymbolDef(Utf8StringBuilder symbolName)
+        public void EmitSymbolDef(Utf8StringBuilder symbolName, bool global = false)
         {
-            EmitSymbolDef(_nativeObjectWriter, symbolName.Append('\0').UnderlyingArray);
+            EmitSymbolDef(_nativeObjectWriter, symbolName.Append('\0').UnderlyingArray, global ? 1 : 0);
         }
 
         [DllImport(NativeObjectWriterFileName)]
@@ -870,7 +870,7 @@ namespace ILCompiler.DependencyAnalysis
                         AppendExternCPrefix(_sb);
                         _sb.Append(alternateName);
 
-                        EmitSymbolDef(_sb);
+                        EmitSymbolDef(_sb, global: true);
                     }
                 }
             }
@@ -980,13 +980,13 @@ namespace ILCompiler.DependencyAnalysis
                                                             ObjectNodeSection.ManagedCodeStartSection :
                                                             objectWriter.GetSharedSection(ObjectNodeSection.ManagedCodeStartSection, "__managedcode_a");
                     objectWriter.SetSection(codeStartSection);
-                    objectWriter.EmitSymbolDef(new Utf8StringBuilder().Append("__managedcode_a"));
+                    objectWriter.EmitSymbolDef(new Utf8StringBuilder().Append("__managedcode_a"), global: true);
                     objectWriter.EmitIntValue(0, 1);
                     ObjectNodeSection codeEndSection = factory.CompilationModuleGroup.IsSingleFileCompilation ?
                                                             ObjectNodeSection.ManagedCodeEndSection :
                                                             objectWriter.GetSharedSection(ObjectNodeSection.ManagedCodeEndSection, "__managedcode_z");
                     objectWriter.SetSection(codeEndSection);
-                    objectWriter.EmitSymbolDef(new Utf8StringBuilder().Append("__managedcode_z"));
+                    objectWriter.EmitSymbolDef(new Utf8StringBuilder().Append("__managedcode_z"), global: true);
                     objectWriter.EmitIntValue(1, 1);
                 }
                 else

src/ILCompiler.Compiler/src/Compiler/Compilation.cs
@@ -492,7 +492,9 @@ namespace ILCompiler
 
             public void RootReadOnlyDataBlob(byte[] data, int alignment, string reason, string exportName)
             {
-                _graph.AddRoot(_factory.ReadOnlyDataBlob(exportName, data, alignment), reason);
+                var blob = _factory.ReadOnlyDataBlob("__readonlydata_" + exportName, data, alignment);
+                _graph.AddRoot(blob, reason);
+                _factory.NodeAliases.Add(blob, exportName);
             }
         }
 

src/ILCompiler.CppCodeGen/src/CppCodeGen/CppWriter.cs
@@ -109,8 +109,10 @@ namespace ILCompiler.CppCodeGen
         {
             foreach (var nodeAlias in _compilation.NodeFactory.NodeAliases)
             {
-                var methodNode = (CppMethodCodeNode)nodeAlias.Key;
-                _externCSignatureMap.Add(nodeAlias.Value, methodNode.Method.Signature);
+                if (nodeAlias.Key is CppMethodCodeNode methodNode)
+                {
+                    _externCSignatureMap.Add(nodeAlias.Value, methodNode.Method.Signature);
+                }
             }
         }
 
@@ -495,21 +497,21 @@ namespace ILCompiler.CppCodeGen
 
         public string GetCppSymbolNodeName(NodeFactory factory, ISymbolNode node)
         {
-            if (node is RuntimeMethodHandleNode)
+            if (node is RuntimeMethodHandleNode r)
             {
-                return GetCppRuntimeMethodHandleName(node as RuntimeMethodHandleNode);
+                return GetCppRuntimeMethodHandleName(r);
             }
-            else if (node is NativeLayoutSignatureNode)
+            else if (node is NativeLayoutSignatureNode n)
             {
-                return GetCppNativeLayoutSignatureName(factory, node as NativeLayoutSignatureNode);
+                return GetCppNativeLayoutSignatureName(factory, n);
             }
-            else if (node is FatFunctionPointerNode)
+            else if (node is FatFunctionPointerNode f)
             {
-                return GetCppFatFunctionPointerName(node as FatFunctionPointerNode);
+                return GetCppFatFunctionPointerName(f);
             }
             else
             {
-                return node.GetMangledName(factory.NameMangler);
+                return factory.GetSymbolAlternateName(node) ?? node.GetMangledName(factory.NameMangler);
             }
         }
 

src/ILCompiler.WebAssembly/src/CodeGen/WebAssemblyObjectWriter.cs
@@ -23,7 +23,7 @@ namespace ILCompiler.DependencyAnalysis
     /// </summary>
     internal class WebAssemblyObjectWriter : IDisposable
     {
-        public static string GetBaseSymbolName(ISymbolNode symbol, NameMangler nameMangler, bool objectWriterUse = false)
+        private string GetBaseSymbolName(ISymbolNode symbol, NameMangler nameMangler, bool objectWriterUse = false)
         {
             if (symbol is WebAssemblyMethodCodeNode)
             {
@@ -33,13 +33,14 @@ namespace ILCompiler.DependencyAnalysis
             if (symbol is ObjectNode)
             {
                 ISymbolDefinitionNode symbolDefNode = (ISymbolDefinitionNode)symbol;
+                var symbolName = _nodeFactory.GetSymbolAlternateName(symbolDefNode) ?? symbol.GetMangledName(nameMangler);
                 if (symbolDefNode.Offset == 0)
                 {
-                    return symbol.GetMangledName(nameMangler);
+                    return symbolName;
                 }
                 else
                 {
-                    return symbol.GetMangledName(nameMangler) + "___REALBASE";
+                    return symbolName + "___REALBASE";
                 }
             }
             else if (symbol is ObjectAndOffsetSymbolNode)

src/Native/ObjWriter/objwriter.cpp
@@ -319,9 +319,14 @@ void ObjectWriter::EmitIntValue(uint64_t Value, unsigned Size) {
   Streamer->EmitIntValue(Value, Size);
 }
 
-void ObjectWriter::EmitSymbolDef(const char *SymbolName) {
+void ObjectWriter::EmitSymbolDef(const char *SymbolName, bool global) {
   MCSymbol *Sym = OutContext->getOrCreateSymbol(Twine(SymbolName));
-  Streamer->EmitSymbolAttribute(Sym, MCSA_Global);
+
+  if (global) {
+    Streamer->EmitSymbolAttribute(Sym, MCSA_Global);
+  } else {
+    Streamer->EmitSymbolAttribute(Sym, MCSA_Local);
+  }
 
   // A Thumb2 function symbol should be marked with an appropriate ELF
   // attribute to make later computation of a relocation address value correct
@@ -390,7 +395,10 @@ int ObjectWriter::EmitSymbolRef(const char *SymbolName,
     break;
   case RelocType::IMAGE_REL_BASED_REL32:
     Size = 4;
-    IsPCRel = true;
+    IsPCRel = true;	
+    // PLT is valid only for code symbols,
+    // but there is shouldn't be references to global data symbols
+    Kind = MCSymbolRefExpr::VK_PLT;
     break;
   case RelocType::IMAGE_REL_BASED_RELPTR32:
     Size = 4;

src/Native/ObjWriter/objwriter.h
@@ -63,7 +63,7 @@ public:
   void EmitAlignment(int ByteAlignment);
   void EmitBlob(int BlobSize, const char *Blob);
   void EmitIntValue(uint64_t Value, unsigned Size);
-  void EmitSymbolDef(const char *SymbolName);
+  void EmitSymbolDef(const char *SymbolName, bool global);
   void EmitWinFrameInfo(const char *FunctionName, int StartOffset,
                         int EndOffset, const char *BlobSymbolName);
   int EmitSymbolRef(const char *SymbolName, RelocType RelocType, int Delta);
@@ -225,9 +225,9 @@ DLL_EXPORT void EmitIntValue(ObjectWriter *OW, uint64_t Value, unsigned Size) {
   OW->EmitIntValue(Value, Size);
 }
 
-DLL_EXPORT void EmitSymbolDef(ObjectWriter *OW, const char *SymbolName) {
+DLL_EXPORT void EmitSymbolDef(ObjectWriter *OW, const char *SymbolName, bool global) {
   assert(OW && "ObjWriter is null");
-  OW->EmitSymbolDef(SymbolName);
+  OW->EmitSymbolDef(SymbolName, global);
 }
 
 DLL_EXPORT int EmitSymbolRef(ObjectWriter *OW, const char *SymbolName,

src/Native/Runtime/amd64/AllocFast.S
@@ -115,7 +115,7 @@ LOCAL_LABEL(NewOutOfMemory):
 
         POP_COOP_PINVOKE_FRAME
 
-        jmp         C_FUNC(RhExceptionHandling_FailedAllocation)
+        jmp         EXTERNAL_C_FUNC(RhExceptionHandling_FailedAllocation)
 NESTED_END RhpNewObject, _TEXT
 
 
@@ -193,7 +193,7 @@ LOCAL_LABEL(StringSizeOverflow):
 
         // rdi holds EEType pointer already
         xor         esi, esi            // Indicate that we should throw OOM.
-        jmp         C_FUNC(RhExceptionHandling_FailedAllocation)
+        jmp         EXTERNAL_C_FUNC(RhExceptionHandling_FailedAllocation)
 
 NESTED_END RhNewString, _TEXT
 
@@ -276,7 +276,7 @@ LOCAL_LABEL(ArraySizeOverflow):
 
         // rdi holds EEType pointer already
         mov         esi, 1              // Indicate that we should throw OverflowException
-        jmp         C_FUNC(RhExceptionHandling_FailedAllocation)
+        jmp         EXTERNAL_C_FUNC(RhExceptionHandling_FailedAllocation)
 
 NESTED_END RhpNewArray, _TEXT
 
@@ -338,7 +338,7 @@ LOCAL_LABEL(ArrayOutOfMemory):
 
         POP_COOP_PINVOKE_FRAME
 
-        jmp         C_FUNC(RhExceptionHandling_FailedAllocation)
+        jmp         EXTERNAL_C_FUNC(RhExceptionHandling_FailedAllocation)
 
 NESTED_END RhpNewArrayRare, _TEXT
 

src/Native/Runtime/amd64/ExceptionHandling.S
@@ -69,7 +69,7 @@ NESTED_ENTRY RhpThrowHwEx, _TEXT, NoHandler
 
         // rdi still contains the exception code
         // rsi contains the address of the ExInfo
-        call    C_FUNC(RhThrowHwEx)
+        call    EXTERNAL_C_FUNC(RhThrowHwEx)
 
         EXPORT_POINTER_TO_ADDRESS PointerToRhpThrowHwEx2
 
@@ -150,7 +150,7 @@ NESTED_ENTRY RhpThrowEx, _TEXT, NoHandler
 
         // rdi still contains the exception object
         // rsi contains the address of the ExInfo
-        call    C_FUNC(RhThrowEx)
+        call    EXTERNAL_C_FUNC(RhThrowEx)
 
         EXPORT_POINTER_TO_ADDRESS PointerToRhpThrowEx2
 
@@ -221,7 +221,7 @@ NESTED_ENTRY RhpRethrow, _TEXT, NoHandler
 
         // rdi contains the currently active ExInfo
         // rsi contains the address of the new ExInfo
-        call    C_FUNC(RhRethrow)
+        call    EXTERNAL_C_FUNC(RhRethrow)
 
         EXPORT_POINTER_TO_ADDRESS PointerToRhpRethrow2
 

commit b796e344489f8ce6e8101740b40495c945721333

Emit all symbols as global on Windows
Symbols don't end up in the PDB otherwise and the only things with symbols are methods with debug information and things that have a non-null `_nodeFactory.GetSymbolAlternateName`.

This regressed in #7249.

src/ILCompiler.Compiler/src/Compiler/DependencyAnalysis/ObjectWriter.cs
@@ -862,9 +862,12 @@ namespace ILCompiler.DependencyAnalysis
                     AppendExternCPrefix(_sb);
                     name.AppendMangledName(_nodeFactory.NameMangler, _sb);
 
+                    // Emit all symbols as global on Windows because they matter only for the PDB.
                     // Emit all symbols as global in multifile builds so that object files can
                     // link against each other.
-                    EmitSymbolDef(_sb, global: !_isSingleFileCompilation);
+                    bool isGlobal = _nodeFactory.Target.IsWindows || !_isSingleFileCompilation;
+
+                    EmitSymbolDef(_sb, isGlobal);
 
                     string alternateName = _nodeFactory.GetSymbolAlternateName(name);
                     if (alternateName != null)