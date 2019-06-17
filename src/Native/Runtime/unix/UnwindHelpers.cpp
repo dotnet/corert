2999291369f1f42be095a7a185f6d628e54a90be
Jan Kotas <jkotas@microsoft.com>
2019-06-17 14:33:23 -0700

    Fix OSX build breaks

--- src/Native/Runtime/unix/UnwindHelpers.cpp
+++ src/Native/Runtime/unix/UnwindHelpers.cpp
@@ -42,18 +42,7 @@ using libunwind::UnwindInfoSections;
 
 LocalAddressSpace _addressSpace;
 
-#ifdef __APPLE__
-
-struct dyld_unwind_sections
-{
-    const struct mach_header*   mh;
-    const void*                 dwarf_section;
-    uintptr_t                   dwarf_section_length;
-    const void*                 compact_unwind_section;
-    uintptr_t                   compact_unwind_section_length;
-};
-
-#else // __APPLE__
+#ifndef __APPLE__
 
 #if defined(_TARGET_AMD64_)
 // Passed to the callback function called by dl_iterate_phdr
@@ -484,9 +473,9 @@ UnwindInfoSections LocateUnwindSections(uintptr_t pc)
     // On macOS, we can use a dyld function from libSystem in order
     // to find the unwind sections.
 
-    libunwind::dyld_unwind_sections dyldInfo;
+    dyld_unwind_sections dyldInfo;
 
-  if (libunwind::_dyld_find_unwind_sections((void *)pc, &dyldInfo))
+    if (_dyld_find_unwind_sections((void *)pc, &dyldInfo))
     {
         uwInfoSections.dso_base                      = (uintptr_t)dyldInfo.mh;
 
