c7e21093691f24d3de3df854e024351810684168
yowl <scott.waye@hubse.com>
2019-09-02 10:07:09 -0500

    Wasm: fixes loading int16 and SBytes from the stack where they were previously not sign extended (#7763)
    Previously loading SBytes from the stack did not sign extend causing problems in the NativeFormatReader. This adds a test and fixes that scenario.

--- src/ILCompiler.WebAssembly/src/CodeGen/EvaluationStack.cs
+++ src/ILCompiler.WebAssembly/src/CodeGen/EvaluationStack.cs
@@ -184,7 +184,7 @@ namespace Internal.IL
 
         public LLVMValueRef ValueAsType(LLVMTypeRef type, LLVMBuilderRef builder)
         {
-            return ValueAsTypeInternal(type, builder, false);
+            return ValueAsTypeInternal(type, builder, Type != null && (Type.IsWellKnownType(WellKnownType.SByte) || Type.IsWellKnownType(WellKnownType.Int16)));
         }
 
         public LLVMValueRef ValueAsType(TypeDesc type, LLVMBuilderRef builder)
