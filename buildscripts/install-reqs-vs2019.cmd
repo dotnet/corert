cb6221360a88d88193fda49d7d6ee5d86a50a35b
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2019-09-14 18:40:50 +0200

    A couple fixes
    * 4.6 targeting pack is also needed for successful build
    * Fixed the script to work with non-preview VS too
    * Retarget reproNative/reproNativeCpp to the toolset installed by vsconfig

--- buildscripts/install-reqs-vs2019.cmd
+++ buildscripts/install-reqs-vs2019.cmd
@@ -5,9 +5,12 @@ set _VSINSTALLER="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_insta
 if not exist %_VSINSTALLER% (
 echo Visual Studio installer not found. && echo Expected at %_VSINSTALLER% && exit /b 1
 )
-set _VSPATH="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Preview"
+set _VSWHERE="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if exist %_VSWHERE% (
+ for /f "usebackq tokens=*" %%i in (`%_VSWHERE% -latest -prerelease -property installationPath -products *`) do set _VSPATH="%%i"
+)
 if not exist %_VSPATH% (
-echo Visual Studio 2019 Preview not found. && echo Expected at %_VSPATH% && exit /b 2
+echo Visual Studio installation not found. && exit /b 2
 )
 
 %_VSINSTALLER% modify %* --installPath %_VSPATH% --config "%~dp0..\.vsconfig"
