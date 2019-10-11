628113f198973bc99c23c5af2c92d5e297318e99
Jan Kotas <jkotas@microsoft.com>
2019-10-11 13:47:32 -0700

    Fix non-shared partition

--- src/System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeHelpers.CoreRT.cs
+++ src/System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeHelpers.CoreRT.cs
@@ -153,13 +153,23 @@ namespace System.Runtime.CompilerServices
 
         public static int OffsetToStringData
         {
-            get
-            {
+            // This offset is baked in by string indexer intrinsic, so there is no harm
+            // in getting it baked in here as well.
+            [System.Runtime.Versioning.NonVersionable]
+            get =>
                 // Number of bytes from the address pointed to by a reference to
-                // a String to the first 16-bit character in the String.  
+                // a String to the first 16-bit character in the String.  Skip
+                // over the MethodTable pointer, & String
+                // length.  Of course, the String reference points to the memory
+                // after the sync block, so don't count that.
                 // This property allows C#'s fixed statement to work on Strings.
-                return string.FIRST_CHAR_OFFSET;
-            }
+                // On 64 bit platforms, this should be 12 (8+4) and on 32 bit 8 (4+4).
+#if BIT64
+                12;
+#else // 32
+                8;
+#endif // BIT64
+
         }
 
         [ThreadStatic]
