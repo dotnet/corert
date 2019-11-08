e30c82781e21f5184bdf521f44231eb1e6ed167e
yowl <scott.waye@hubse.com>
2019-11-08 08:39:25 -0500

    Wasm: fix more occurrences of loading Sbytes into int32 without sign extending. (#7764)
    * add test for sbyte extend
    
    * fix loading short ints and sbytes from stack where they need to be sign extended.
    
    * Add more sign extending for other stack loads.
    
    * add test for sbyte br instructions which occur (and failed) in release mode.
    
    * sbyte il test.

--- tests/src/Simple/HelloWasm/Program.cs
+++ tests/src/Simple/HelloWasm/Program.cs
@@ -1007,6 +1007,31 @@ internal static class Program
         {
             FailTest("Expected -1 and 1 but got " + x.ToString() + " and " + x2.ToString());
         }
+
+        StartTest("SByte left shift");
+        x = (int)(s << 1);
+        if(x == -2)
+        {
+            PassTest();
+        }
+        else
+        {
+            FailTest("Expected -2 but got " + x.ToString());
+        }
+
+        sbyte minus1 = -1;
+        StartTest("Negative SByte op");
+        if((s & minus1) == -1)
+        {
+            PassTest();
+        }
+        else
+        {
+            FailTest();
+        }
+
+        StartTest("Negative SByte br"); 
+        EndTest(ILHelpers.ILHelpersTest.BneSbyteExtend());
     }
 
     [DllImport("*")]
