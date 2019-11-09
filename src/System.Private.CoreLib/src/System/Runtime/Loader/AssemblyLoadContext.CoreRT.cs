5971a79577751d4bc28a59ab8fbf67aec97115fa
Jan Kotas <jkotas@microsoft.com>
2019-11-09 06:45:25 -0800

    Delete method from shared partition

--- src/System.Private.CoreLib/src/System/Runtime/Loader/AssemblyLoadContext.CoreRT.cs
+++ src/System.Private.CoreLib/src/System/Runtime/Loader/AssemblyLoadContext.CoreRT.cs
@@ -51,28 +51,6 @@ namespace System.Runtime.Loader
             return ReflectionAugments.ReflectionCoreCallbacks.Load(arrAssembly, arrSymbols);
         }
 
-        internal IntPtr GetResolvedUnmanagedDll(Assembly assembly, string unmanagedDllName)
-        {
-            IntPtr resolvedDll = IntPtr.Zero;
-
-            Func<Assembly, string, IntPtr> dllResolveHandler = _resolvingUnmanagedDll;
-
-            if (dllResolveHandler != null)
-            {
-                // Loop through the event subscribers and return the first non-null native library handle
-                foreach (Func<Assembly, string, IntPtr>  handler in dllResolveHandler.GetInvocationList())
-                {
-                    resolvedDll = handler(assembly, unmanagedDllName);
-                    if (resolvedDll != IntPtr.Zero)
-                    {
-                        return resolvedDll;
-                    }
-                }
-            }
-
-            return IntPtr.Zero;
-        }
-
         private void ReferenceUnreferencedEvents()
         {
             // Dummy method to avoid CS0067 "Event is never used" warning.
