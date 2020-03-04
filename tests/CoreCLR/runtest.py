c2163f8137cf14d817fd100ef20e2892528a8e58
Andrii Kurdiumov <kant2002@gmail.com>
2020-03-04 19:22:29 +0600

    Fix running test without that code I have stack trace on Python 3.7.1 when run `tests\runtest.cmd /coreclr Top200` ``` Traceback (most recent call last):   File "runtest.py", line 2450, in <module>     sys.exit(main(args))   File "runtest.py", line 2423, in main     lambda path: do_setup(host_os,   File "runtest.py", line 550, in create_and_use_test_env     ret_code = func(None)   File "runtest.py", line 2433, in <lambda>     test_filter_path))   File "runtest.py", line 2356, in do_setup     build_test_wrappers(host_os, arch, build_type, coreclr_repo_location, test_location, None, test_filter_path)   File "runtest.py", line 1871, in build_test_wrappers     delete_existing_wrappers(to_unicode(test_location))   File "runtest.py", line 1822, in to_unicode     return str(s, "utf-8") TypeError: decoding str is not supported ```

--- tests/CoreCLR/runtest.py
+++ tests/CoreCLR/runtest.py
@@ -1816,7 +1816,10 @@ if sys.version_info.major < 3:
         return unicode(s, "utf-8")
 else:
     def to_unicode(s):
-        return str(s, "utf-8")
+        if not isinstance(s, str):
+            return str(s, "utf-8")
+        else:
+            return s
 
 def delete_existing_wrappers(test_location):
     """ Delete the existing xunit wrappers
