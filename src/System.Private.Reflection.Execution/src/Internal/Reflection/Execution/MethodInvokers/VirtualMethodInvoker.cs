d8712ad0dfd1fb597e936119442e233d56a5da90
Robin Sue <robinsue@live.de>
2020-05-18 02:11:41 +0200

    Implement GetInterfaceMap

--- src/System.Private.Reflection.Execution/src/Internal/Reflection/Execution/MethodInvokers/VirtualMethodInvoker.cs
+++ src/System.Private.Reflection.Execution/src/Internal/Reflection/Execution/MethodInvokers/VirtualMethodInvoker.cs
@@ -74,6 +74,11 @@ namespace Internal.Reflection.Execution.MethodInvokers
             return result;
         }
 
+        internal IntPtr ResolveTarget(RuntimeTypeHandle type)
+        {
+            return OpenMethodResolver.ResolveMethod(MethodInvokeInfo.VirtualResolveData, type);
+        }
+
         // On CoreCLR/Desktop, we do not attempt to resolve the target virtual method based on the type of the 'this' pointer.
         // For compatibility reasons, we'll do the same here.
         public sealed override IntPtr LdFtnResult
