211d95764088fe4444e4e0baf42cca617d38d677
Scott Waye <scott.waye@hubse.com>
2020-05-20 20:48:21 -0500

    remove tabs

--- tests/src/Simple/HelloWasm/dotnet_support.js
+++ tests/src/Simple/HelloWasm/dotnet_support.js
@@ -1,29 +1,29 @@
 
 var DotNetSupportLib = {
-	$DOTNET: {
-		_dotnet_get_global: function() {
-			function testGlobal(obj) {
-				obj['___dotnet_global___'] = obj;
-				var success = typeof ___dotnet_global___ === 'object' && obj['___dotnet_global___'] === obj;
-				if (!success) {
-					delete obj['___dotnet_global___'];
-				}
-				return success;
-			}
-			if (typeof ___dotnet_global___ === 'object') {
-				return ___dotnet_global___;
-			}
-			if (typeof global === 'object' && testGlobal(global)) {
-				___dotnet_global___ = global;
-			} else if (typeof window === 'object' && testGlobal(window)) {
-				___dotnet_global___ = window;
-			}
-			if (typeof ___dotnet_global___ === 'object') {
-				return ___dotnet_global___;
-			}
-			throw Error('unable to get DotNet global object.');
-		},
-	},
+    $DOTNET: {
+        _dotnet_get_global: function () {
+            function testGlobal(obj) {
+                obj['___dotnet_global___'] = obj;
+                var success = typeof ___dotnet_global___ === 'object' && obj['___dotnet_global___'] === obj;
+                if (!success) {
+                    delete obj['___dotnet_global___'];
+                }
+                return success;
+            }
+            if (typeof ___dotnet_global___ === 'object') {
+                return ___dotnet_global___;
+            }
+            if (typeof global === 'object' && testGlobal(global)) {
+                ___dotnet_global___ = global;
+            } else if (typeof window === 'object' && testGlobal(window)) {
+                ___dotnet_global___ = window;
+            }
+            if (typeof ___dotnet_global___ === 'object') {
+                return ___dotnet_global___;
+            }
+            throw Error('unable to get DotNet global object.');
+        },
+    },
     corert_wasm_invoke_js_unmarshalled: function (js, length, arg0, arg1, arg2, exception) {
 
         var jsFuncName = UTF8ToString(js, length);
