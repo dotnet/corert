a61834bee4b9db644dc8f7c083b9a2c075a1d0d6
Stephen Toub <stoub@microsoft.com>
2020-07-12 14:37:18 -0400

    Update license headers

--- src/System.Private.CoreLib/src/Internal/IntrinsicSupport/ComparerHelpers.cs
+++ src/System.Private.CoreLib/src/Internal/IntrinsicSupport/ComparerHelpers.cs
@@ -1,6 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 // The algoritm to choose the default comparer is duplicated in the IL compiler. The compiler will replace the code within
 // Comparer<T>.Create method with more specific implementation based on what sort of type is being compared where possible.
--- src/Test.CoreLib/src/System/Type.cs
+++ src/Test.CoreLib/src/System/Type.cs
@@ -1,6 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 // System.Type is only defined to support C# typeof. We shouldn't have it here since the semantic
 // is not very compatible.
