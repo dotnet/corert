821f52e4960b08893599fbc5cf2f80e406c17e27
Christian Scheuer <info@creatix.dk>
2020-07-23 12:36:13 +0200

    Temporary workaround for https://github.com/dotnet/corert/issues/8241

--- src/ILCompiler/src/ConfigurablePInvokePolicy.cs
+++ src/ILCompiler/src/ConfigurablePInvokePolicy.cs
@@ -70,7 +70,8 @@ namespace ILCompiler
             else
             {
                 // Account for System.Private.CoreLib.Native / System.Globalization.Native / System.Native / etc
-                return importModule.StartsWith("libSystem.");
+                // TODO: Remove "System." prefix - temporary workaround for https://github.com/dotnet/corert/issues/8241
+                return importModule.StartsWith("libSystem.") || importModule.StartsWith("System.");
             }
         }
     }
